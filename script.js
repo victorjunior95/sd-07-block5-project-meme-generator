d313c3b03f2ed115089ae333d3700ce61601de61
sagacello <sagacello@hotmail.com>
2020-09-11 17:42:28 -0300

    requisitos_parte1

---
+++ script.js
@@ -0,0 +1,7 @@
+let caixaTexto = document.querySelector('#text-input');
+        
+caixaTexto.addEventListener('input', function(){
+    console.log(caixaTexto)
+    let saidaTexto = document.querySelector('#meme-text');
+    saidaTexto.innerHTML = caixaTexto.value;
+});
