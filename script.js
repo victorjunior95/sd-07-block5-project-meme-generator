ae695f35044ebd6ca2aa68d88a094f7d2be66710
Anderson Santana <anderslash@gmail.com>
2020-09-11 18:10:25 -0300

    Ajustado texto para ficar na imagem

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let memeTextBox = document.querySelector("#meme-text");
 textInputBox.addEventListener("keyup", function() {
     memeTextBox.innerText = textInputBox.value;
 })
+
+let loadFile = function(event) {
+    let memeImage = document.querySelector("#meme-image");
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
+    memeImage.onload = function() {
+      URL.revokeObjectURL(memeImage.src) // free memory
+    }
+  };
