cf4f6fe1d677cbed766a7d8c02e88eeb8df57cb5
Bruno <bruno_grb@hotmail.com>
2020-09-11 15:38:29 -0300

    adicionando cabecalho com titulo e borda onde recebera a imagem

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let loadFile = function(event) {
-    let output = document.getElementById('output');
+let loadFile = function (event) { // Function taken from: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+    let output = document.getElementById('meme-image');
     output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
-      URL.revokeObjectURL(output.src) // free memory
+    output.onload = function () {
+        URL.revokeObjectURL(output.src) // free memory
     }
-};
+}
+
+let container = document.getElementById('meme-image-container');
