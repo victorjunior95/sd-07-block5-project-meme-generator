68891c01725c21b64699be58b18dac9b67522933
Julio Kauer <juliokauer@gmail.com>
2020-09-11 16:57:24 -0300

    Vamo ver se a parte 2

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 window.onload = function () {
   let textInput = document.querySelector("#text-input");
   let memeText = document.querySelector("#meme-text");
-  // let memeInsert = document.querySelector("#meme-insert");
+  let memeInsert = document.querySelector("#meme-insert");
   let memeImage = document.querySelector("#meme-image");
   function changeText() {
     memeText.innerText = `${textInput.value}`;
   }
   // https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
-  function changeImage(event) {
-    memeImage.src = URL.createObjectURL(event.target.files[0]);
+  function changeImage() {
+    memeImage.src = URL.createObjectURL(memeInsert.files[0]);
     memeImage.onload = function () {
       URL.revokeObjectURL(memeImage.src); // free memory
     };
-    console.log("oi")
   }
   textInput.addEventListener("input", changeText);
+  memeInsert.addEventListener("change",changeImage);
 };
