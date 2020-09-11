763d6facaa7c183553fd8442e406423190e3f05f
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-11 19:26:27 -0300

    Requisito 6 trocando as bordas

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let textInput = document.querySelector("#text-input");
 let memeText = document.querySelector("#meme-text");
 let inputImgMeme = document.querySelector("#meme-insert");
 let memeImg = document.querySelector("#meme-image");
+let memeContainer = document.querySelector("#meme-image-container");
+let fire = document.querySelector("#fire");
+let water = document.querySelector("#water");
+let earth = document.querySelector("#earth");
 
 
 textInput.addEventListener("keyup", function() {
@@ -15,4 +19,16 @@ inputImgMeme.addEventListener("change", function() {
         URL.revokeObjectURL(memeImg.src)
     }
 });
+
+fire.addEventListener("click", function() {
+    memeContainer.style.border =  "3px dashed rgb(255, 0, 0)"
+});
+water.addEventListener("click", function() {
+    memeContainer.style.border =  "5px double rgb(0, 0, 255)"
+});
+earth.addEventListener("click", function() {
+    memeContainer.style.border = "6px groove rgb(0, 128 ,0)"
+});
+
+
 
