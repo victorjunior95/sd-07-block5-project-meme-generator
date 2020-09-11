53f5a4d83b934c858088cc4e719c47ebe91be177
Renan OLiveira <ren4n.dev@gmail.com>
2020-09-11 18:59:50 -0300

    Comeeçando implementação dos requisitos bônus

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
+//Declarando elemento img que contém a imagem do meme para manipular
+let imageInsert = document.getElementById('meme-image');
+
+//Inserindo imagem por seleção de arquivo no dispositivo
 let memeInsert = document.querySelector('#meme-insert');
 memeInsert.addEventListener('change', function(event){
-    let imageInsert = document.getElementById('meme-image');
     imageInsert.src = URL.createObjectURL(event.target.files[0]);
     imageInsert.onload = function() {
         URL.revokeObjectURL(imageInsert.src);
     }
 })
 
+//Selecionando a miniatura 1
+let miniatura1 = document.querySelector('#miniatura1')
+miniatura1.addEventListener('click', function(event){
+    imageInsert.src = event.target.src;
+})
+
+//Selecionando a miniatura 2
+let miniatura2 = document.querySelector('#miniatura2')
+miniatura2.addEventListener('click', function(event){
+    imageInsert.src = event.target.src;
+})
+
+//Selecionando a miniatura 3
+let miniatura3 = document.querySelector('#miniatura3')
+miniatura3.addEventListener('click', function(event){
+    imageInsert.src = event.target.src;
+})
+
+//Inserindo texto no meme
 let textInput = document.querySelector('#text-input');
 textInput.addEventListener('input', function() {
     if (textInput.value.length <= 60) {
@@ -14,4 +36,25 @@ textInput.addEventListener('input', function() {
     } else {
         console.log("Você ultrapassou o limite de caracteres");
     } 
+})
+
+//Selecionando a DIV que será estilizada pelos botões
+let divMeme = document.querySelector('#meme-image-container');
+
+//Implementando a função do botão fire
+let fire = document.querySelector('#fire');
+fire.addEventListener('click', function(){
+    divMeme.className = "fire";
+})
+
+//Implementando a função do botão water
+let water = document.querySelector('#water');
+water.addEventListener('click', function(){
+    divMeme.className = "water";
+})
+
+//IMplementando a função do botão Earth
+let earth = document.querySelector('#earth');
+earth.addEventListener('click', function(){
+    divMeme.className = "earth";
 })
