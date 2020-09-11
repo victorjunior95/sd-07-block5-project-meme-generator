27e1efc9bbdf348ba1762ecf25caca71788a11b2
Carol Andrade <a.carolg1@gmail.com>
2020-09-11 20:13:56 -0300

    codeClimate para 80% obrigatorios

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 window.onload = function () {
-
   document.querySelector('#text-input').setAttribute('maxlength', 60);
 const textInputBox = document.querySelector('#text-input');
-  textInputBox.addEventListener('keyup', function () {
+    textInputBox.addEventListener('keyup', function () {
   const textInput = document.querySelector('#text-input').value;
-  const textMeme = document.querySelector('#meme-text');
-  textMeme.innerText = textInput ;
-});
+    const textMeme = document.querySelector('#meme-text');
+    textMeme.innerText = textInput;
+  });
 };
-
