8994282a3263656180323264cb01094217c58cf3
Bruno <bruno_grb@hotmail.com>
2020-09-11 17:41:09 -0300

    adicioando fotos para selecionar e realizando alteracoes

--- script.js
+++ script.js
@@ -8,6 +8,30 @@ let loadFile = function (event) { // Function taken from: https://stackoverflow.
 
 let textFill = document.getElementById('text-input');
 let text = document.getElementById('meme-text');
-textFill.addEventListener('change',function() {
+textFill.addEventListener('change', function () {
     text.innerText = textFill.value;
 });
+
+let image1 = document.getElementById('image1');
+image1.addEventListener('click', function(){
+    let imagem = document.getElementById('meme-image');
+    imagem.src = image1.src;
+})
+
+let image2 = document.getElementById('image2');
+image2.addEventListener('click', function(){
+    let imagem = document.getElementById('meme-image');
+    imagem.src = image2.src;
+})
+
+let image3 = document.getElementById('image3');
+image3.addEventListener('click', function(){
+    let imagem = document.getElementById('meme-image');
+    imagem.src = image3.src;
+})
+
+let image4 = document.getElementById('image4');
+image4.addEventListener('click', function(){
+    let imagem = document.getElementById('meme-image');
+    imagem.src = image4.src;
+})
