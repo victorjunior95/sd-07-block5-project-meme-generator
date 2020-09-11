ca6b63b883f6c1707b9c959d6ff7bc44a6c154c9
ericvini <ericcobain3@gmail.com>
2020-09-11 16:08:42 -0300

    crei a div que englobará a imagem.também foi adicionado estilo no texto digitado

---
+++ script.js
@@ -0,0 +1,5 @@
+const textoIdTextInput = document.getElementById('text-input').value;
+const textoIdMemeText = document.getElementById('meme-text');
+textoIdMemeText.innerHTML = textoIdTextInput;
+
+
