bbba09cb6cdc5b38cae5cba8cb3717b6ca70605a
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-11 18:04:59 -0300

    2. feito a organização da imagem e texto dentro da div meme-image-container

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.getElementById('text-input').addEventListener("keyup", function(){
+document.getElementById('text-input').addEventListener('keyup', function () {
     document.getElementById('meme-text').innerText = document.getElementById('text-input').value;
 })
+
+
+var loadFile = function (event) {
+    //localiza onde a imagem será exibida.
+    var memeImage = document.getElementById('meme-image');
+    //muda a atibuicao src da imagem itilizando manipulacao de eventose o RL.createObjectURL.
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
+    //libera a memoria apos a imagem ser carregada na pagina
+    memeImage.onload = function () {
+        URL.revokeObjectURL(memeImage.src);
+    }
+};
