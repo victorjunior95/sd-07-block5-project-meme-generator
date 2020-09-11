264baebb52d16ab37c3b6a1b0071a97a189b77bb
Berilo Wanderley <wberilo@gmail.com>
2020-09-11 16:49:51 -0300

    border buttons

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 window.onload = function (){
-  let input = document.querySelector('#text-input')
-  let output = document.querySelector('#meme-text')
-  let imgload = document.querySelector('#meme-insert')
-  let imgout = document.querySelector('#meme-image')
-  input.addEventListener('input',function(){
+  const input = document.querySelector('#text-input');
+  const output = document.querySelector('#meme-text');
+  const imgload = document.querySelector('#meme-insert');
+  const imgout = document.querySelector('#meme-image');
+  const container = document.querySelector('#meme-image-container')
+  input.addEventListener('input',function (){
     output.innerText = input.value;
   })
-  imgload.addEventListener('input',function(){
+  imgload.addEventListener('input',function (){
     console.log(imgload.value);
     imgout.src=imgload.value;
+  });
+  document.querySelector('#earth').addEventListener('click',function (){
+    container.style.border = '6px goove green'
+    console.log('erf')
   })
-}
+  document.querySelector('#water').addEventListener('click',function (){
+    container.style.border = '5px double blue'
+  })
+  document.querySelector('#fire').addEventListener('click',function (){
+    container.style.border = '3px dashed red'
+  })
+};
