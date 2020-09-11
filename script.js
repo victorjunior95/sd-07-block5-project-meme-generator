896c13cfce337f090a4f8f181ef885ccc1df3d39
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-11 16:30:49 -0300

    Adicionando alguns estilos e input da foto no conteiner

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ let memeText = document.querySelector("#meme-text");
 textInput.addEventListener("input", function () {
   memeText.innerHTML = textInput.value;
 });
+
+let memeInsert = document.querySelector("#meme-insert");
+let memeImage = document.querySelector("#meme-image");
+
+memeInsert.addEventListener("change", function (event) {
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = function () {
+    URL.revokeObjectURL(memeImage.src);
+  };
+});
+
