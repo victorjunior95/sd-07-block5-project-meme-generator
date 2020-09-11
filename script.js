5d89c6f5d08c38e4124a6115f3e683e6e58a45cd
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-11 17:10:37 -0300

    Adicionando imagens de memes prontos

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const choseFile = document.querySelector('#meme-insert');
-const btnFire = document.querySelector("#fire");
-const btnWater = document.querySelector("#water");
-const btnEarth = document.querySelector("#earth");
-const memeImageContainer = document.querySelector("#meme-image-container");
+const btnFire = document.querySelector('#fire');
+const btnWater = document.querySelector('#water');
+const btnEarth = document.querySelector('#earth');
+const memeImageContainer = document.querySelector('#meme-image-container');
+const meme1 = document.querySelector('#meme-1');
+const meme2 = document.querySelector('#meme-2');
+const meme3 = document.querySelector('#meme-3');
+const meme4 = document.querySelector('#meme-4');
 
 inputText.addEventListener('input', function () {
   memeText.innerText = inputText.value;
@@ -24,4 +28,20 @@ btnWater.addEventListener('click', function () {
 
 btnEarth.addEventListener('click', function () {
   memeImageContainer.style.border = '6px groove green';
+});
+
+meme1.addEventListener('click', function () {
+  document.querySelector('#meme-image').src = meme1.src;
+});
+
+meme2.addEventListener('click', function () {
+  document.querySelector('#meme-image').src = meme2.src;
+});
+
+meme3.addEventListener('click', function () {
+  document.querySelector('#meme-image').src = meme3.src;
+});
+
+meme4.addEventListener('click', function () {
+  document.querySelector('#meme-image').src = meme4.src;
 });
