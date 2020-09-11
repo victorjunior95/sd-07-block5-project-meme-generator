9bd81ac10f1962fa350e807b7d7ddb7644360fc1
josielvs <josielvs@gmail.com>
2020-09-11 18:32:39 -0300

    Atualizando o requisito 1.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = function () {
     function buttonTextInsert () {
-        let divInputText = document.getElementsByClassName('div-insert')[0]
+        let divInputText = document.getElementsByClassName("div-insert")[0]
 
-        sendButton = document.createElement('button')
-        sendButton.innerHTML = 'Enviar'
+        sendButton = document.createElement("button")
+        sendButton.innerHTML = "Enviar"
         divInputText.appendChild(sendButton)
     }
     buttonTextInsert()
