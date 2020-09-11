e0030dc397c2b938a328f1ef29a3b545ed6d8489
Fernando Antunes <fernando.antunes1@gmail.com>
2020-09-11 17:30:03 -0300

    Ajustando botões para aplicar as bodas corretas

--- script.js
+++ script.js
@@ -7,16 +7,19 @@ window.onload=function(){
     
     botaoUm.addEventListener('click', function(){
         conteinerImagem.style.borderColor='blue';
-        conteinerImagem.style.borderWidth='3px';
+        conteinerImagem.style.borderWidth='5px';
+        conteinerImagem.style.borderStyle='double';
     })
 
     botaoDois.addEventListener('click', function(){
         conteinerImagem.style.borderColor='red';
         conteinerImagem.style.borderWidth='3px';
+        conteinerImagem.style.borderStyle='dashed';
     })
 
     botaoTres.addEventListener('click', function(){
         conteinerImagem.style.borderColor='green';
-        conteinerImagem.style.borderWidth='3px';
+        conteinerImagem.style.borderWidth='6px';
+        conteinerImagem.style.borderStyle='groove';
     })
 }
