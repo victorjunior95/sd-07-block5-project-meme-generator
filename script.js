f3f17ec00ae21eab9a924870f8e9fe5e0962d3b6
Anderson Santana <anderslash@gmail.com>
2020-09-11 18:26:20 -0300

    Fundo branco no meme-image

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ textInputBox.addEventListener("keyup", function() {
     memeTextBox.innerText = textInputBox.value;
 })
 
+// Código abaixo inspirado em https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
 let loadFile = function(event) {
     let memeImage = document.querySelector("#meme-image");
     memeImage.src = URL.createObjectURL(event.target.files[0]);
