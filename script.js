105bf6b567d2970ceabff9a95b50005eae776fce
cezar88augusto <cezar88augusto@gmail.com>
2020-09-11 17:16:09 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ let caixaDeTexto = document.getElementById("text-input");
 let textoDiv = document.getElementById("meme-text");
 
 caixaDeTexto.addEventListener("keyup", function () {
-    
     textoDiv.innerHTML = caixaDeTexto.value;
 })
+
+//Método tirado do link: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+
+var loadFile = function (event) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var output = document.getElementById('meme-image');
+        output.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+};
+
+
+
