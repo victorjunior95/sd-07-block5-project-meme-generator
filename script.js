0f2c8978273001344baf6171d7eb713b05080d23
Francisco <franciscoliratorres@gmail.com>
2020-09-11 17:05:30 -0300

    Add finalização parte 1

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-let elementHeader = document.createElement("header");
-document.body.appendChild(elementHeader);
+let inputText = document.getElementById("text-input");
+inputText.addEventListener('input', function(){
+let showText = document.getElementById("meme-text");
+showText.innerHTML = inputText.value; 
+}); 
 
-let elementH1 = document.createElement("h1");
-elementH1.innerHTML = "MEME GENERATOR";
-elementHeader.appendChild(elementH1);
-
-let textImput = document.getElementById("text-imput");
-let memeText = socument.getElementById("meme-text")
-
-textImput.addEventListener("keyup", function () {
-    memeText.innerText = textImput.value
-})
 
 
 
