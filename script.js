1cbaa16ea264e55e61ba0ac3163d3d5001dad0dd
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-11 16:55:19 -0300

    cria referếncia para os elementos

---
+++ script.js
@@ -0,0 +1,6 @@
+const textInput = document.getElementById('text-input');
+const memeInsert = document.getElementById('meme-insert');
+const memeImageContainer = document.getElementById('meme-image-container');
+const memeImage = document.getElementById('meme-image');
+const memeText = document.getElementById('meme-text');
+const image = document.getElementById('image');
