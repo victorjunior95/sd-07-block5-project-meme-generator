7c6739c1ff05efbbee63d981a797361c048c03d8
Pedro MF <pedromarques1011@gmail.com>
2020-09-11 17:55:18 -0300

    dsp das dicas

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let textOutput = document.querySelector("#meme-text");
-document.querySelector("#text-input").addEventListener("change", (event) => {
+document.querySelector("#text-input").addEventListener("keyup", (event) => {
   textOutput.innerHTML = event.target.value;
   console.log(textOutput.innerHTML);
 });
