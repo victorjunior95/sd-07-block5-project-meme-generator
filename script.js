3b02171042ff54b998083a761014082ec869534b
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-11 13:27:16 -0300

    Adicionado a box para digitar um texto

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-var loadFile = function(event) {
-    var output = document.getElementById('meme-image');
+// fonte https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+const loadFile = function(event) {
+    const output = document.getElementById('meme-image');
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function() {
       URL.revokeObjectURL(output.src) // free memory
     }
   };
+
+  const textInput = document.getElementById('text-input');
+  textInput.addEventListener('keyup',function(){
+      document.getElementById('meme-text').innerText = textInput.value;
+  });
+  const textColor = document.getElementById('text-color');
+  textColor.addEventListener('change',function(){
+      document.getElementById('meme-text').style.color = textColor.value;
+  })
