26742605798984b5fcaa6b57672a2d7d6da0b9ce
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-09-11 19:04:10 -0300

    Estilizando projeto

--- script.js
+++ script.js
@@ -18,35 +18,35 @@ buttonWater.addEventListener("click", function () {
 
     let backGroundCont = document.querySelector("#meme-image-container")
     backGroundCont.style.border = `5px blue double`
- })
+})
 
 let buttonEarth = document.querySelector("#earth")
-buttonEarth.addEventListener("click" ,function(){
-    
+buttonEarth.addEventListener("click", function () {
+
     let backGroundCont = document.querySelector("#meme-image-container")
     backGroundCont.style.border = `6px green groove`
 })
 
- let meme1 = document.querySelector("#meme-1")
- meme1.addEventListener("click" , function(){
-   let imageMeme=document.querySelector("#meme-image")
-   imageMeme.src=meme1.src
- })
+let meme1 = document.querySelector("#meme-1")
+meme1.addEventListener("click", function () {
+    let imageMeme = document.querySelector("#meme-image")
+    imageMeme.src = meme1.src
+})
 
- let meme2 =document.querySelector("#meme-2")
- meme2.addEventListener("click" ,function(){
-    let imageMeme=document.querySelector("#meme-image")
-    imageMeme.src=meme2.src
+let meme2 = document.querySelector("#meme-2")
+meme2.addEventListener("click", function () {
+    let imageMeme = document.querySelector("#meme-image")
+    imageMeme.src = meme2.src
 })
 
 let meme3 = document.querySelector("#meme-3")
-meme3.addEventListener("click" ,function (){
-    let imageMeme=document.querySelector("#meme-image")
-    imageMeme.src=meme3.src
+meme3.addEventListener("click", function () {
+    let imageMeme = document.querySelector("#meme-image")
+    imageMeme.src = meme3.src
 })
 
 let meme4 = document.querySelector("#meme-4")
-meme4.addEventListener("click" ,function(){
-    let imageMeme=document.querySelector("#meme-image")
-    imageMeme.src=meme4.src
+meme4.addEventListener("click", function () {
+    let imageMeme = document.querySelector("#meme-image")
+    imageMeme.src = meme4.src
 })
