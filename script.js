332b7f8f0f0a9e5c3e9e2ef1d548c537885ce43e
vanderson-henrique <vhbenedito@gmail.com>
2020-09-11 16:51:33 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let text = document.querySelector('#text-input');
-text.addEventListener("input", insereTexto);
-
-function insereTexto(){
-    let textMeme = document.querySelector('#meme-text');
-    textMeme.innerText = text.value;
+function insereTexto() {
+    const textMeme = document.querySelector('#meme-text');
+  textMeme.innerText = text.value;
 }
+
+let text = document.querySelector('#text-input');
+text.addEventListener('input', insereTexto);
+
+function insereImagem(event) {
+    
+    let imageMeme = document.querySelector('#meme-image');
+    imageMeme.src = URL.createObjectURL(event.target.files[0]);
+}
+
+let memeInsert = document.querySelector('#meme-insert');
+memeInsert.addEventListener('change', insereImagem);
+
