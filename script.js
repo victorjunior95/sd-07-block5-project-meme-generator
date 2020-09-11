ea94ba228d76b6bccedb821afe308da46e632b6b
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-11 16:56:32 -0300

    adiciona funcionalidade de fazer upload de imagem

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const memeImageContainer = document.getElementById('meme-image-container');
 const memeImage = document.getElementById('meme-image');
 const memeText = document.getElementById('meme-text');
 const image = document.getElementById('image');
+
+memeInsert.addEventListener('change', loadFile);
+
+function loadFile(event) {
+  image.src = URL.createObjectURL(memeInsert.files[0]);
+  image.style.width = '500px';
+  image.style.height = '500px';
+
+  image.onload = () => URL.revokeObjectURL(image.src);
+}
