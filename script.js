4207fcafa0110a834f1413152b95cb165fa1c771
Danwhat <daanwhat@gmail.com>
2020-09-11 17:27:54 -0300

    pre meme done

--- script.js
+++ script.js
@@ -20,8 +20,25 @@ function imgUse(){
     memeImage.src = URL.createObjectURL(event.target.files[0])
 }
 
+let prememe = document.querySelectorAll(".prememe")
+for (let i = 0; i < prememe.length; i += 1) {
+    prememe[i].addEventListener("click", prememef)
+}
+function prememef() {
+    let memeImage = document.querySelector("#meme-image")
+    console.log(event.target.id)
+    switch (event.target.id) {
+        case "meme-1": memeImage.src = "/img1.jpg"
+        break
+        case "meme-2": memeImage.src = "/img2.jpg"
+        break
+        case "meme-3": memeImage.src = "/img3.jpeg"
+        break
+        case "meme-4": memeImage.src = "/img4.jpg"
+        break
+    }
 
-
+}
 
 
 let saidaTexto = document.getElementById('meme-text');
