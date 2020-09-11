9c09c9888deadd3e588bec42c96f8a26e35f7beb
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-09-11 20:33:18 -0300

    Botões de bordas

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const textInput = document.querySelector('#text-input');
-const memeText = document.querySelector('#meme-text');
+const textInput = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
+const button01 = document.querySelector('.button-01');
+const button02 = document.querySelector('.button-02');
+const button03 = document.querySelector('.button-03');
+const memeImageContainer = document.getElementById('meme-image-container');
 textInput.addEventListener('keyup', function() {
   memeText.innerHTML = textInput.value;
+});
+button01.addEventListener('click', function () {
+  memeImageContainer.style.border="3px red dashed"
+});
+button02.addEventListener('click', function () {
+  memeImageContainer.style.border="5px blue double"
+});
+button03.addEventListener('click', function () {
+  memeImageContainer.style.border="6px green groove"
 });
