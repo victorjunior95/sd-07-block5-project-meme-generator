8f62be2d51f244b756b7e5539ba0d663b24d2530
Carol Bezerra <carol.arbe@gmail.com>
2020-09-11 18:26:58 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ document.querySelector('#meme-insert').addEventListener('change', function (even
   imageHere.src = URL.createObjectURL(event.target.files[0]);
 });
 // essa função serve para gerar uma URL qualquer como src, assim a img não precisa estar na pasta
+
+document.querySelector('#fire').addEventListener('click', function () {
+  let toFire = document.querySelector('#meme-image-container');
+  toFire.style.border = '3px dashed red';
+});
+
+document.querySelector('#water').addEventListener('click', function () {
+  let toWater = document.querySelector('#meme-image-container');
+  toWater.style.border = '5px double blue';
+});
+
+document.querySelector('#earth').addEventListener('click', function () {
+  let toEarth = document.querySelector('#meme-image-container');
+  toEarth.style.border = '6px groove green';
+});
