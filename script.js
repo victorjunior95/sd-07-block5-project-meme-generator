f7a29ca59175ec70970711c10b89b7fbc5195e24
ericvini <ericcobain3@gmail.com>
2020-09-11 18:28:02 -0300

    Implementação requisito 2

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function getInputValue() {
   const input = document.getElementById('text-input').value;
-  const showInput=document.getElementById('meme-text');
-  showInput.innerHTML=input;
+  const showInput = document.getElementById('meme-text');
+  showInput.innerHTML = input;
 }
 
-//function getInputImage(){
-
-  //const inputImage = document.getElementById('meme-insert').value;
-  //const showInputImage=document.getElementById('meme-image');
-  //showInputImage.appendChild=inputImage;
-//}
-
-//}
+const loadImage = function (event) {
+  const output = document.getElementById('meme-insert');
+  const showImage=document.getElementById('meme-image');
+  showImage.innerHTML=output;
+  showImage.src = URL.createObjectURL(event.target.files[0]);
+  showImage.onload = function () {
+    URL.revokeObjectURL(showImage.src); // free memory
+  };
+};
