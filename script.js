57139052ffe63ceb934bf6d88beef583bb0a0fa3
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-09-11 17:11:20 -0300

    Finalizando requisito bonus 6

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let text = document.querySelector("#text-input")
 
+
 text.addEventListener("keyup", function () {
     let textInput = document.querySelector("#text-input").value
     let memeText = document.querySelector("#meme-text")
@@ -10,4 +11,22 @@ let image = document.querySelector("#meme-image")
 image.style.width = conteiner.style.width
 image.style.height = conteiner.style.height
 
+let buttonFire = document.querySelector("#fire")
 
+buttonFire.addEventListener("click", function () {
+
+    let backGroundCont = document.querySelector("#meme-image-container")
+    backGroundCont.style.border = `3px red dashed`
+})
+let buttonWater = document.querySelector("#water")
+buttonWater.addEventListener("click", function () {
+
+    let backGroundCont = document.querySelector("#meme-image-container")
+    backGroundCont.style.border = `3px blue double`
+ })
+let buttonEarth = document.querySelector("#earth")
+buttonEarth.addEventListener("click" ,function(){
+    
+    let backGroundCont = document.querySelector("#meme-image-container")
+    backGroundCont.style.border = `3px green groove`
+})
