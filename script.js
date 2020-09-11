3249043ae593550733913d858766000bb68c97e6
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-11 17:58:57 -0300

    terminei o requisito 3

--- script.js
+++ script.js
@@ -19,10 +19,11 @@ const insertTextInImage = function(event) {
 
 // utilizou-se essa referencia
 // https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
-
 const loadFile = function (event) {
   const memeImage = document.getElementById('meme-image');
+
   memeImage.src = URL.createObjectURL(event.target.files[0]);
+  
   memeImage.onload = function () {
     URL.revokeObjectURL(memeImage.src) // free memory
   }
