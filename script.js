eb5cd81f7413a7cf9358c9a188b10e66416b0e02
Danwhat <daanwhat@gmail.com>
2020-09-11 10:06:04 -0300

    just woke up

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let memeTela = document.querySelector("#meme-image-container")
 
-
 let areaTexto = document.querySelector("#text-input")
-areaTexto.addEventListener("change", textoMeme)
-
+areaTexto.addEventListener("input", textoMeme)
 
 function textoMeme(){
     let memeTelaTexto = document.querySelector("#meme-text")
