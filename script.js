1162046b5539e823441d1b3b44949183703f2e32
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-11 16:50:10 -0300

    Arrumando a referencia do JS

---
+++ script.js
@@ -0,0 +1,9 @@
+   
+let textInput = document.querySelector("#text-input");
+let memeText = document.querySelector("#meme-text");
+    
+ textInput.addEventListener("keyup", function() {
+    memeText.innerHTML = textInput.value;
+ });
+    
+
