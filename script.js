cdfc78800f1f93c3307c7e0ee4d79780a75be7fd
Julio Kauer <juliokauer@gmail.com>
2020-09-11 16:39:06 -0300

    Commit pra guardar o jeito que ta no stack overflow, no proximo vai ser do meu jeito

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 window.onload = function () {
-    let textInput = document.querySelector("#text-input");
-    let memeText = document.querySelector("#meme-text");
-    function changeText() {
-      memeText.innerText = `${textInput.value}`;
-    }
-    textInput.addEventListener("input",changeText);
-}
+  let textInput = document.querySelector("#text-input");
+  let memeText = document.querySelector("#meme-text");
+  // let memeInsert = document.querySelector("#meme-insert");
+  let memeImage = document.querySelector("#meme-image");
+  function changeText() {
+    memeText.innerText = `${textInput.value}`;
+  }
+  // https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+  function changeImage(event) {
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
+    memeImage.onload = function () {
+      URL.revokeObjectURL(memeImage.src); // free memory
+    };
+    console.log("oi")
+  }
+  textInput.addEventListener("input", changeText);
+};
