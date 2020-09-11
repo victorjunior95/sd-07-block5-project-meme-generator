94817936ea3c80685a7795e89907ddbbcb7d42cd
Berilo Wanderley <wberilo@gmail.com>
2020-09-11 15:58:23 -0300

    images

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 window.onload = function (){
   let input = document.querySelector('#text-input')
   let output = document.querySelector('#meme-text')
+  let imgload = document.querySelector('#meme-insert')
+  let imgout = document.querySelector('#meme-image')
   input.addEventListener('input',function(){
     output.innerText = input.value;
+  })
+  imgload.addEventListener('input',function(){
+    console.log(imgload.value);
+    imgout.src=imgload.value;
   })
 }
