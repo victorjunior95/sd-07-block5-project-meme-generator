4e59e225fce352150cacdf917dc0d5d3111e90fb
Fernando <nando.neves@gmail.com>
2020-09-11 12:21:48 -0300

    inclui imagem

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 //Inserindo o texto
-
-let entrada = document.getElementByClassName("caixa-texto")
-entrada.id = 'meme-image-container'
+let entrada = document.querySelector(".caixa-texto");
+entrada.id = 'meme-image-container';
 let legenda = document.querySelector(".meme-text");
 
+entrada.addEventListener("change",function(){
+    
 legenda.innerText = entrada.value; 
+console.log(legenda.innerText);
+})
+
+
+
 
 
 
 // Inserindo a imagem
 
-let imagem = document.querySelector(".imagem");
-let imageurl = document.querySelector("#meme-insert"); //caixatexto
-imagem.scr = imageurl.value;  
-//document.addEventListener()
+// var carregar = function (event){
+//     let imagem = document.querySelector(".image");
+//     imagem.scr = URL.createObjectURL(event.target.files[0]);
+//     console.log(imagem.src)
+//     imagem.onload = function() {
+//         URL.revokeObjectURL(imagem.src) // limpar memória
+//       }
+// }
+
+var loadFile = function(event) {
+    var output = document.getElementById('meme-image');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+  };
+
+
