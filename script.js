52e2092452262fa70bbbe6627a9e72e15c0acb0d
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-09-11 19:11:57 -0300

    aadicionando imagem

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let inputText = document.getElementById("text-input");
 inputText.addEventListener('input', function(){
-let showText = document.getElementById("meme-text");
-showText.innerHTML = inputText.value; 
+let memeText = document.getElementById("meme-text");
+memeText.innerHTML = inputText.value; 
 }); 
+
+let inputImg = document.getElementById('meme-insert')
+let imgElement = document.getElementById('meme-image');
+
+inputImg.addEventListener('change', function() {
+  var url = URL.createObjectURL(inputImg.files[0]);
+  imgElement.src = url;
+});
