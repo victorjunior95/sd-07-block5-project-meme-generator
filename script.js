76a9de13da53245aca6a4d23a91a57c89f32f67c
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-11 18:30:49 -0300

    :(

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let text = document.querySelector("#text-input")
 let textOnPhoto = document.querySelector("#meme-text")
 let imagem = document.querySelector("#image-insert")
-let imagemPhoto = document.querySelector("#meme-insert")
+let imagemPhoto = document.querySelector("#meme-image")
 
+function maxText(){
+    if(text.value.split('').length <= 60){
+        return text.value
+    }
+    else{
+        alert("O máximo é de 60 letras!")
+        return null
+    }
+}
 
 text.addEventListener("change", function(){
-    textOnPhoto.innerText = text.value
+    textOnPhoto.innerText = maxText()
 })
 imagem.addEventListener("input", function(){
     imagemPhoto.src = URL.createObjectURL(event.target.files[0])
