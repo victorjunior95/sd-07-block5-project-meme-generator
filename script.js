4848f0accaa34c8a6772f178b1b9ab31a5a441a4
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-09-11 20:55:24 -0300

    Correções climate

--- script.js
+++ script.js
@@ -5,14 +5,10 @@ const button02 = document.querySelector('.button-02');
 const button03 = document.querySelector('.button-03');
 const memeImageContainer = document.getElementById('meme-image-container');
 textInput.addEventListener('keyup', function() {
-  memeText.innerHTML = textInput.value;
-});
+  memeText.innerHTML = textInput.value;});
 button01.addEventListener('click', function () {
-  memeImageContainer.style.border="3px red dashed"
-});
+  memeImageContainer.style.border = '3px red dashed'});
 button02.addEventListener('click', function () {
-  memeImageContainer.style.border="5px blue double"
-});
+  memeImageContainer.style.border = '5px blue double'});
 button03.addEventListener('click', function () {
-  memeImageContainer.style.border="6px green groove"
-});
+  memeImageContainer.style.border = '6px green groove'});
