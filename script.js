da17a15f28e6fdde10e9b10da07462be34dd8f44
moraisgabri <gabriel8azevedo@gmail.com>
2020-09-11 18:26:02 -0300

    projeto inteiro?

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+  let img = document.querySelector('img')
+  let upload = document.querySelector('#meme-image')
+  let text = document.querySelector('#text-input')
+  let memeText = document.querySelector('#meme-text')
+  
+  
+  let openImage = function(event) {
+    console.log(upload.value)
+    img.src = URL.createObjectURL(event.target.files[0]);
+    img.width = 'fit-content'; 
+  };
+  
+  upload.addEventListener('change', openImage)
+
+  text.addEventListener('input', function() {
+    memeText.innerText = text.value
+  })
+}
+  
