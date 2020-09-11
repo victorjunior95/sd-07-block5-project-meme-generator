a0b12610297d646f7f59188eefbde65ba773ed7f
Vladimir <vladimirrchagas@gmail.com>
2020-09-11 17:39:32 -0300

    Verificando requisitos

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.querySelector('#text-input').addEventListener('keyup', function(){
+document.querySelector('#text-input').addEventListener('keyup', function () {
   document.querySelector('#meme-text').innerHTML = document.querySelector('#text-input').value;
 });
 
-document.querySelector('#meme-insert').addEventListener('change', function(event){
-  let img = document.querySelector('#image');
+document.querySelector('#meme-insert').addEventListener('change', function (event) {
+  const img = document.querySelector('#image');
   img.src = URL.createObjectURL(event.target.files[0]);
-}); //get code helper on https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+}); /*get code helper on https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977*/
