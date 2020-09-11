f0a01e7818338a08fe8e03eeaf6d2f0b5e52f894
Vladimir <vladimirrchagas@gmail.com>
2020-09-11 18:34:34 -0300

    Verificando requisitos

--- script.js
+++ script.js
@@ -6,3 +6,48 @@ document.querySelector('#meme-insert').addEventListener('change', function (even
   const img = document.querySelector('#image');
   img.src = URL.createObjectURL(event.target.files[0]);
 }); /* get code helper on https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977 */
+
+
+document.querySelector('#fire').addEventListener('click', function () {
+  const container = document.querySelector('.meme-image-container');
+
+  container.style.borderColor = 'red';
+  container.style.borderStyle = 'dashed';
+  container.style.borderWidth = '5px';
+});
+
+document.querySelector('#water').addEventListener('click', function () {
+  const container = document.querySelector('.meme-image-container');
+
+  container.style.borderColor = 'blue';
+  container.style.borderStyle = 'groove';
+  container.style.borderWidth = '10px';
+});
+
+document.querySelector('#earth').addEventListener('click', function () {
+  const container = document.querySelector('.meme-image-container');
+
+  container.style.borderColor = 'brown';
+  container.style.borderStyle = 'outset';
+  container.style.borderWidth = '15px';
+});
+
+document.querySelector('#src1').addEventListener('click', function () {
+  const img = document.querySelector('#image');
+  img.src = document.querySelector('#src1').src;
+});
+
+document.querySelector('#src2').addEventListener('click', function () {
+  const img = document.querySelector('#image');
+  img.src = document.querySelector('#src2').src;
+});
+
+document.querySelector('#src3').addEventListener('click', function () {
+  const img = document.querySelector('#image');
+  img.src = document.querySelector('#src3').src;
+});
+
+document.querySelector('#src4').addEventListener('click', function () {
+  const img = document.querySelector('#image');
+  img.src = document.querySelector('#src4').src;
+});
