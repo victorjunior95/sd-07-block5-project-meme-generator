63775f13637050230f55e73162db5d784689145a
Carol Bezerra <carol.arbe@gmail.com>
2020-09-11 18:53:34 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -3,23 +3,39 @@ document.querySelector('#text-input').addEventListener('keyup', function () {
   document.querySelector('#meme-text').innerText = text;
 });
 
+let imageHere = document.querySelector('#meme-image');
 document.querySelector('#meme-insert').addEventListener('change', function (event) {
-  const imageHere = document.querySelector('#meme-image');
   imageHere.src = URL.createObjectURL(event.target.files[0]);
 });
 // essa função serve para gerar uma URL qualquer como src, assim a img não precisa estar na pasta
 
 document.querySelector('#fire').addEventListener('click', function () {
-  let toFire = document.querySelector('#meme-image-container');
+  const toFire = document.querySelector('#meme-image-container');
   toFire.style.border = '3px dashed red';
 });
 
 document.querySelector('#water').addEventListener('click', function () {
-  let toWater = document.querySelector('#meme-image-container');
+  const toWater = document.querySelector('#meme-image-container');
   toWater.style.border = '5px double blue';
 });
 
 document.querySelector('#earth').addEventListener('click', function () {
-  let toEarth = document.querySelector('#meme-image-container');
+  const toEarth = document.querySelector('#meme-image-container');
   toEarth.style.border = '6px groove green';
+});
+
+document.querySelector('#meme-1').addEventListener('click', function () {
+  imageHere.src = document.querySelector('#meme-1').src;
+});
+
+document.querySelector('#meme-2').addEventListener('click', function () {
+  imageHere.src = document.querySelector('#meme-2').src;
+});
+
+document.querySelector('#meme-3').addEventListener('click', function () {
+  imageHere.src = document.querySelector('#meme-3').src;
+});
+
+document.querySelector('#meme-4').addEventListener('click', function () {
+  imageHere.src = document.querySelector('#meme-4').src;
 });
