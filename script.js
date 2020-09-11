bad1aa6260c105cfbea641cbbe1d571674908157
Carol Andrade <a.carolg1@gmail.com>
2020-09-11 18:41:06 -0300

    sub7

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
 
-textInputBox = document.querySelector('#text-input');
+let textInputBox = document.querySelector('#text-input').setAttribute('maxlength',60);
 textInputBox.addEventListener('change', function() {
     let textInput = document.querySelector('#text-input').value;
     let textMeme = document.querySelector('#meme-text');
