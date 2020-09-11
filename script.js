b9646c3b2c9d7556ef2c6d9d9b242563661d53de
jvitorof <j.vitoroflores@gmail.com>
2020-09-11 18:40:56 -0300

    Commit for the second request from Trybe Project

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//1
 const textBox = document.querySelector("#text-input")
 const textPresentation = document.querySelector("#meme-text")
 function showOnImg () {
@@ -5,3 +6,12 @@ function showOnImg () {
 }
 
 textBox.addEventListener("input", showOnImg);
+
+//2
+const source = document.querySelector("#meme-insert")
+const image = document.querySelector("#meme-image")
+function showTheImg (event) {
+    image.src = URL.createObjectURL(event.target.files[0]);
+}
+
+source.addEventListener("change", showTheImg);
