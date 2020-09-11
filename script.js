07add0d71939aed16dfd5a92a402837854fc44ec
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-11 16:14:49 -0300

    Wrap the text

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ window.onload = function() {
         }
     }
 
+    // Function that apply text on meme
+    let applyText = function() {
+        // get input text
+        let textInput = document.getElementById("text-input").value;
+        console.log("textMeme");
+        // put text on meme
+        let memeText = document.getElementById("meme-text");
+        memeText.innerText = textInput;
+    }
+
     // Get Element meme-insert and assign the event loadPhoto
     document.getElementById("meme-insert").addEventListener("change",loadPhoto);
+    
+    // Get Element meme-input and assign the event applyText
+    document.getElementById("text-input").addEventListener("input",applyText);
 }
