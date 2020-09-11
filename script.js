69f07f71d74e0a4e740147e5ffe2f2dcea5779d9
gabrieldeori <deori.pessoal@gmail.com>
2020-09-11 16:45:55 -0300

    Implementing famous meme Images

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ const memeInsert = document.getElementById('meme-insert');
 const memeText = document.getElementById('meme-text');
 const memeImage = document.getElementById('meme-image');
 
-// Buttons Fire Water Earth
+// Buttons Fire Water Earth Default
 const buttonDefault = document.getElementById('default');
 const buttonEarth = document.getElementById('earth');
 const buttonFire = document.getElementById('fire');
 const buttonWater = document.getElementById('water');
 
+// Memes Menu
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
+
+// Input Output functions
 function limpaObjectURL() {
   URL.revokeObjectURL(memeImage.src);
 }
@@ -24,7 +31,7 @@ function trocaImagem() {
   memeImage.onload = limpaObjectURL;
 }
 
-// Funções de borda
+// Border functions
 function borderDefault() {
   const eborder = document.getElementById('meme-image-container');
   eborder.style.borderWidth = '1px';
@@ -52,9 +59,35 @@ function borderWater() {
   wborder.style.borderColor = 'blue';
 }
 
+// Memes Menu functions
+function trocaMeme1() {
+  memeImage.src = meme1.src;
+}
+
+function trocaMeme2() {
+  memeImage.src = meme2.src;
+}
+
+function trocaMeme3() {
+  memeImage.src = meme3.src;
+}
+
+function trocaMeme4() {
+  memeImage.src = meme4.src;
+}
+
+// Input Output events 
 textInput.addEventListener('keyup', trocaTexto);
 memeInsert.addEventListener('change', trocaImagem);
+
+// Elemental Buttons events
 buttonDefault.addEventListener('click', borderDefault);
 buttonEarth.addEventListener('click', borderEarth);
 buttonFire.addEventListener('click', borderFire);
 buttonWater.addEventListener('click', borderWater);
+
+// Memes Menu events
+meme1.addEventListener('click', trocaMeme1);
+meme2.addEventListener('click', trocaMeme2);
+meme3.addEventListener('click', trocaMeme3);
+meme4.addEventListener('click', trocaMeme4);
