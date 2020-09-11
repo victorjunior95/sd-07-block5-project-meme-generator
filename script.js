07136f028e3fdd57ed84270dc6f2c5e0119fddef
Pedro MF <pedromarques1011@gmail.com>
2020-09-11 19:07:55 -0300

    climate3 o resto eh imutavel id

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const textOutput = document.querySelector('#meme-text');
 document.querySelector('#text-input').addEventListener('keyup', (event) => {
-    textOutput.innerHTML = event.target.value;
+  textOutput.innerHTML = event.target.value;
 });
 
 document.querySelector('#meme-insert').addEventListener('change', (event) => {
-    const image = document.querySelector('#meme-image');
-    image.src = URL.createObjectURL(event.target.files[0]);
+  const image = document.querySelector('#meme-image');
+  image.src = URL.createObjectURL(event.target.files[0]);
 });
