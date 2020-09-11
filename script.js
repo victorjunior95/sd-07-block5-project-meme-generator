16401836dd3153c1ead4181f255fd5dbb418dc8a
Felipe Nascimento <flpnascto@gmail.com>
2020-09-11 19:36:32 -0300

    Adicionando requisito 4 e 5.

--- script.js
+++ script.js
@@ -9,17 +9,16 @@ window.onload = function () {
     memeText.innerText = textInput.value;
   });
 
-  memeInsert.addEventListener('change', handleFiles, false)
+  memeInsert.addEventListener('change', handleFiles, false);
 
   function handleFiles () {
-    const img = document.createElement("img");
+    const img = document.createElement('img');
     img.src = URL.createObjectURL(this.files[0]);
     img.id = 'image';
     img.height = 400;
-    img.onload = function() {
+    img.onload = function () {
       URL.revokeObjectURL(this.src);
     }
-
     // Atualiza a imagem, se alguma foi carregada anteriormente.
     if (document.querySelector('#image') === null) {
       memeImage.appendChild(img);
