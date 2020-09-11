6a4f1787519c4533aba4603704c4476540b86e42
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-09-11 19:16:48 -0300

    adicionando botoes que mudam estilo da borda

--- script.js
+++ script.js
@@ -12,7 +12,21 @@ function addText() {
     document.getElementById('meme-text').innerText = text;
 }
 
+function changeBorderColor(event) {
+    let memeBorder = document.getElementById('meme-image-container');
+    if (event.target.value === "red") {
+        memeBorder.style.border = "3px dashed red";
+    } else if(event.target.value == "blue") {
+        memeBorder.style.border = "5px double blue";
+    } else {
+        memeBorder.style.border = "6px groove green";
+    }
+}
 let imgInsert = document.getElementById('meme-insert');
 imgInsert.addEventListener('input', addImage);
 let textInsert = document.getElementById('text-input');
 textInsert.addEventListener('input', addText);
+let buttons = document.getElementsByTagName('button');
+for (let i = 0; i < 3; i += 1) {
+    buttons[i].addEventListener('click', changeBorderColor);
+}
