c15b8144515c4fcfb5ec5a24ced9de6bb84851d0
Leonnardo Lair <leonnardo0316@gmail.com>
2020-09-11 19:31:37 -0300

    vendotext modification :3

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let uploadButton = document.getElementById("meme-insert");
 let imageContainer = document.getElementById("meme-image");
 let textInput = document.getElementById("text-input")
+let textmeme = document.getElementById("meme-text")
 uploadButton.addEventListener("change", function(){
     imageContainer.src = URL.createObjectURL(uploadButton.files[0]);
     console.log("amendoin");
 });
+textInput.addEventListener("change", function(){
+    textmeme.innerText = textInput.value; 
+})
 alert("amém");
