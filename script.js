81d23b0660170ce26ad4d17ffe8c4c0e3e28c4bd
Andre Serudo <andreserudo@gmail.com>
2020-09-11 15:27:35 -0400

    criando funcoes para manusear a img

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 let inputMemeText = document.getElementById('text-input');
 let memeTextArea = document.getElementById('meme-text');
+let inputMemeImage = document.getElementById('meme-insert');
+let imageMeme = document.getElementById('meme-image');
 
-inputMemeText.addEventListener('keyup', fillText);
-
-function fillText(e){
-
-  if(e.keyCode === 13){
-    console.log(inputMemeText.value);
-    memeTextArea.innerText = inputMemeText.value;
+function findImg(){
+  if( imageMeme != null) {
+    return true;
+  }else{
+    return false;
   }
 }
 
+function loadImg(e){
+  console.log(e);
+}
+
+function fillText(e){
+
+  if( !findImg() ){
+    if(e.keyCode === 13){
+      console.log(inputMemeText.value);
+      memeTextArea.innerText = inputMemeText.value;
+    }
+  }
+}
+
+inputMemeText.addEventListener('keyup', fillText);
+inputMemeImage.addEventListener('change', loadImg);
