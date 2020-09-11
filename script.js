3a28ea1bef7cf2e1299614363d892e190c747426
Carol Andrade <a.carolg1@gmail.com>
2020-09-11 20:07:44 -0300

    requisitos preenchidos

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 window.onload = function () {
 
-document.querySelector('#text-input').setAttribute('maxlength',60);
-
-let textInputBox = document.querySelector('#text-input');
-textInputBox.addEventListener('keyup', function() {
-    let textInput = document.querySelector('#text-input').value;
-    let textMeme = document.querySelector('#meme-text');
-    textMeme.innerText = textInput ;
+  document.querySelector('#text-input').setAttribute('maxlength', 60);
+const textInputBox = document.querySelector('#text-input');
+  textInputBox.addEventListener('keyup', function () {
+  const textInput = document.querySelector('#text-input').value;
+  const textMeme = document.querySelector('#meme-text');
+  textMeme.innerText = textInput ;
 });
-}
+};
 
