32b8f8a3078505a785d82f1e2005167288daf885
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-11 16:50:24 -0300

    Permitindo a quem usa customizar o meme escolhido acrescentando a ele uma de três bordas

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const choseFile = document.querySelector('#meme-insert');
+const btnFire = document.querySelector("#fire");
+const btnWater = document.querySelector("#water");
+const btnEarth = document.querySelector("#earth");
+const memeImageContainer = document.querySelector("#meme-image-container");
 
 inputText.addEventListener('input', function () {
   memeText.innerText = inputText.value;
@@ -8,4 +12,16 @@ inputText.addEventListener('input', function () {
 
 choseFile.addEventListener('change', function () {
   document.querySelector('#meme-image').src = window.URL.createObjectURL(choseFile.files[0]);
+});
+
+btnFire.addEventListener('click', function () {
+  memeImageContainer.style.border = '3px dashed red';
+});
+
+btnWater.addEventListener('click', function () {
+  memeImageContainer.style.border = '5px double blue';
+});
+
+btnEarth.addEventListener('click', function () {
+  memeImageContainer.style.border = '6px groove green';
 });
