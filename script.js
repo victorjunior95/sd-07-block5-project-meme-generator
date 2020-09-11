059e16373c24c3ee15e288b97bb60896dcc601e8
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-09-11 16:19:33 -0300

    Finaliando requisito 2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let text = document.querySelector("#text-input")
 
-text.addEventListener("keyup" , function(){
+text.addEventListener("keyup", function () {
     let textInput = document.querySelector("#text-input").value
     let memeText = document.querySelector("#meme-text")
-    memeText.innerText= textInput
+    memeText.innerText = textInput
 })
+let = conteiner = document.querySelector("#meme-image-container")
+let image = document.querySelector("#meme-image")
+image.style.width = conteiner.style.width
+image.style.height = conteiner.style.height
+
