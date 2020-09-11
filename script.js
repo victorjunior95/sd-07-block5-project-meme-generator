2d14dd024ab7e84b7e75abf5e3e6e866984e7c11
Francisco <franciscoliratorres@gmail.com>
2020-09-11 15:35:31 -0300

    Add parte 1

---
+++ script.js
@@ -0,0 +1,11 @@
+let elementHeader = document.createElement("header");
+document.body.appendChild(elementHeader);
+
+let elementH1 = document.createElement("h1");
+elementH1.innerHTML = "MEME GENERATOR";
+elementHeader.appendChild(elementH1);
+
+
+
+
+
