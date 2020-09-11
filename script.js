c2e1e59fc2680ab06857af8b45f56eb972f712cb
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-11 18:42:37 -0300

    Updating HTML

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let inputText = document.querySelector("#text-input")
 let memeText = document.querySelector(".meme-text")
+let memeImage = document.querySelector("#meme-image")
 
 inputText.addEventListener("keyup", ()=>{
     memeText.innerHTML = inputText.value
 })
+
+function printIMG(event){
+    if(event.target.files.lenght > 0){
+        let src = URL.createObjectURL(event.target.files[0])
+        memeImage.src = src
+    }
+}
