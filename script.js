91fb1de6617ed4d7bd53f98ae5d429682989b8c3
Carol Bezerra <carol.arbe@gmail.com>
2020-09-11 17:45:37 -0300

    Requisito 2 imagem

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.querySelector('#text-input').addEventListener("keyup", function() {
-  let text = document.querySelector('#text-input').value;
+document.querySelector('#text-input').addEventListener('keyup', function () {
+  const text = document.querySelector('#text-input').value;
   document.querySelector('#meme-text').innerText = text;
 });
+
+document.querySelector('#meme-insert').addEventListener('change', function(event) {
+  let imageHere = document.querySelector('img');
+  imageHere.src = URL.createObjectURL(event.target.files[0]);
+});
+// essa função serve para gerar uma URL qualquer, assim a img não precisa estar na pasta
