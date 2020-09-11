87ac33c86df874e81fe63d555bd969ee5c75a949
marcelosoliveira <msbobsk8@gmail.com>
2020-09-11 19:34:24 -0300

    Requisitos basicos terminados, vamos ver seo evaluator aprova

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ let insertMeme = document.querySelector('#meme-insert');
 let memeText = document.querySelector('#meme-text');
 let imageMeme = document.querySelector('#meme-image');
 
-//memeText.appendChild(inputText.nodeValue);
+inputText.addEventListener('keyup', function() {
+    memeText.innerHTML = inputText.value;
+    console.log(inputText.value);
+})
 
 insertMeme.addEventListener('change', (event) => {
     let arquivos = event.target.files;
     imageMeme.appendChild(document.createElement('img'));
-    imageMeme.querySelector('img').setAttribute('width', '500px');
-    imageMeme.querySelector('img').setAttribute('height', '500px');
+    imageMeme.querySelector('img').setAttribute('width', '495px');
+    imageMeme.querySelector('img').setAttribute('height', '395px');
     imageMeme.querySelector('img').src = URL.createObjectURL(arquivos[0]);
     imageMeme.querySelector('img').onload = function() {
     URL.revokeObjectURL(imageMeme.querySelector('img').src);
     }
-    console.log(arquivos);
 });
-
-
-
