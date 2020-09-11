3291d664dbd8c589734531e2ff71be1889f8404d
Fernando <nando.neves@gmail.com>
2020-09-11 14:17:08 -0300

    com botoes

--- script.js
+++ script.js
@@ -9,10 +9,6 @@ console.log(legenda.innerText);
 })
 
 
-
-
-
-
 // Inserindo a imagem
 
 var loadFile = function(event) {
@@ -22,5 +18,21 @@ var loadFile = function(event) {
       URL.revokeObjectURL(output.src) // free memory
     }
   };
+// colocando os botões
 
+  let botao1 = document.querySelector("#fire");
+  botao1.addEventListener("click",function(){
+      document.querySelector("#meme-image-container").style.border = "3px dashed red" 
+  })
 
+  let botao2 = document.querySelector("#water");
+  botao2.addEventListener("click",function(){
+      document.querySelector("#meme-image-container").style.border = "5px double blue"
+  })
+
+  let botao3 = document.querySelector("#earth");
+  botao3.addEventListener("click",function(){
+      document.querySelector("#meme-image-container").style.border = "6px groove green"
+
+  })
+//
