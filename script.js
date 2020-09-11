d3413533b1b246938231da367293001e328cb883
Eduardo Pizorno <pizorno@gmail.com>
2020-09-11 16:01:50 -0300

    Captura de imagem pronta

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function getText() {
 let userText = document.getElementById('text-input');
 userText.addEventListener('keypress', getText);
 
+function getImage(event){
+  let memeInsert = document.getElementById('meme-insert');
+  memeInsert.src = URL.createObjectURL(event.target.files[0]);
+  memeInsert.onload = function () {
+    URL.revokeObjectURL(memeInsert.src);
+  };
+}
+
+let memeInsert = document.getElementById('image');
+memeInsert.addEventListener('change', getImage);
