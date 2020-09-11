85ba68e823fc586d8cc561d5a095f5b69efaad7f
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-11 19:38:28 -0300

    Corrigindo erro ID

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let inputText = document.querySelector("#text-input")
-let memeText = document.querySelector(".meme-text")
+let memeText = document.querySelector("#meme-text")
 let memeImage = document.querySelector("#meme-image")
 
 inputText.addEventListener("keyup", ()=>{
