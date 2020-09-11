f838ae2ff88c2a2728d937e879866572e1efe04d
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-11 18:27:10 -0300

    alterando o evento 'change' para 'keyup'

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let selectTextInput = document.getElementById('text-input');
 
-selectTextInput.addEventListener ('change', insertText)
+selectTextInput.addEventListener ('keyup', insertText)
 
 function insertText() {
     let text = document.getElementById('text-input');
     let memeText = document.getElementById('meme-text');
 
     memeText.innerText = text.value;
-
 }
