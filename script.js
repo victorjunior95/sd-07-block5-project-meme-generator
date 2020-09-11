6a32681db1998a17276317f2e7f8039b59dc661b
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-11 13:24:42 -0300

    Estilizando a página, refatorando o input e a função de escolha da imagem

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let inputText = document.getElementById("text-input");
-let memeText = document.getElementById("meme-text");
+const inputText = document.getElementById("text-input");
+const memeText = document.getElementById("meme-text");
+const choseFile = document.querySelector("#meme-insert");
+const maxCharacters = 32;
 
 inputText.addEventListener("input", function(){
-  if (inputText.value.length <= 32) {
+  if (inputText.value.length <= maxCharacters) {
     memeText.innerText = inputText.value;
   }
-})
+});
+
+choseFile.addEventListener("change", function(){
+  let imgSrc = window.URL.createObjectURL(choseFile.files[0]);
+  document.querySelector("#meme-image").src = imgSrc;
+});
