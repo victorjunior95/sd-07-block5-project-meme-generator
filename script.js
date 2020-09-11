f7310714d5cb9fe6be82e32ebc5637b794e453bf
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-11 16:09:56 -0300

    Adicionados botoes para alterar a cor da borda

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ textColor.addEventListener('change', function () {
 
 const memeInsert = document.getElementById('meme-insert');
 memeInsert.addEventListener('change', loadFile);
+
+const fireButton = document.getElementById('fire');
+fireButton.addEventListener('click', function(){
+    document.getElementById('meme-image-container').style.border = '3px dashed red';
+});
+
+const waterButton = document.getElementById('water');
+waterButton.addEventListener('click', function(){
+    document.getElementById('meme-image-container').style.border = '5px double blue';
+});
+
+const earthButton = document.getElementById('earth');
+earthButton.addEventListener('click', function(){
+    document.getElementById('meme-image-container').style.border = '6px groove green'
+});
