58a195d2d84e70a843571edabb48b6b69b2c39d6
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-11 18:35:58 -0300

    terceiro requisito

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 window.onload = function(){
     //Declarando variáveis:
     let textMemeInput = document.getElementById("text-input");
+    let inputImg = document.getElementById("meme-insert");
     
 
-    //Criando evento
+    //Criando evento do texto
     textMemeInput.addEventListener("keyup", function(){
         let memeText = document.getElementById("meme-text");
         memeText.innerText = textMemeInput.value;
     });
+    //Criando evento da imagem
+    inputImg.addEventListener("change", function(event){
+        let imgMeme = document.getElementById("meme-image");
+        imgMeme.src = URL.createObjectURL(event.target.file[0]);
+    })
 
 
 
