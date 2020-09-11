01d63891f4cbf08c51bd82351f962f02b87c321f
sagacello <sagacello@hotmail.com>
2020-09-11 20:21:09 -0300

    requisitos_parte1_parte2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let caixaTexto = document.querySelector('#text-input');
-        
-caixaTexto.addEventListener('input', function(){
-    console.log(caixaTexto)
-    let saidaTexto = document.querySelector('#meme-text');
-    saidaTexto.innerHTML = caixaTexto.value;
-});
+
+window.onload = function() {
+    let caixaTexto = document.querySelector('#text-input');
+            
+    caixaTexto.addEventListener('input', function(){
+        console.log(caixaTexto)
+        let saidaTexto = document.querySelector('#meme-text');
+        saidaTexto.innerHTML = caixaTexto.value;
+    });
+
+ 
+}
