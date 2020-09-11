98b01454860eb0091a816b4e670107de1ace6ac6
dennerpl <denner.souzapl@gmail.com>
2020-09-11 19:29:42 -0300

    testando novos estilos

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 function changeImage() {
-  let backgroundImage =
-document.getElementById('meme-insert').files[0];
-let image = document.getElementById('meme-image');
-image.src = URL.createObjectURL(backgroundImage);
+  let backgroundImage = document.getElementById("meme-insert").files[0];
+  let image = document.getElementById("meme-image");
+  image.src = URL.createObjectURL(backgroundImage);
 }
 
 function changeText() {
   let memeText = document.getElementById("text-input").value;
-  document.getElementById('meme-text').innerHTML = memeText;
-
+  document.getElementById("meme-text").innerHTML = memeText;
 }
 
-document.getElementById('meme-insert').addEventListener("change", changeImage)
-document.getElementById('meme-insert').addEventListener("change", changeText)
-
+// document.getElementById("meme-insert").addEventListener("change", changeImage);
+// document.getElementById("text-insert").addEventListener("onChange", changeText);
