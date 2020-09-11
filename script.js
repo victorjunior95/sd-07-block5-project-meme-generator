254a4f7b630435e2179e6443cbfd0172418eac03
lisvalver <lisvalver.atte@gmail.com>
2020-09-11 18:56:01 -0300

    preview da imagem apos input

--- script.js
+++ script.js
@@ -5,9 +5,30 @@ inputText.addEventListener("input", function() {
     previewText.innerHTML = inputText.value;
 })
 
-let inputImage = document.getElementById("meme-insert");
+// Função baseada no código disponível em https://www.horadecodar.com.br/2020/05/20/javascript-preview-de-imagem-carregada-em-input-file/
 
-inputImage.addEventListener("input", function() {
-    let previewImage = document.getElementById("meme-image");
-    previewImage.innerHTML = inputImage.value
-})
+function readImage() {
+    if (this.files && this.files[0]) {
+        let file = new FileReader();
+        file.onload = function(e) {
+            document.getElementById("imageMemeInput").src = e.target.result;
+        };
+        file.readAsDataURL(this.files[0])
+    }
+}
+
+document.getElementById("meme-insert").addEventListener("change", readImage, false);
+
+
+
+
+// let inputImage = document.getElementById("meme-insert");
+
+// inputImage.addEventListener("input", function() {
+//     let previewImage = document.getElementById("meme-image");
+//     previewImage.innerHTML = inputImage.value;
+//     previewImage.src = URL.createObjectURL(this.files[0]);
+//     previewImage.onload = function() {
+//         URL.revokeObjectURL(previewImage.src);
+//       }
+// })
