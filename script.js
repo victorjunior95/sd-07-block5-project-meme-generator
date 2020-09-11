41edc2743c26f612b9a8b89b6e18fa45b79728df
Vanessa Oliveira <vanessanaara@gmail.com>
2020-09-11 17:53:27 -0300

    Adicionando o script que add a foto no container

---
+++ script.js
@@ -0,0 +1,7 @@
+let loadFile = function(event) {
+    let output = document.getElementById('meme-image');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src);
+    }
+  };
