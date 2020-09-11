5eb4b4faad71e82768e1d48b33850fb2f3bf492d
Carol Andrade <a.carolg1@gmail.com>
2020-09-11 18:17:55 -0300

    sub5

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 window.onload = function () {
 
 text_input_box = document.querySelector('#text-input');
@@ -6,5 +5,12 @@ text_input_box.addEventListener('change', function() {
     let text_input = document.querySelector('#text-input').value;
     let text_meme = document.querySelector('#meme-text');
     text_meme.innerText = text_input;
+})
+
+meme_insert_box = document.querySelector('#meme-insert');
+meme_insert_box.addEventListener('change', function() {
+    let meme_insert = document.querySelector('#meme-insert').value;
+    let meme_image = document.querySelector('#meme-image');
+    meme_image = meme_insert;
 })
 }
