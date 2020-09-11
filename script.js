697fe0a774578434d369cd4549c8d0c1c026c41c
Pablo Pessanha <pablopessanh@gmail.com>
2020-09-11 19:32:35 -0300

    All bonus request done

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let textInput = document.querySelector('#text-input');
+let buttons = document.querySelectorAll('button');
+let imageClick = document.querySelector('#pre-memes').querySelectorAll('img');
+
 textInput.addEventListener('keyup', () => {
    let newOutput = document.querySelector('#meme-text');
    
@@ -13,3 +16,33 @@ function loadFile (e) {
       URL.revokeObjectURL(output.src);
    }
 };
+
+for (let i = 0; i < buttons.length; i += 1){
+   let border = document.querySelector('#meme-image-container');
+
+   buttons[i].addEventListener('click', () => {
+      if (i === 0) {
+         border.style.border = 'red dashed 3px';
+      } else if ( i === 1) {
+         border.style.border = 'blue double 5px';
+      }else if ( i === 2) {
+         border.style.border = 'green groove 6px';
+      }
+   });
+}
+
+for (let i = 0; i < imageClick.length; i += 1){
+   let output = document.querySelector('#meme-image');
+
+   imageClick[i].addEventListener('click', () => {
+      if (i === 0) {
+         output.src = './imgs/chloe.jpg';
+      } else if ( i === 1) {
+         output.src = './imgs/michel-temer.jpg';
+      } else if ( i === 2) {
+         output.src = './imgs/voldemort.jpg';
+      } else if ( i === 3) {
+         output.src = './imgs/fry.jpg';
+      }
+   });
+}
