cfc312ca49a7b10dc2aa9f44b44e77becaadca33
Carol Andrade <a.carolg1@gmail.com>
2020-09-11 20:18:45 -0300

    codeClimate 80%

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ window.onload = function () {
   document.querySelector('#text-input').setAttribute('maxlength', 60);
   const textInputBox = document.querySelector('#text-input');
   textInputBox.addEventListener('keyup', function () {
-      const textInput = document.querySelector('#text-input').value;
-      const textMeme = document.querySelector('#meme-text');
-      textMeme.innerText = textInput;
-    });
+    const textInput = document.querySelector('#text-input').value;
+    const textMeme = document.querySelector('#meme-text');
+    textMeme.innerText = textInput;
+});
 };
