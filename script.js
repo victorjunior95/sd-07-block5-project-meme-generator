de4b88b422e3f091815c4fcc49e5c940c568c80d
Berilo Wanderley <wberilo@gmail.com>
2020-09-11 15:40:38 -0300

    containers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function (){
   let input = document.querySelector('#text-input')
   let output = document.querySelector('#meme-text')
-  input.addEventListener('change',function(){
+  input.addEventListener('input',function(){
     output.innerText = input.value;
   })
 }
