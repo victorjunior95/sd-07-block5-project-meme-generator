79d17c802e8fdaa8ab578e3fafccf3001e51ae08
Israel Menon <zezomenon@gmail.com>
2020-09-11 19:23:25 -0300

    Corrigindo upload da imagem

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ textInput.addEventListener("keyup", function() {
   console.log(document.getElementById("meme-text").innerText = textInput.value);
 });
 
-function loadFile(event) {
-  let insertImage = document.getElementById("meme-insert");
-  insertImage.addEventListener("click", function() {
-    let memeImage = document.getElementById("meme-image");
-    memeImage.src = URL.createObjectURL(event.target.files[0]);
-    memeImage.onload = function() {
-      URL.revokeObjectURL(memeImage.src);
-    }
-  });
-  console.log(memeImage);  
-}
+
+let insertImage = document.getElementById("meme-insert");
+let memeImage = document.getElementById("meme-image");
+insertImage.addEventListener("change", function() {
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = function() {
+    URL.revokeObjectURL(memeImage.src);
+  }
+});
+console.log(memeImage);  
+
