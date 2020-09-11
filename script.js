1f3963e085ba1d22ef0e43068a7702360cee9646
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-11 19:08:46 -0300

    6. adicionado os botoes coloridos para mudar a borda do container

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById('text-input').addEventListener('keyup', function () {
     document.getElementById('meme-text').innerText = document.getElementById('text-input').value;
 })
 
-var loadFile = function (event) {
+let loadFile = function (event) {
     //localiza onde a imagem será exibida.
-    var memeImage = document.getElementById('meme-image');
+    let memeImage = document.getElementById('meme-image');
     //muda a atibuicao src da imagem itilizando manipulacao de eventose o RL.createObjectURL.
     memeImage.src = URL.createObjectURL(event.target.files[0]);
     //libera a memoria apos a imagem ser carregada na pagina
@@ -13,3 +13,19 @@ var loadFile = function (event) {
     }
 };
 
+let memeContainer = document.getElementById('meme-image-container');
+
+let botaoFire = document.getElementById('fire');
+botaoFire.addEventListener('click', function () {
+    memeContainer.style.border = "dashed 3px rgb(255, 0, 0)"
+});
+
+let botaoWater = document.getElementById('water');
+botaoWater.addEventListener('click', function () {
+    memeContainer.style.border = "double 5px rgb(0, 0, 255)"
+});
+
+let botaoEarth = document.getElementById('earth');
+botaoEarth.addEventListener('click', function () {
+    memeContainer.style.border = "groove 6px rgb(0, 128, 0)"
+});
