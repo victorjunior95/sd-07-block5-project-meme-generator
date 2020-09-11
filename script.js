7132217b51b109c5d0469b94a5f99e2173393431
marcelosoliveira <msbobsk8@gmail.com>
2020-09-11 18:24:38 -0300

    Requisitos 3 em desenvolvimento

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ insertMeme.addEventListener('change', (event) => {
     let arquivos = event.target.files;
     imageMeme.appendChild(document.createElement('img'));
     imageMeme.querySelector('img').setAttribute('width', '500px');
-    imageMeme.querySelector('img').setAttribute('height', '400px');
-    imageMeme.querySelector('img').src = arquivos[0].name;
+    imageMeme.querySelector('img').setAttribute('height', '500px');
+    imageMeme.querySelector('img').src = URL.createObjectURL(arquivos[0]);
+    imageMeme.querySelector('img').onload = function() {
+    URL.revokeObjectURL(imageMeme.querySelector('img').src);
+    }
     console.log(arquivos);
 });
+
+
+
