df52b1987507ba76a4c8cc9742fc3b4192197b5e
Carol Andrade <a.carolg1@gmail.com>
2020-09-11 19:51:17 -0300

    requisito1

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ window.onload = function () {
 document.querySelector('#text-input').setAttribute('maxlength',60);
 
 let textInputBox = document.querySelector('#text-input');
-textInputBox.addEventListener('change', function() {
+textInputBox.addEventListener('keyup', function() {
     let textInput = document.querySelector('#text-input').value;
     let textMeme = document.querySelector('#meme-text');
     textMeme.innerText = textInput ;
-})
+});
 }
 
