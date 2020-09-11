03d019cdda77b80c42911cd8b88f8fa7ac10a8c1
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-11 17:30:31 -0300

    Corrigindo erro

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let inputText = document.querySelector("#text-input")
 let memeText = document.querySelector(".meme-text")
-let image = document.querySelector(#meme-insert")
 
 inputText.addEventListener("keyup", ()=>{
     memeText.innerHTML = inputText.value
