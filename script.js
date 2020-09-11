c258f6638608fefefe21fba7fb02b9fb5bda9b32
Bruno L Nogueira <nugobruno@gmail.com>
2020-09-11 16:33:21 -0300

    Imagem inserida

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let insertText = document.querySelector("#text-input");
 let textButton = document.querySelector("#text-button");
 let container = document.querySelector("#meme-image-container");
+let memeImage = document.querySelector("#meme-image");
 let memeText = document.querySelector("#meme-text");
 
 
 textButton.addEventListener("click", function() {
   memeText.innerHTML = insertText.value;
 })
+
+let loadFile = function(event) {
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = function() {
+    URL.revokeObjectURL(memeImage.src)
+  }
+}
