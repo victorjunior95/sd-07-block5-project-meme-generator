2aa08d3105c3f45da80c4f9c28b3189ad09338c4
brendalima <brenda.c.lima@poli.ufrj.br>
2020-09-11 19:44:28 -0300

    attempt to fix bonus requirement 6: adding background colors. Starting on requirement 7

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let inputText = document.getElementById("text-input");
 let text = document.getElementById("meme-text");
 let inputImage = document.getElementById("meme-insert");
+let image = document.getElementById("meme-image");
 let container =  document.getElementById("meme-image-container");
 let buttonFire = document.getElementById("fire");
 let buttonWater = document.getElementById("water");
 let buttonEarth = document.getElementById("earth");
+let replacementMeme = document.querySelectorAll(".miniature-memes");
 
 
 inputText.addEventListener("keyup", function() {
@@ -12,7 +14,6 @@ inputText.addEventListener("keyup", function() {
 });
 
 inputImage.addEventListener("change", function(event) {
-    let image = document.getElementById("meme-image");
     image.src = URL.createObjectURL(event.target.files[0]);
     image.onload = function() {
         URL.revokeObjectURL(image.src);
@@ -21,17 +22,21 @@ inputImage.addEventListener("change", function(event) {
 
 buttonFire.addEventListener("click", function(){
     container.style.border = "3px dashed red";
-})
+});
 
 buttonWater.addEventListener("click", function(){
     container.style.border = "5px double blue";
-})
+});
 
 buttonEarth.addEventListener("click", function(){
     container.style.border = "6px groove green";
+});
+
+replacementMeme.addEventListener("click", function(event){
+    image.src = URL.createObjectURL(event.target.files[0]);
+    image.onload = function() {
+        URL.revokeObjectURL(image.src);
 })
-
-
 
 
 
