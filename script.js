24d611857d6e4c5f8fb2f1d7c6a830e03b358824
vanderson-henrique <vhbenedito@gmail.com>
2020-09-11 18:10:42 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let imageMeme = document.querySelector('#meme-image');
+let container = document.getElementById('meme-image-container');
+
 function insereTexto() {
     const textMeme = document.querySelector('#meme-text');
   textMeme.innerText = text.value;
@@ -7,15 +10,12 @@ let text = document.querySelector('#text-input');
 text.addEventListener('input', insereTexto);
 
 function insereImagem(event) {
-    
-    let imageMeme = document.querySelector('#meme-image');
+
     imageMeme.src = URL.createObjectURL(event.target.files[0]);
 }
 
 let memeInsert = document.querySelector('#meme-insert');
 memeInsert.addEventListener('change', insereImagem);
-
-let container = document.getElementById('meme-image-container');
 
 function bordaFire() {
     container.style.border = "3px dashed red";
@@ -35,3 +35,28 @@ let buttonWater = document.getElementById('water');
 buttonWater.addEventListener('click', bordaWater);
 let buttonEarth = document.getElementById('earth');
 buttonEarth.addEventListener('click', bordaEarth);
+
+function mini1() {
+    imageMeme.src = "imgs/meme1.png";
+}
+
+function mini2() {
+    imageMeme.src = "imgs/meme2.png";
+}
+
+function mini3() {
+    imageMeme.src = "imgs/meme3.png";
+}
+
+function mini4() {
+    imageMeme.src = "imgs/meme4.png";
+}
+
+let miniMeme1 = document.getElementById('meme-1');
+miniMeme1.addEventListener('click', mini1);
+let miniMeme2 = document.getElementById('meme-2');
+miniMeme2.addEventListener('click', mini2);
+let miniMeme3 = document.getElementById('meme-3');
+miniMeme3.addEventListener('click', mini3);
+let miniMeme4 = document.getElementById('meme-4');
+miniMeme4.addEventListener('click', mini4);
