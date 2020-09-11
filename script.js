c7ca7af8008352c573131a38c452decae214061a
brendalima <brenda.c.lima@poli.ufrj.br>
2020-09-11 19:14:07 -0300

    bonus: request 6. custom borders for the container

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let inputText = document.getElementById("text-input");
 let text = document.getElementById("meme-text");
 let inputImage = document.getElementById("meme-insert");
+let container =  document.getElementById("meme-image-container");
+let buttonFire = document.getElementById("fire");
+let buttonWater = document.getElementById("water");
+let buttonEarth = document.getElementById("earth");
 
 
 inputText.addEventListener("keyup", function() {
@@ -14,6 +18,18 @@ inputImage.addEventListener("change", function(event) {
         URL.revokeObjectURL(image.src);
     }
 });
+
+buttonFire.addEventListener("click", function(){
+    container.style.border = "3px dashed red";
+})
+
+buttonWater.addEventListener("click", function(){
+    container.style.border = "5px double blue";
+})
+
+buttonEarth.addEventListener("click", function(){
+    container.style.border = "6px groove green";
+})
 
 
 
