7abd9585d3aff71e0aec0752e01a5b88caddd2f0
Berilo Wanderley <wberilo@gmail.com>
2020-09-11 17:37:08 -0300

    cc changes

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ window.onload = function () {
   const container = document.querySelector('#meme-image-container');
   input.addEventListener('input', function () {output.innerText = input.value;});
   imgload.addEventListener('input', function () {imgout.src = imgload.value;});
-  document.querySelector('#earth').addEventListener('click', function () {container.style.border = '6px groove green';});
-  document.querySelector('#water').addEventListener('click', function () {container.style.border = '5px double blue';});
-  document.querySelector('#fire').addEventListener('click', function () {container.style.border = '3px dashed red';});
+  document.querySelector('#earth').addEventListener('click', function () { container.style.border = '6px groove green'; });
+  document.querySelector('#water').addEventListener('click', function () { container.style.border = '5px double blue'; });
+  document.querySelector('#fire').addEventListener('click', function () { container.style.border = '3px dashed red'; });
   const buttons = document.querySelectorAll('.memes');
   for (let i = 0; i < buttons.length; i += 1) {
     buttons[i].addEventListener('click', function () {
