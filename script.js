88c1206c18f8e33f794129cd396ad82588715af3
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-11 20:30:36 -0300

    atualizando 1 e 2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// adicionando texto ao meme
+const textInput = document.querySelector('#text-input');
+textInput.addEventListener("keyup", updateMemeText);
+
+const memeText = document.querySelector('#meme-text');
+
+function updateMemeText() {
+  memeText.innerHTML = textInput.value;
+}
 
 // utilizou-se essa referencia
 // https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
