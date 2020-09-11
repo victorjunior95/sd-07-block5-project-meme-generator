9d01549a6a4a54bfb1eb242cf43fe2b2153c4d78
Francisco <franciscoliratorres@gmail.com>
2020-09-11 16:03:58 -0300

    Add ajustes parte 1

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let elementH1 = document.createElement("h1");
 elementH1.innerHTML = "MEME GENERATOR";
 elementHeader.appendChild(elementH1);
 
+let textImput = document.getElementById("text-imput");
+textImput.addEventListener("keyup", function () {
+    document.getElementById("meme-text").innerText = textImput.value;
+})
 
 
 
