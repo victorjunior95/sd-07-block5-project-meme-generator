538e4f2697105d91bdd80eabc005b7b9aa021484
Felipe Nascimento <flpnascto@gmail.com>
2020-09-11 17:14:48 -0300

    Verificando requisito 1.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 window.onload = function () {
-  
   const textInput = document.querySelector('#text-input');
   const memeText = document.querySelector('#meme-text');
 
   // insere texto digitado no input dentro do container do meme
   textInput.addEventListener('keyup', function () {
     memeText.innerText = textInput.value;
-  });  
-}
+  });
+};
