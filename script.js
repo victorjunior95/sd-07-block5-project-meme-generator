68c11695e63e056fa994fa093218068f0ceee520
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-11 16:52:59 -0300

    Várias coisas

---
+++ script.js
@@ -0,0 +1,12 @@
+let text = document.querySelector("#text-input")
+let textOnPhoto = document.querySelector("#meme-text")
+let imagem = document.querySelector("#image-insert")
+let imagemPhoto = document.querySelector("#meme-insert")
+
+
+text.addEventListener("change", function(){
+    textOnPhoto.innerText = text.value
+})
+imagem.addEventListener("change", function(){
+    imagemPhoto.src = URL.createObjectURL(event.target.files[0])
+})
