e8be0936572986ec75e306c1743f9bd6b058bd0b
hvenses <hvenses@gmail.com>
2020-09-11 19:02:39 -0300

    Incluindo parametros do texto sob a imagem.

--- script.js
+++ script.js
@@ -9,10 +9,8 @@ caixaTexto.addEventListener("keyup", insertText);
 
 let inputImage = document.querySelector("meme-insert");
 let image = document.getElementById("meme-image");
-
+inputImage.addEventListener("change", insertImage);
 function insertImage(event) {
   image.src = URL.createObjectURL(event.target.files[0]);
-  document.getElementById("img.loaded").src = URL.createObjectURL(inputImage);
 }
-
-inputImage.addEventListener("change", insertImage);
+window.URL.revokeObjectURL(insertImage);
