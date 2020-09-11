268de5ab99498a7994ae24263db5526721aad833
Vanessa Oliveira <vanessanaara@gmail.com>
2020-09-11 20:23:20 -0300

    Adicionando script de inserção do texto na imagem

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let loadFile = function(event) {
+function loadFile(event) {
   let output = document.getElementById('meme-image');
   output.src = URL.createObjectURL(event.target.files[0]);
   output.onload = function() {
     URL.revokeObjectURL(output.src);
-  }
+  };
 };
+
+let writeText = document.getElementById('text-input');
+let showText = document.getElementById('meme-text');
+function inputText() {
+  showText.innerText = writeText.value;
+}
+
+writeText.addEventListener('keyup', inputText);
