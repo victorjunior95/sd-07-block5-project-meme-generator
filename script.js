63571cf92d2fbedf402c9515c4c315169e2d2bec
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-11 17:35:40 -0300

    meme text shadow, size and color completed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+//Visualizar texto do input no canvas da imagem-meme
 let textInput = document.getElementById('text-input');
 let memeText = document.getElementById('meme-text');
 
 textInput.addEventListener('input', function() {
     memeText.innerText = textInput.value;
 })
+
