bfc2eaee6b955f7087b9934c52b9fe51a84742d0
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-11 13:47:46 -0300

    Limitando a quantidade de caracteres digitados

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const inputText = document.getElementById("text-input");
-const memeText = document.getElementById("meme-text");
-const choseFile = document.querySelector("#meme-insert");
-const maxCharacters = 32;
+const inputText = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
+const choseFile = document.querySelector('#meme-insert');
+const maxCharacters = 60;
 
-inputText.addEventListener("input", function(){
-  if (inputText.value.length <= maxCharacters) {
-    memeText.innerText = inputText.value;
-  }
+inputText.addEventListener('input', function() {
+  memeText.innerText = inputText.value;
 });
 
-choseFile.addEventListener("change", function(){
+choseFile.addEventListener('change', function() {
   let imgSrc = window.URL.createObjectURL(choseFile.files[0]);
-  document.querySelector("#meme-image").src = imgSrc;
+  document.querySelector('#meme-image').src = imgSrc;
 });
