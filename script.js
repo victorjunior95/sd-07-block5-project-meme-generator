c47020b940914f270f3f0933580626375f166af6
alvaro <alvarovascon2@gmail.com>
2020-09-11 17:15:34 -0300

    pausa para lanche

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let textInput = document.getElementById('text-input');
-textInput.addEventListener('keyup', function() {
-  let memeText = document.getElementById('meme-text');
+//  escrevendo em meme-image-container
+const textInput = document.getElementById('text-input');
+textInput.addEventListener('keyup', function () {
+  const memeText = document.getElementById('meme-text');
   memeText.innerHTML = textInput.value;
-})
+});
+
+//  colocando imagem em meme-image-container
+// const memeInput = document.getElementById('meme-insert');
+
+// memeInput.addEventListener('onload', function (event) {
+//   let memeImage = document.getElementById('meme-image');
+//   memeImage.src = URL.createObjectURL(event.target.files[0]);
+// });
