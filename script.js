21543d55023f9b662ea0b7feb3bb61e921c10e42
Vladimir <vladimirrchagas@gmail.com>
2020-09-11 18:15:15 -0300

    Verificando requisitos

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ document.querySelector('#text-input').addEventListener('keyup', function () {
 document.querySelector('#meme-insert').addEventListener('change', function (event) {
   const img = document.querySelector('#image');
   img.src = URL.createObjectURL(event.target.files[0]);
-}); /*get code helper on https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977*/
+}); /* get code helper on https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977 */
