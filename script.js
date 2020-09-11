6eb93bdc4157158b55a44fa969a57175756aad8c
hvenses <hvenses@gmail.com>
2020-09-11 17:22:34 -0300

    Alterando a cor do container para branco.

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let caixaTexto = document.querySelector("#text-input");
 let saidaTexto = document.querySelector("#meme-text");
-function insereTexto() {
+
+function insertText() {
   saidaTexto.innerText = caixaTexto.value;
 }
-caixaTexto.addEventListener("keyup", insereTexto);
+
+caixaTexto.addEventListener("keyup", insertText);
+
+let inputImage = document.querySelector("meme-insert");
+let image = document.getElementById("meme-image");
+
+function insertImage(event) {
+  image.src = URL.createObjectURL(event.target.files[0]);
+  document.getElementById("img.loaded").src = URL.createObjectURL(inputImage);
+}
+
+inputImage.addEventListener("change", insertImage);
