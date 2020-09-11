5246977a93beddcb94155953f676b0f2a122b16e
Vladimir <vladimirrchagas@gmail.com>
2020-09-11 18:56:27 -0300

    Verificando requisitos

--- script.js
+++ script.js
@@ -13,41 +13,41 @@ document.querySelector('#fire').addEventListener('click', function () {
 
   container.style.borderColor = 'red';
   container.style.borderStyle = 'dashed';
-  container.style.borderWidth = '5px';
+  container.style.borderWidth = '3px';
 });
 
 document.querySelector('#water').addEventListener('click', function () {
   const container = document.querySelector('.meme-image-container');
 
   container.style.borderColor = 'blue';
-  container.style.borderStyle = 'groove';
-  container.style.borderWidth = '10px';
+  container.style.borderStyle = 'double';
+  container.style.borderWidth = '5px';
 });
 
 document.querySelector('#earth').addEventListener('click', function () {
   const container = document.querySelector('.meme-image-container');
 
-  container.style.borderColor = 'brown';
-  container.style.borderStyle = 'outset';
-  container.style.borderWidth = '15px';
+  container.style.borderColor = 'green';
+  container.style.borderStyle = 'groove';
+  container.style.borderWidth = '6px';
 });
 
-document.querySelector('#src1').addEventListener('click', function () {
+document.querySelector('#meme-1').addEventListener('click', function () {
   const img = document.querySelector('#image');
-  img.src = document.querySelector('#src1').src;
+  img.src = document.querySelector('#meme-1').src;
 });
 
-document.querySelector('#src2').addEventListener('click', function () {
+document.querySelector('#meme-2').addEventListener('click', function () {
   const img = document.querySelector('#image');
-  img.src = document.querySelector('#src2').src;
+  img.src = document.querySelector('#meme-2').src;
 });
 
-document.querySelector('#src3').addEventListener('click', function () {
+document.querySelector('#meme-3').addEventListener('click', function () {
   const img = document.querySelector('#image');
-  img.src = document.querySelector('#src3').src;
+  img.src = document.querySelector('#meme-3').src;
 });
 
-document.querySelector('#src4').addEventListener('click', function () {
+document.querySelector('#meme-4').addEventListener('click', function () {
   const img = document.querySelector('#image');
-  img.src = document.querySelector('#src4').src;
+  img.src = document.querySelector('#meme-4').src;
 });
