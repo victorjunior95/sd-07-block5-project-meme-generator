9fd5a088465426f9b1a5a47f2efba84f84a97bb2
Vitor Rodrigues <vitor_rc@outlook.com>
2020-09-11 16:58:07 -0300

    Ajuste requisito obrigatórios 2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function () {
   let textInput = document.querySelector('#text-input');
   let memeText = document.querySelector('#meme-text');
 
-  textInput.addEventListener('keyup', function () {
+  textInput.addEventListener('input', function () {
     memeText.innerText = textInput.value;
   });
 };
