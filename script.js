30ecbd93e41b1cf2bbde46461d2b5888ecb64360
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-11 15:35:04 -0300

    code climate

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let memeImage = document.getElementById('meme-image');
-let selectImageId = document.getElementById('select-image');
-let divImage = document.getElementById('meme-image-container');
+// let memeImage = document.getElementById('meme-image');
+const selectImageId = document.getElementById('select-image');
+const divImage = document.getElementById('meme-image-container');
+const memeText = document.getElementById('meme-text');
 // memeImage.src = window.URL.createObjectURL(imageSelected);
 
 if (memeImage.src === '') {
-  console.log('não tem imagem')
   divImage.appendChild(selectImageId);
+  memeText.className = "disappear"
 } 
