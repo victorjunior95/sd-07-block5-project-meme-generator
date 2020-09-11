60c947358544caf075fec972be0935aac3fc5832
Pablo Pessanha <pablopessanh@gmail.com>
2020-09-11 18:11:50 -0300

    all request required done

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 let textButton = document.querySelector('#text-button');
-
 textButton.addEventListener('click', () => {
    let text = document.querySelector('#text-input').value;
    let newOutput = document.querySelector('#meme-text');
-
+   
    newOutput.innerText = text;
 });
 
 function loadFile (e) {
-   let output = document.querySelector('#output-meme');
+   let output = document.querySelector('#meme-image');
 
    output.src = URL.createObjectURL(e.target.files[0]);
    output.onload = () => {
