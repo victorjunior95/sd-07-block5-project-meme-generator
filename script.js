8669fa6cc9d4eab272fd8b82f81ae5e90bf64627
Vladimir <vladimirrchagas@gmail.com>
2020-09-11 19:40:00 -0300

    Verificando requisitos

--- script.js
+++ script.js
@@ -12,39 +12,36 @@ document.querySelector('#fire').addEventListener('click', function () {
   const container = document.querySelector('.meme-image-container');
 
   container.style.border = '3px dashed red';
-
 });
 
 document.querySelector('#water').addEventListener('click', function () {
   const container = document.querySelector('.meme-image-container');
 
   container.style.border = '5px double blue';
-
 });
 
 document.querySelector('#earth').addEventListener('click', function () {
   const container = document.querySelector('.meme-image-container');
 
   container.style.border = '6px groove green';
-
 });
 
 document.querySelector('#meme-1').addEventListener('click', function () {
   const img = document.querySelector('#image');
-  img.src = document.querySelector('#meme-1').src;
+  img.src = document.querySelector('#meme-1').firstElementChild.src;
 });
 
 document.querySelector('#meme-2').addEventListener('click', function () {
   const img = document.querySelector('#image');
-  img.src = document.querySelector('#meme-2').src;
+  img.src = document.querySelector('#meme-2').firstElementChild.src;
 });
 
 document.querySelector('#meme-3').addEventListener('click', function () {
   const img = document.querySelector('#image');
-  img.src = document.querySelector('#meme-3').src;
+  img.src = document.querySelector('#meme-3').firstElementChild.src;
 });
 
 document.querySelector('#meme-4').addEventListener('click', function () {
   const img = document.querySelector('#image');
-  img.src = document.querySelector('#meme-4').src;
+  img.src = document.querySelector('#meme-4').firstElementChild.src;
 });
