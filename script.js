ea9e2dc36afa51afde3a576d4918fdcb4c447858
kramersilva <kramerscs@gmail.com>
2020-09-11 19:11:54 -0300

    requisitos 2 e 3

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 window.onload = function() {
   let inputText = document.getElementById('text-input')
   let memeText = document.getElementById('meme-text')
-  let memeImage = document.getElementById('meme-image')
-
   let memeInsert = document.getElementById('meme-insert')
+
   
 
+  memeInsert.addEventListener('change', function(event) {
+    let memeImage = document.getElementById('meme-image')
+    memeImage.src = URL.createObjectURL(event.target.files[0])
+  })
+  
+  
 inputText.addEventListener('keyup', function() {
   memeText.innerText = inputText.value
 })
