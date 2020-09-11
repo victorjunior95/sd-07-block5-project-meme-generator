1172f20324099796cd61c67c7cbf141056fa0038
Emerson Nogueira Junqueira <emoonlit@pop-os.localdomain>
2020-09-11 19:05:20 -0300

    Structuring Elements

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Font: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+let fileImag = function (event) {
+    let output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+        URL.revokeObjectURL(output.src) // free memory
+    }
+};
+
+let textInsid = document.getElementById('text-input');
+textInsid.addEventListener('keyup', function(){
+    document.getElementById('meme-text').innerText = textInsid.value;
+});
+
+let insidMeme = document.getElementById('meme-insert');
+insidMeme.addEventListener('change', fileImag);
