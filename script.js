491c4f05efd732a7ab384d8422582fb31c0f6483
Danwhat <daanwhat@gmail.com>
2020-09-11 16:56:02 -0300

    bolo de cenoura

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let memeTela = document.querySelector("#meme-image-container")
+
 
 let areaTexto = document.querySelector("#text-input")
 areaTexto.addEventListener("input", textoMeme)
@@ -7,3 +7,26 @@ function textoMeme(){
     let memeTelaTexto = document.querySelector("#meme-text")
     memeTelaTexto.innerHTML = areaTexto.value
 }
+
+let inputImg = document.querySelector("#meme-insert")
+inputImg.addEventListener("change",imgUse)
+
+function imgUse(){
+    console.log("funciona carai")
+    let memeTela = document.querySelector("#meme-image-container")
+
+    console.log(inputImg)
+    let memeImage = document.querySelector("#meme-image")
+    memeImage.src = URL.createObjectURL(event.target.files[0])
+}
+
+
+
+
+
+let saidaTexto = document.getElementById('meme-text');
+function trocaTextoA () {
+    
+    saidaTexto.innerText = caixaTexto.value;
+  }
+saidaTexto.addEventListener('click', trocaTextoA);
