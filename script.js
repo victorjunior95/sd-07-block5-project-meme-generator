f75006ce72f2ecd5e732dd1d2009cda96cd3fe53
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-09-11 18:03:36 -0300

    Etrutura 01 pronta

---
+++ script.js
@@ -0,0 +1,3 @@
+let textInput = document.querySelector("#text-input");
+let memeText = document.querySelector("#meme-text");
+textInput.addEventListener("keyup",()=>memeText.innerHTML=textInput.value)
