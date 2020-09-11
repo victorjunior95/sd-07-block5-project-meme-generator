d93f337807a43bc171927a5a58a1a98581555560
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-11 14:00:05 -0300

    Resolvendo issues no codeclimate

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const choseFile = document.querySelector('#meme-insert');
 
-inputText.addEventListener( 'input', function() {
+inputText.addEventListener('input', function () {
   memeText.innerText = inputText.value;
 });
 
-choseFile.addEventListener( 'change', function() {
+choseFile.addEventListener('change', function () {
   document.querySelector('#meme-image').src = window.URL.createObjectURL(choseFile.files[0]);
 });
