e5f6b1288078f9cc7b3554dc10c88f5d7b59a40c
Francisco <franciscoliratorres@gmail.com>
2020-09-11 17:49:05 -0300

    Resolvendo css e refatorando javascript

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let inputText = document.getElementById("text-input");
+
 inputText.addEventListener("input", function(){
 let showText = document.getElementById("meme-text");
 showText.innerHTML = inputText.value;   
-}); 
+});     
 
+let chooseFile = document.getElementById("meme-insert");
 
-
+chooseFile.addEventListener("change", function() {
+    let imgSrc = window.URL.createObjectURL(choseFile.files[0]);
+    document.getElementById("meme-image").src = imgSrc;
+})
 
 
