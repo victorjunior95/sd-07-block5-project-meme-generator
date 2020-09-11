ec6c66398e431dd2023473d8f4930d96d94232b8
Berilo Wanderley <wberilo@gmail.com>
2020-09-11 17:16:12 -0300

    cc

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-window.onload = function (){
+window.onload = function () {
   const input = document.querySelector('#text-input');
   const output = document.querySelector('#meme-text');
   const imgload = document.querySelector('#meme-insert');
   const imgout = document.querySelector('#meme-image');
-  const container = document.querySelector('#meme-image-container')
-  input.addEventListener('input',function (){
+  const container = document.querySelector('#meme-image-container');
+  input.addEventListener('input', function () {
     output.innerText = input.value;
-  })
-  imgload.addEventListener('input',function (){
-    console.log(imgload.value);
-    imgout.src=imgload.value;
   });
-  document.querySelector('#earth').addEventListener('click',function (){
-    container.style.border = '6px groove green'
-    console.log('erf')
-  })
-  document.querySelector('#water').addEventListener('click',function (){
-    container.style.border = '5px double blue'
-  })
-  document.querySelector('#fire').addEventListener('click',function (){
-    container.style.border = '3px dashed red'
-  })
-  const buttons = document.querySelectorAll('.memes')
-  for(let i =0; i < buttons.length; i += 1 ){
-    buttons[i].addEventListener('click',function(){
-      imgout.src = buttons[i].src
-    })
+  imgload.addEventListener('input', function () {
+    console.log(imgload.value);
+    imgout.src = imgload.value;
+  });
+  document.querySelector('#earth').addEventListener('click', function () {
+    container.style.border = '6px groove green';
+    console.log('erf');
+  });
+  document.querySelector('#water').addEventListener('click', function () {
+    container.style.border = '5px double blue';
+  });
+  document.querySelector('#fire').addEventListener('click', function () {
+    container.style.border = '3px dashed red';
+  });
+  const buttons = document.querySelectorAll('.memes');
+  for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener('click', function () {
+      imgout.src = buttons[i].src;
+    });
   }
 };
