3d788474625603327a7f9ce4c2d3cda6a4abdbea
Rodolfo <rodolfojr94@gmail.com>
2020-09-11 21:56:10 -0300

    HTML Approved

---
+++ script.js
@@ -0,0 +1,14 @@
+//A meme-image imagem deve estar totalmente contida dentro do elemento identificado como meme-image-container-->
+//<!--O texto inserido no elemento text-input deve ser inserido sobre a imagem escolhida meme-image.-->  
+/*         <!--Dentro do elemento de container, você deve criar um outro elemento 
+        para mostrar o texto digitado. O elemento de texto deve estar 
+        totalmente contido dentro do container e ter o id denominado meme-text;--> */
+/* 
+        <!--Se não houver imagem inserida, ele deve ser inserido e estar visível
+       dentro do container vazio onde a imagem aparecerá.-->
+      
+       <!--5 - Limite o tamanho do texto que o usuário pode inserir.-->
+        <!--A quantidade máxima de caracteres digitáveis no elemento text-input deve ser 60.--> 
+              <!--Image-->
+      <imag id="meme-image" src="document.querySelector('#meme-insert')">
+        */
