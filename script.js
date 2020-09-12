5096ef37030f8ee6dc2850f20e7eeb03dff2d510
Israel Menon <zezomenon@gmail.com>
2020-09-12 12:01:15 -0300

    Criando botões do requisito bonus nro 6

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let textInput = document.getElementById("text-input");
-textInput.addEventListener("keyup", function() {
-  console.log(document.getElementById("meme-text").innerText = textInput.value);
+let textInput = document.getElementById('text-input');
+textInput.addEventListener('keyup', function () {
+  (document.getElementById('meme-text').innerText = textInput.value);
 });
 
-let insertImage = document.getElementById("meme-insert");
-let memeImage = document.getElementById("meme-image");
-insertImage.addEventListener("change", function() {
+let insertImage = document.getElementById('meme-insert');
+let memeImage = document.getElementById('meme-image');
+insertImage.addEventListener('change', function () {
   memeImage.src = URL.createObjectURL(event.target.files[0]);
-  memeImage.onload = function() {
+  memeImage.onload = function () {
     URL.revokeObjectURL(memeImage.src);
   };
+});
+
+let imgContainer = document.getElementById('meme-image-container');
+let effectButtonFire = document.getElementById('fire');
+effectButtonFire.addEventListener('click', function () {
+  imgContainer.style.border = "3px dashed red";
+});
+
+let effectButtonWater = document.getElementById('water');
+effectButtonWater.addEventListener('click', function () {
+  imgContainer.style.border = "5px double blue";
+});
+
+let effectButtonEarth = document.getElementById('earth');
+effectButtonEarth.addEventListener('click', function () {
+  imgContainer.style.border = "6px groove green";
 });
