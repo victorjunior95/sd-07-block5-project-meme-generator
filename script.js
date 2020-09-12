742f9563aabfcaefb56e33d712fbf17f4cb1a023
Rogerio-feitor <roger.feitor@gmail.com>
2020-09-12 09:09:53 -0300

    Creating alternate borders

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-let textInput = document.querySelector("#text-input");
-let memeText = document.querySelector("#meme-text");
-
+const textInput = document.querySelector("#text-input");
+const memeText = document.querySelector("#meme-text");
 textInput.addEventListener("input", function () {
   memeText.innerHTML = textInput.value;
 });
 
-let memeInsert = document.querySelector("#meme-insert");
-let memeImage = document.querySelector("#meme-image");
-
+const memeInsert = document.querySelector("#meme-insert");
+const memeImage = document.querySelector("#meme-image");
 memeInsert.addEventListener("change", function (event) {
   memeImage.src = URL.createObjectURL(event.target.files[0]);
   memeImage.onload = function () {
     URL.revokeObjectURL(memeImage.src);
   };
+});
+
+const memeBorder = document.querySelector("#meme-image-container");
+
+const  fireButton = document.querySelector("#fire");
+fireButton.addEventListener("click", function () {
+  memeBorder.style.border = "3px dashed red";
+});
+
+const waterButton = document.querySelector("#water");
+waterButton.addEventListener("click", function () {
+  memeBorder.style.border = "5px double blue";
+});
+
+const earthButton = document.querySelector("#earth");
+earthButton.addEventListener("click", function () {
+  memeBorder.style.border = "6px groove green";
 });
 
 
