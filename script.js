5a833de18e3faade9397c85162886fa3303ec646
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-12 13:30:36 -0300

    colocando botões para estilizar borda

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ image.addEventListener ('change', function (event) {
     let imageOutput = document.getElementById('meme-image');
     imageOutput.src = URL.createObjectURL(event.target.files[0]);    
 })
+
+let fireButton = document.getElementById('fire');
+let waterButton = document.getElementById('water');
+let earthButton = document.getElementById('earth');
+let getBorder = document.getElementById('meme-image-container');
+
+fireButton.addEventListener ('click', redBorder);
+waterButton.addEventListener ('click', blueBorder);
+earthButton.addEventListener ('click', greenBorder);
+
+function redBorder() {
+    getBorder.style.border = '3px dashed red';   
+}
+
+function blueBorder() {
+    getBorder.style.border = '5px double blue';   
+}
+
+function greenBorder() {
+    getBorder.style.border = '6px groove green';   
+}
