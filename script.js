9a3279e790af0ac7343909dc9c6890a7d3fc6bd3
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-09-12 14:18:01 -0300

    feito ate o requisito 5

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 window.onload = function(){
+    
     let textInput = document.getElementById('text-input')
     textInput.addEventListener('keyup',function(){
-        document.getElementById('meme-text').innerText = textInput.value
+        document.getElementById('meme-text').innerText = textInput.value;
     })
-    
+    document.querySelector('#meme-insert').addEventListener('change', function (event) {
+        let img = document.querySelector('#image');
+        img.src = URL.createObjectURL(event.target.files[0]);
+      });
+      //https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
 
     
 }
