bb3dc41cd99144fcab3aeeb2e8fd77c80430c038
Raphael Caputo <faelelf2@gmail.com>
2020-09-12 18:41:54 -0300

    Tentando o primeiro entregavel

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-document.querySelector('#text-input').addEventListener("keyup", function()){
+document.querySelector('#text-input').addEventListener("keyup", function(){
   document.querySelector('#meme-text').innerText = document.querySelector('#text-input').nodeValue;
-}
+})
+
