e1c8daad6d6237c75e9cace3c2e54198aa5418cd
marcelosoliveira <msbobsk8@gmail.com>
2020-09-12 16:00:18 -0300

    Todos requisitos feitos

--- script.js
+++ script.js
@@ -41,6 +41,6 @@ for (let index of image) {
     index.addEventListener('click', (event) => {
         let imagens = event.path[0];
         imageMeme.src = imagens.src;
-        console.log(imageMeme.src)
+        console.log(imagens);
     })
 }
