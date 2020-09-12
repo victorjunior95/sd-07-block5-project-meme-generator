56d30a145bdf765f0d3cd5f37a3dc8312f15179d
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-12 15:44:20 -0300

    colocando imagens pré definidas para meme

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Função para inserir o texto do input no container 'meme-image-container'
+
 let text = document.getElementById('text-input');
 
 text.addEventListener ('keyup', insertText);
@@ -8,10 +10,12 @@ function insertText() {
     memeText.innerText = text.value;
 }
 
-let image = document.getElementById('meme-insert');
+//Função para inserir o arquivo(imagem) do input no container 'meme-image-container'
 
-image.addEventListener ('change', function (event) {
-    let imageOutput = document.getElementById('meme-image');
+let image = document.getElementById('meme-insert');
+let imageOutput = document.getElementById('meme-image');
+
+image.addEventListener ('change', function (event) {    
     imageOutput.src = URL.createObjectURL(event.target.files[0]);    
 })
 
@@ -19,6 +23,8 @@ let fireButton = document.getElementById('fire');
 let waterButton = document.getElementById('water');
 let earthButton = document.getElementById('earth');
 let getBorder = document.getElementById('meme-image-container');
+
+//Funções para estilizar a borda do container 'meme-image-container':
 
 fireButton.addEventListener ('click', redBorder);
 waterButton.addEventListener ('click', blueBorder);
@@ -34,4 +40,30 @@ function blueBorder() {
 
 function greenBorder() {
     getBorder.style.border = '6px groove green';   
+}
+
+let meme1 = document.getElementById('meme-1');
+let meme2 = document.getElementById('meme-2');
+let meme3 = document.getElementById('meme-3');
+let meme4 = document.getElementById('meme-4');
+
+meme1.addEventListener('click', showsInTheContainer1);
+meme2.addEventListener('click', showsInTheContainer2);
+meme3.addEventListener('click', showsInTheContainer3);
+meme4.addEventListener('click', showsInTheContainer4);
+
+function showsInTheContainer1(){
+    imageOutput.src = meme1.src;
+}
+
+function showsInTheContainer2(){
+    imageOutput.src = meme2.src;
+}
+
+function showsInTheContainer3(){
+    imageOutput.src = meme3.src;
+}
+
+function showsInTheContainer4(){
+    imageOutput.src = meme4.src;
 }
