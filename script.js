992b63cd33fa526ec57c0f9ba13ac710295df86d
Lorena Góes <goesloren@gmail.com>
2020-09-11 22:17:20 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1) Declare the variables:
+
+let textInput = document.getElementById('text-input');
+let imageInput = document.getElementById('meme-insert');
+let container = document.getElementById('meme-image-container');
+let memeImage = document.getElementById('meme-image');
+let memeText = document.getElementById('meme-text');
+let img = document.querySelector('img');
+
+// 2) Define the functions:
+
+// To load the image file:
+function loadFile(event) {
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
+    memeImage.onload = function() {
+        URL.revokeObjectURL(memeImage.src) // free memory
+    }
+}
+
+// To write the text on the image:
+function writeText() {
+    textInput.setAttribute('maxlength', 60);
+    memeText.innerHTML = textInput.value;
+}
