30a40cb922560ba805fe962f7408a931c091fac8
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-09-12 00:32:46 -0300

    Miniaturas das imagens

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const memeImage = document.createElement('img');
+const memeImages = document.getElementsByClassName('meme-image')[0];
 const button01 = document.querySelector('.button-01');
 const button02 = document.querySelector('.button-02');
 const button03 = document.querySelector('.button-03');
+const imgTip01 = document.getElementById('img-tip-01');
+const imgTip02 = document.getElementById('img-tip-02');
+const imgTip03 = document.getElementById('img-tip-03');
+const imgTip04 = document.getElementById('img-tip-04');
 const memeImageContainer = document.getElementById('meme-image-container');
 textInput.addEventListener('keyup', function() {
   memeText.innerHTML = textInput.value;
