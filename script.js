2d607b3abec11177e62c79949e5a531e36a823fa
hvenses <hvenses@gmail.com>
2020-09-11 21:19:32 -0300

    Alteraçao de parametros de posicionamento do container.

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ caixaTexto.addEventListener("keyup", insertText);
 
 let inputImage = document.querySelector("meme-insert");
 let image = document.getElementById("meme-image");
+
 inputImage.addEventListener("change", insertImage);
+
 function insertImage(event) {
   image.src = URL.createObjectURL(event.target.files[0]);
 }
-window.URL.revokeObjectURL(insertImage);
