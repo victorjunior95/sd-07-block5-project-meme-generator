a87a6e8a8941263e30ab4a24b13a063aaa9d29c9
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-09-12 03:06:24 -0300

    Refatorando JS e comentando entendimento da lógica aprendida...

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const textOutput = document.querySelector('#meme-text');
+// Recupera os elementos HTML por ID.
 const textInput = document.querySelector('#text-input');
+const textOutput = document.querySelector('#meme-text');
+const imgInput = document.getElementById('meme-insert');
 const imgOutput = document.getElementById('meme-image');
 
-function loadImage() {
+// Cria URL única para documento local recebendo no parametro dados do evento onchange.
+function loadImage(event) {
   imgOutput.src = URL.createObjectURL(event.target.files[0]);
+// Assim que os dados do arquivo estiverem carregados a URL é descartada .
   imgOutput.onload = () => {
     URL.revokeObjectURL(imgOutput.src);
   };
 }
-
-const imgInput = document.getElementById('meme-insert');
+// Adiciona escutador de eventos no input da imagem.
 imgInput.addEventListener('change', loadImage);
-
+// Adiciona escutador de eventos no input do texto.
 textInput.addEventListener('keyup', () => {
   textOutput.innerText = textInput.value;
 });
