2af216b677911f55e52c45b1fe9c0cb640584cb7
brendalima <brenda.c.lima@poli.ufrj.br>
2020-09-11 21:25:42 -0300

    Attempt to fix CodeClimate issues

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
-let inputText = document.getElementById("text-input");
-let text = document.getElementById("meme-text");
-let inputImage = document.getElementById("meme-insert");
-let image = document.getElementById("meme-image");
-let container =  document.getElementById("meme-image-container");
-let buttonFire = document.getElementById("fire");
-let buttonWater = document.getElementById("water");
-let buttonEarth = document.getElementById("earth");
-let replacementMemeOne = document.getElementById("meme-1");
-let replacementMemeTwo = document.getElementById("meme-2");
-let replacementMemeThree = document.getElementById("meme-3");
-let replacementMemeFour = document.getElementById("meme-4");
+const inputText = document.getElementById('text-input');
+const text = document.getElementById('meme-text');
+const inputImage = document.getElementById('meme-insert');
+const image = document.getElementById('meme-image');
+const container = document.getElementById('meme-image-container');
+const buttonFire = document.getElementById('fire');
+const buttonWater = document.getElementById('water');
+const buttonEarth = document.getElementById('earth');
+const replacementMemeOne = document.getElementById('meme-1');
+const replacementMemeTwo = document.getElementById('meme-2');
+const replacementMemeThree = document.getElementById('meme-3');
+const replacementMemeFour = document.getElementById('meme-4');
 
 
-inputText.addEventListener("keyup", function() {
-    text.innerText = inputText.value;
+inputText.addEventListener('keyup', function () {
+  text.innerText = inputText.value;
 });
 
-inputImage.addEventListener("change", function(event) {
-    image.src = URL.createObjectURL(event.target.files[0]);
-    image.onload = function() {
-        URL.revokeObjectURL(image.src);
-    }
+inputImage.addEventListener('change', function (event) {
+  image.src = URL.createObjectURL(event.target.files[0]);
+  image.onload = function() {
+    URL.revokeObjectURL(image.src);
+  }
 });
 
-buttonFire.addEventListener("click", function(){
-    container.style.border = "3px dashed red";
+buttonFire.addEventListener('click', function () {
+  container.style.border = '3px dashed red';
 });
 
-buttonWater.addEventListener("click", function(){
-    container.style.border = "5px double blue";
+buttonWater.addEventListener('click', function () {
+  container.style.border = '5px double blue';
 });
 
-buttonEarth.addEventListener("click", function(){
-    container.style.border = "6px groove green";
+buttonEarth.addEventListener('click', function () {
+  container.style.border = '6px groove green';
 });
 
-replacementMemeOne.addEventListener("click", function() {
-    image.src = replacementMemeOne.src;
+replacementMemeOne.addEventListener('click', function () {
+  image.src = replacementMemeOne.src;
 });
 
-replacementMemeTwo.addEventListener("click", function() {
-    image.src = replacementMemeTwo.src;
+replacementMemeTwo.addEventListener('click', function () {
+  image.src = replacementMemeTwo.src;
 });
 
-replacementMemeThree.addEventListener("click", function() {
-    image.src = replacementMemeThree.src;
+replacementMemeThree.addEventListener('click', function () {
+  image.src = replacementMemeThree.src;
 });
 
-replacementMemeFour.addEventListener("click", function() {
-    image.src = replacementMemeFour.src;
+replacementMemeFour.addEventListener('click', function () {
+  image.src = replacementMemeFour.src;
 });
-
-
