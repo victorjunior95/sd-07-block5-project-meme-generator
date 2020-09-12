34705eb835bf173bad6033052180d7ebbd660de3
Felipe Nascimento <flpnascto@gmail.com>
2020-09-12 09:59:37 -0300

    Adicionando requisito bônus 6. Verificando requisito 4.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 window.onload = function () {
   const textInput = document.querySelector('#text-input');
   const memeText = document.querySelector('#meme-text');
-  const memeInsert = document.querySelector('#meme-insert');
-  const memeImage = document.querySelector('#meme-image');
 
   // insere texto digitado no input dentro do container do meme
   textInput.addEventListener('keyup', function () {
@@ -10,6 +8,8 @@ window.onload = function () {
   });
 
   // insere elemento img, com a imagem carregada pelo usurário, dentro co container
+  const memeImage = document.querySelector('#meme-image');
+  const memeInsert = document.querySelector('#meme-insert');
 
   function handleFiles (){
     const img = document.createElement('img');
@@ -28,6 +28,28 @@ window.onload = function () {
       memeImage.replaceChild(img, oldImg);
     }
   }
-
   memeInsert.addEventListener('change', handleFiles, false);
+
+  // Altera borda do meme conforme botão acionado
+  const memeImageContainer = document.querySelector('#meme-image-container');
+  const buttonFire = document.querySelector('#fire');
+  buttonFire.addEventListener('click', function() {
+    memeImageContainer.style.borderColor = 'red';
+    memeImageContainer.style.borderStyle = 'dashed';
+    memeImageContainer.style.borderWidth = '3px';
+  });
+
+  const buttonWater = document.querySelector('#water');
+  buttonWater.addEventListener('click', function() {
+    memeImageContainer.style.borderColor = 'blue';
+    memeImageContainer.style.borderStyle = 'double';
+    memeImageContainer.style.borderWidth = '5px';
+  });
+
+  const buttonEarth = document.querySelector('#earth');
+  buttonEarth.addEventListener('click', function() {
+    memeImageContainer.style.borderColor = 'green';
+    memeImageContainer.style.borderStyle = 'groove';
+    memeImageContainer.style.borderWidth = '6px';
+  });
 };
