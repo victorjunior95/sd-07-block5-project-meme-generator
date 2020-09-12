7e051ed5b004ed8002156fc763f6158ddc352961
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-12 11:26:02 -0300

    Requisite 6 - Adding  memes ready to use

--- script.js
+++ script.js
@@ -13,8 +13,24 @@ var LoadMemeImage = function(event) {
 	image.src = URL.createObjectURL(event.target.files[0]);
 };
 
+function frog() {
+    document.getElementById("meme-image").src = "/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme1.png"
+}
+
+function lol() {
+    document.getElementById("meme-image").src = "/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme2.png"
+}
+
+function homer() {
+    document.getElementById("meme-image").src = "/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme3.png"
+}
+
+function omg() {
+    document.getElementById("meme-image").src = "/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme4.png"
+}
+
 function fire() {
-    document.getElementById("meme-image-container").style.border = "dashed 5px red";
+    document.getElementById("meme-image-container").style.border = "dashed 3px red";
 }
 
 function water() {
