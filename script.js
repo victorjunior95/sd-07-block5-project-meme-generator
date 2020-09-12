4d35a2c7172ab37ebd47d4df3acba4f45120a89d
vini-lima <vini20.henrique@gmail.com>
2020-09-12 20:04:47 -0300

    Alterando o Code Climate 8

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 window.onload = function () {
-  const textMemeIn = document.getElementById('text-input');
-  const chooseImage = document.getElementById('meme-insert');
-  const textMemeOut = document.getElementById('meme-text');
-  const containerImage = document.getElementById('meme-image');
-  let buttonFire = document.getElementById('fire');
-  let buttonWater = document.getElementById('water');
-  let buttonEarth = document.getElementById('earth');
-
+  const textMemeIn = document.getElementById('text-input'); //Texto digitado
+  const chooseImage = document.getElementById('meme-insert'); //Imagem Selecionada
+  const textMemeOut = document.getElementById('meme-text'); //Texto digitado na imagem
+  const containerImage = document.getElementById('meme-image');//Imagem na caixa
+  let bF = document.getElementById('fire'); //bF = buttonFire
+  let bW = document.getElementById('water'); //bW = buttonWater
+  let bE = document.getElementById('earth'); //bE = buttonEarth
   textMemeIn.addEventListener('keyup', function () { textMemeOut.innerText = textMemeIn.value; });
   chooseImage.addEventListener('change', function (event) {
     containerImage.src = URL.createObjectURL(event.target.files[0]);
-    containerImage.onload = function () { URL.revokeObjectURL(containerImage.src) }
+    containerImage.onload = function () { URL.revokeObjectURL(containerImage.src); }
   });  
-    buttonFire.addEventListener('click', function () {
-    buttonFire = document.getElementById('meme-image-container');
-    buttonFire.style.border = '3px dashed red';
+    bF.addEventListener('click', function () {
+    bF = document.getElementById('meme-image-container'); bF.style.border = '3px dashed red';
   });
-  buttonWater.addEventListener('click', function () {
-    buttonWater = document.getElementById('meme-image-container');
-    buttonWater.style.border = '5px double blue';
+  bW.addEventListener('click', function () {
+    bW = document.getElementById('meme-image-container'); bW.style.border = '5px double blue';
   });
-  buttonEarth.addEventListener('click', function () {
-    buttonEarth = document.getElementById('meme-image-container');
-    buttonEarth.style.border = '6px groove green';
+  bE.addEventListener('click', function () {
+    bE = document.getElementById('meme-image-container'); bE.style.border = '6px groove green';
   });
   const buttons = document.querySelectorAll('.memes');
   for (let i = 0; i < buttons.length; i += 1) { buttons[i].addEventListener('click', function () { containerImage.src = buttons[i].src; }); }
