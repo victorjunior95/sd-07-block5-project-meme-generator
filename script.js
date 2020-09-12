6c510f36c1260000bb32eb10d72e9546fe739660
Lets <letsferreiratx@gmail.com>
2020-09-11 21:38:40 -0300

    meme generator

---
+++ script.js
@@ -0,0 +1,31 @@
+let loadFile = function(event) {
+  let memeImage = document.getElementById('meme-image');
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = function() {
+    URL.revokeObjectURL(memeImage.src) // free memory
+  }
+};
+let TextButton = document.querySelector('#text-input');
+TextButton.addEventListener('change', function () {
+  let text = document.getElementById('text-input').value;
+  let textInput = document.getElementById('meme-text');
+  textInput.innerHTML = text;
+});
+
+let memeConteiner = document.querySelector('#meme-image-container');
+
+let fireButton = document.querySelector('#fire');
+fireButton.addEventListener('click', function () {
+  memeConteiner.style.border = '3px dashed red';
+});
+
+let waterButton = document.querySelector('#water');
+waterButton.addEventListener('click', function () {
+  memeConteiner.style.border = '5px double blue';
+});
+
+let earthButton = document.querySelector('#earth');
+earthButton.addEventListener('click', function () {
+  memeConteiner.style.border = '6px groove green';
+});
+
