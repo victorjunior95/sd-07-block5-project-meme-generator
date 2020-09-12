91b106083f03b17df5c900ed7191210964445fd8
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-12 11:01:23 -0300

    ajustes para o codeclimate

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
 const textInput = document.getElementById('text-input');
 const memeInsert = document.getElementById('meme-insert');
 const memeImageContainer = document.getElementById('meme-image-container');
-const memeImage = document.getElementById('meme-image');
 const memeText = document.getElementById('meme-text');
 const image = document.getElementById('image');
 const buttonFire = document.getElementById('fire');
 const buttonWater = document.getElementById('water');
 const buttonEarth = document.getElementById('earth');
-const meme_1 = document.getElementById('meme-1');
-const meme_2 = document.getElementById('meme-2');
-const meme_3 = document.getElementById('meme-3');
-const meme_4 = document.getElementById('meme-4');
-const img_1 = './imgs/meme-1.png';
-const img_2 = './imgs/meme-2.png';
-const img_3 = './imgs/meme-3.png';
-const img_4 = './imgs/meme-4.png';
-
-memeInsert.addEventListener('change', () => { loadFile(memeInsert.files[0]) });
-textInput.addEventListener('input', writeText);
-buttonFire.addEventListener('click', changeBorder);
-buttonWater.addEventListener('click', changeBorder);
-buttonEarth.addEventListener('click', changeBorder);
-meme_1.addEventListener('click', () => { loadFile(img_1) });
-meme_2.addEventListener('click', () => { loadFile(img_2) });
-meme_3.addEventListener('click', () => { loadFile(img_3) });
-meme_4.addEventListener('click', () => { loadFile(img_4) });
-
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
+const img1 = './imgs/meme-1.png';
+const img2 = './imgs/meme-2.png';
+const img3 = './imgs/meme-3.png';
+const img4 = './imgs/meme-4.png';
 
 function loadFile(url) {
   if (typeof url === 'object') {
@@ -40,11 +28,21 @@ function writeText() {
 }
 
 function changeBorder(event) {
-  if (event.target.id == 'fire') {
+  if (event.target.id === 'fire') {
     memeImageContainer.style.border = '3px dashed red';
-  } else if (event.target.id == 'water') {
+  } else if (event.target.id === 'water') {
     memeImageContainer.style.border = '5px double blue';
   } else {
     memeImageContainer.style.border = '6px groove green';
   }
 }
+
+memeInsert.addEventListener('change', () => loadFile(memeInsert.files[0]));
+textInput.addEventListener('input', writeText);
+buttonFire.addEventListener('click', changeBorder);
+buttonWater.addEventListener('click', changeBorder);
+buttonEarth.addEventListener('click', changeBorder);
+meme1.addEventListener('click', () => loadFile(img1));
+meme2.addEventListener('click', () => loadFile(img2));
+meme3.addEventListener('click', () => loadFile(img3));
+meme4.addEventListener('click', () => loadFile(img4));
