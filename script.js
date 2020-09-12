95d910866149e44b09208863e093511516268435
jvitorof <j.vitoroflores@gmail.com>
2020-09-11 22:38:41 -0300

    Commit to the fifth requirement from Trybe Project

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-//1
-const textBox = document.querySelector("#text-input")
-const textPresentation = document.querySelector("#meme-text")
-function showOnImg () {
+//  1
+const textBox = document.querySelector('#text-input');
+const textPresentation = document.querySelector('#meme-text');
+function showOnImg() {
     textPresentation.innerHTML = textBox.value;
 }
 
-textBox.addEventListener("input", showOnImg);
+textBox.addEventListener('input', showOnImg);
 
-//2
-const source = document.querySelector("#meme-insert")
-const image = document.querySelector("#meme-image")
-function showTheImg (event) {
+//  2
+const source = document.querySelector('#meme-insert');
+const image = document.querySelector('#meme-image');
+function showTheImg(event) {
     image.src = URL.createObjectURL(event.target.files[0]);
 }
 
-source.addEventListener("change", showTheImg);
+source.addEventListener('change', showTheImg);
