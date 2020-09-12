0805e91f2347b3643b502be926ef7c602f30be69
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-09-12 14:40:24 -0300

    bonus ex 6v0.5

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ document.querySelector('input#text-input').addEventListener('input', function() 
 //Changing the border.
 let containerBorder = document.querySelector('#meme-image-container');
 document.querySelector('#fire').addEventListener('click', function() {
-  containerBorder.classList.remove = 'default-border';
+  containerBorder.classList.remove('default-border');
   containerBorder.style.border = '3px dashed red';
 })
 
 document.querySelector('#water').addEventListener('click', function() {
-  containerBorder.classList.remove = 'default-border';
+  containerBorder.classList.remove('default-border');
   containerBorder.style.border = '5px double blue';
 
 })
 
 document.querySelector('#earth').addEventListener('click', function() {
-  containerBorder.classList.remove = 'default-border';
+  containerBorder.classList.remove('default-border');
   containerBorder.style.border = '6px groove green';
 })
