323605f79a89196893be3c28b189622f56b7550b
Ana Karine <anakarine.ti@gmail.com>
2020-09-12 03:27:17 -0300

    ajustes requisito 2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Input
-let textInput = document.querySelector('#text-input');
+const textInput = document.querySelector('#text-input');
 
 // Output
-let memeText = document.querySelector('#meme-text');
+const memeText = document.querySelector('#meme-text');
 
 // Function: input --> output
 function inOut() {
@@ -13,7 +13,7 @@ function inOut() {
 textInput.addEventListener('keyup', inOut);
 
 // Reference: https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
-let loadFile = function(event) {
-	let image = document.getElementById('meme-image');
-	image.src = URL.createObjectURL(event.target.files[0]);
+const loadFile = function (event) {
+    const image = document.getElementById('meme-image');
+    image.src = URL.createObjectURL(event.target.files[0]);
 };
