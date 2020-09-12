fb3c3aad84add0bb616c64082dbeb91bd53a74c4
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-09-11 23:36:58 -0300

    bonus 1 (ex 6)

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-function showText() {
-  document.querySelector('#meme-text').innerText =  event.target.value;
-}
+//Getting the input value and placing it in the image.
+document.querySelector('input#text-input').addEventListener('input', function() {
+  document.querySelector('#meme-text').innerText = document.querySelector('input#text-input').value;
+});
+
+//Changing the border.
+let containerBorder = document.querySelector('#meme-image-container');
+let borderTrue;
+document.querySelector('#fire').addEventListener('click', function() {
+  
+  if (!borderTrue) {
+    containerBorder.style.border = '3px dashed red';
+  }
+
+})
+
+document.querySelector('#water').addEventListener('click', function() {
+
+  if (!borderTrue) {
+    containerBorder.style.border = '5px double blue';
+  }
+
+})
+
+document.querySelector('#earth').addEventListener('click', function() {
+
+  if (!borderTrue) {
+    containerBorder.style.border = '6px groove green';
+  }
+})
