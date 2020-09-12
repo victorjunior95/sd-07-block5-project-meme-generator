2bb42867fca000a54abd0964697662578e42d59c
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-09-11 22:24:13 -0300

    Adicionado funcionalidades e estilo CSS.

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-window.onload = () => {
+const output = document.querySelector('#meme-text');
+const input = document.querySelector('#text-input');
 
+function loadFile(event) {
+  let output = document.getElementById('output');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = () => {
+    URL.revokeObjectURL(output.src);
+  };
+}
+
+const insertMemeText = () => {
+  output.innerHTML = '';
+  output.innerText = input.value;
 };
+
+const btnSubmitMemeText = document.querySelector('#btnSubmitMemeText');
+btnSubmitMemeText.addEventListener('click', insertMemeText);
+
+input.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    btnSubmitMemeText.click();
+  }
+});
