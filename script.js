f43c53b77f7e1c72584073e5132a7c5b7e6843bc
vini-lima <vini20.henrique@gmail.com>
2020-09-12 19:52:23 -0300

    Alterando o Code Climate 7

--- script.js
+++ script.js
@@ -9,15 +9,21 @@ window.onload = function () {
 
   textMemeIn.addEventListener('keyup', function () { textMemeOut.innerText = textMemeIn.value; });
   chooseImage.addEventListener('change', function (event) {
-    containerImage.src = URL.createObjectURL(event.target.files[0]); containerImage.onload = function () {
-      URL.revokeObjectURL(containerImage.src); }; });
-  buttonFire.addEventListener('click', function () { buttonFire = document.getElementById('meme-image-container');
-  buttonFire.style.border = '3px dashed red'; });
-  buttonWater.addEventListener('click', function () { buttonWater = document.getElementById('meme-image-container');
-  buttonWater.style.border = '5px double blue'; });
-  buttonEarth.addEventListener('click', function () { buttonEarth = document.getElementById('meme-image-container');
-  buttonEarth.style.border = '6px groove green'; });
-
+    containerImage.src = URL.createObjectURL(event.target.files[0]);
+    containerImage.onload = function () { URL.revokeObjectURL(containerImage.src) }
+  });  
+    buttonFire.addEventListener('click', function () {
+    buttonFire = document.getElementById('meme-image-container');
+    buttonFire.style.border = '3px dashed red';
+  });
+  buttonWater.addEventListener('click', function () {
+    buttonWater = document.getElementById('meme-image-container');
+    buttonWater.style.border = '5px double blue';
+  });
+  buttonEarth.addEventListener('click', function () {
+    buttonEarth = document.getElementById('meme-image-container');
+    buttonEarth.style.border = '6px groove green';
+  });
   const buttons = document.querySelectorAll('.memes');
   for (let i = 0; i < buttons.length; i += 1) { buttons[i].addEventListener('click', function () { containerImage.src = buttons[i].src; }); }
 };
