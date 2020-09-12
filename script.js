1e9f69552d6e1d7e577fbcb201bbf1444c519ed5
Julio Kauer <juliokauer@gmail.com>
2020-09-12 16:14:37 -0300

    Memes prontos

--- script.js
+++ script.js
@@ -3,16 +3,33 @@ window.onload = function () {
   let memeText = document.querySelector("#meme-text");
   let memeInsert = document.querySelector("#meme-insert");
   let memeImage = document.querySelector("#meme-image");
+  let memeImageContainer = document.querySelector("#meme-image-container");
+  let meme1 = document.querySelector("#meme-1");
+  let meme2 = document.querySelector("#meme-2");
+  let meme3 = document.querySelector("#meme-3");
+  let meme4 = document.querySelector("#meme-4");
   function changeText() {
     memeText.innerText = `${textInput.value}`;
   }
   // https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
-  function changeImage() {
-    memeImage.src = URL.createObjectURL(memeInsert.files[0]);
+  function changeImage(event, fileImage) {
+    if (event.target.id === "meme-insert") {
+      memeImage.src = URL.createObjectURL(memeInsert.files[0]);
+    } else {
+      memeImage.src = `${event.target.src}`;
+    }
     memeImage.onload = function () {
-      URL.revokeObjectURL(memeImage.src); // free memory
+      // URL.revokeObjectURL(memeImage.src); // free memory
+      memeImage.style.height = `${memeImage.naturalHeight}px`;
+      memeImage.style.width = `${memeImage.naturalWidth}px`;
+      memeImageContainer.style.height = `${memeImage.naturalHeight}px`;
+      memeImageContainer.style.width = `${memeImage.naturalWidth}px`;
     };
   }
   textInput.addEventListener("input", changeText);
-  memeInsert.addEventListener("change",changeImage);
+  memeInsert.addEventListener("change", changeImage);
+  meme1.addEventListener("click", changeImage);
+  meme2.addEventListener("click", changeImage);
+  meme3.addEventListener("click", changeImage);
+  meme4.addEventListener("click", changeImage);
 };
