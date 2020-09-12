bd0fb75c49bb1576eafc30f3c729fd5cab69d9c1
Elano Evaristo <elano3004@gmail.com>
2020-09-11 23:31:47 -0300

    primeiras atualizações

---
+++ script.js
@@ -0,0 +1,21 @@
+const textInput = document.getElementById('text-input');
+const memeInsert = document.getElementById('meme-insert');
+
+const memeText = document.getElementById('meme-text');
+const memeImage = document.getElementById('meme-image');
+
+function limpaObjectURL() {
+  URL.revokeObjectURL(memeImage.src);
+}
+
+function trocaTexto() {
+  memeText.innerText = textInput.value;
+}
+
+function trocaImagem() {
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = limpaObjectURL;
+}
+
+textInput.addEventListener('keyup', trocaTexto);
+memeInsert.addEventListener('change', trocaImagem);
