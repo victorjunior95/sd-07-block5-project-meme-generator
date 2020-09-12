080e3bf0ac9b26142dbb9560b4214cece67a693a
moacyrrln <moacyrrln@gmail.com>
2020-09-12 05:42:57 -0300

    teste12

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let img = document.getElementById("meme-image");
 let input = document.getElementById("meme-insert");
 let text = document.getElementById("text-input");
 let memetext = document.getElementById("meme-text");
+let fire = document.getElementById("fire");
+let water = document.getElementById("water");
+let earth = document.getElementById("earth");
+let container = document.getElementById("meme-image-container");
 
 input.addEventListener('change', function() {
 	
@@ -15,4 +19,16 @@ input.addEventListener('change', function() {
 text.addEventListener("keypress", function() {
 
     memetext.innerText = text.value;
+})
+
+fire.addEventListener("click", function() {
+    container.className = "fireborder";
+})
+
+water.addEventListener("click", function() {
+    container.className = "waterborder";
+})
+
+earth.addEventListener("click", function() {
+    container.className = "earthborder";
 })
