66cdbf9fa7d2806f81e004bd3bea942141bbe7ac
Carol Andrade <a.carolg1@gmail.com>
2020-09-12 13:43:38 -0300

    requisito6 sub1

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-window.onload = function () {
-  document.querySelector('#text-input').setAttribute('maxlength', 60);
+//  Trata evento de inserção de texto sobre a imagem
+document.querySelector('#text-input').setAttribute('maxlength', 60);
   const textInputBox = document.querySelector('#text-input');
   textInputBox.addEventListener('keyup', function () {
     const textInput = document.querySelector('#text-input').value;
     const textMeme = document.querySelector('#meme-text');
     textMeme.innerText = textInput;
   });
+//  Trata evento de inserção de imagem pelo usuário
   const memeImage = document.querySelector('#meme-image');
   const memeInput = document.querySelector('#meme-insert');
   function upLoadImg(event) {
     memeImage.src = URL.createObjectURL(event.target.files[0]);
   }
   memeInput.addEventListener('change', upLoadImg);
-};
+// Trata evento de trocar a borda do conteiner conforme usuário escolhe
+let imageBox = document.querySelector('#meme-image-container')
+let btnFire = document.querySelector('#fire');
+btnFire.addEventListener('click', function() {
+  imageBox.style.border = 'dashed 3px  red';
+});
+let btnWater = document.querySelector('#water');
+btnWater.addEventListener('click', function () {
+  imageBox.style.border = 'double 5px  blue';
+});
+let btnEarth = document.querySelector('#earth');
+btnEarth.addEventListener('click', function () {
+  imageBox.style.border = 'groove 6px  green';
+});
+
