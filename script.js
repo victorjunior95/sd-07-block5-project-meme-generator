3d41c023d0c62518491f6fceb8677848dbf19548
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-09-12 01:47:32 -0300

    Code climate

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const textOutput = document.querySelector('#meme-text');
 const textInput = document.querySelector('#text-input');
 
-function loadFile(event) {
-  const imgOutput = document.getElementById('meme-image');
+const imgOutput = document.getElementById('meme-image');
+
+function loadFile() {
   imgOutput.src = URL.createObjectURL(event.target.files[0]);
-  imgOutput.onload = () => {
-    URL.revokeObjectURL(imgOutput.src);
-  };
 }
 
 textInput.addEventListener('keyup', () => {
