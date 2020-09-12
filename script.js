95528716a10efa891d8dd2bfffeb451ff80409bd
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-11 22:37:45 -0300

    requisito 7.2 adicionando arquivos deletados

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ let memeContainer = document.querySelector("#meme-image-container");
 let fire = document.querySelector("#fire");
 let water = document.querySelector("#water");
 let earth = document.querySelector("#earth");
-
+let meme1 = document.querySelector("#meme-1");
+let meme2 = document.querySelector("#meme-2");
+let meme3 = document.querySelector("#meme-3");
+let meme4 = document.querySelector("#meme-4");
 
 textInput.addEventListener("keyup", function() {
     memeText.innerHTML = textInput.value;
@@ -30,5 +33,21 @@ earth.addEventListener("click", function() {
     memeContainer.style.border = "6px groove rgb(0, 128 ,0)"
 });
 
+meme1.addEventListener("click", function() {
+    memeImg.src = meme1.src;
+    memeText.innerHTML = "Depois de 15 horas codando";
+});
+meme2.addEventListener("click", function() {
+    memeImg.src = meme2.src;
+    memeText.innerHTML = "Zerei as issues do code climate";
+});
+meme3.addEventListener("click", function() {
+    memeImg.src = meme3.src;
+    memeText.innerHTML = "Eu depois de formar na trybe";
+});
+meme4.addEventListener("click", function() {
+    memeImg.src = meme4.src;
+    memeText.innerHTML = "Lucas pede para eu não invadir as aulas, pois já formei. EU:";
+});
 
 
