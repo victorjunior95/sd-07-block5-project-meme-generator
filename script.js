e17f84def54d90dda82906b08f9178ec6aa8c15b
Francisco <franciscoliratorres@gmail.com>
2020-09-12 19:39:09 -0300

    tentando passar no corretor de novo

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let inputText = document.getElementById("text-input");
+let inputText = document.querySelector("#text-input");
 
 inputText.addEventListener("input", function(){
-let showText = document.getElementById("meme-text");
+let showText = document.querySelector("#meme-text");
 showText.innerHTML = inputText.value;   
 });     
 
 let loadFile = function(event) {
-    var output = document.getElementById("meme-image")
+    var output = document.querySelector("#meme-image")
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function () {
         URL.revokeObjectURL(output.src);
 }
 }
 
-let chooseFile = document.getElementById("meme-insert");
+let chooseFile = document.querySelector("#meme-insert");
 chooseFile.addEventListener('change', loadFile);
 
-let buttonFire = getElementById("fire");
-let buttonWater = getElementById("water");
-let buttonEarth = getElementById("earth");
-let memeImageContainer = getElementById("meme-image-container")
+let buttonFire = querySelector("#fire");
+let buttonWater = querySelector("#water");
+let buttonEarth = gquerySelector("#earth");
+let memeImageContainer = gquerySelector("#meme-image-container")
 
 buttonFire.addEventListener("click", function () {
     memeImageContainer.style.border = "3px dashed red";
