a6ead972140853c351026b0363933973e1bc9f41
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-12 17:24:40 -0400

    correções codeclimate

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-window.onload = function() {
-  
-  //Função para colocar foto no meme
-  let carregaFoto = document.getElementById('insere-foto'); 
+window.onload=function () {
+  // Função para colocar foto no meme
+  const carregaFoto=document.getElementById('insere-foto'); 
   carregaFoto.addEventListener('input', function(event) {
-    let url = URL.createObjectURL(event.target.files[0]);
-    let saida = document.getElementById('imagem-meme');
-    saida.src = url;
+    const url=URL.createObjectURL(event.target.files[0]);
+    const saida=document.getElementById('imagem-meme');
+    saida.src=url;
   });
-
   // Função para colocar texto no meme
-  let lbltexto = document.getElementById('text-input');
-  lbltexto.addEventListener('change', function() {
-    let texto = document.getElementById('text-input').value;
+  const lbltexto=document.getElementById('text-input');
+  lbltexto.addEventListener('change', function () {
+    const texto=document.getElementById('text-input').value;
     document.getElementById('texto-meme').innerHTML = texto;
   });
-  
 }
