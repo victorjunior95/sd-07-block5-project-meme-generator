251ce8b5b0ceb0386ad8672efab7e28e427492ab
luc-zago <luc.zago@hotmail.com>
2020-09-12 10:57:36 -0300

    Alterada função 'memeText' para que o texto do meme fique em caixa alta

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function() {
 document.getElementById('txt-input').addEventListener('keyup', memeTxt);
 
 function memeTxt() {
-    memeText = document.getElementById('txt-input').value;
+    memeText = document.getElementById('txt-input').value.toUpperCase();
     return document.getElementById('meme-text').innerText = memeText;
 }
 }
