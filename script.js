4dba904649d3abe170e7e7ef62e785d6ec4da492
josielvs <josielvs@gmail.com>
2020-09-12 02:04:32 -0300

    Atualizando o requisito 1. Recomeçando com JS

--- script.js
+++ script.js
@@ -1,55 +1,17 @@
-window.onload = function () {
-function createReader () {
-    let headerPage = document.createElement('header')
-    headerPage.className = 'head-class'
-    document.body.appendChild(headerPage)
-
-    let titleProject = document.createElement('h1')
-    titleProject.innerHTML = "MEME GENERATOR"
-    titleProject.className = 'title-class'
-    titleProject.setAttribute('id', 'title')
-    headerPage.appendChild(titleProject)
+function updateText(text) {
+    document.querySelector('#meme-image-container p').innerText = text.target.value;
 }
-createReader()
+  
+function updatePhoto(files) {
+    const img = document.querySelector('#meme-image');
+    const objectURL = window.URL.createObjectURL(files[0]);
+    img.src = objectURL;
+    img.onload = function () {
+      window.URL.revokeObjectURL(this.src);
+  };
+} 
 
-function createSectionInput () {
-    let sectionContainerLeft = document.createElement('section')
-    sectionContainerLeft.className = 'container-left'
-    document.body.appendChild(sectionContainerLeft)
-    
-    let inputLabel = document.createElement('label')
-    inputLabel.setAttribute('for', 'text-input')
-    inputLabel.innerHTML = 'Insira o texto ao MEME: '
-    sectionContainerLeft.appendChild(inputLabel)
-
-    let imputText = document.createElement('input')
-    imputText.setAttribute('id', 'text-input')
-    sectionContainerLeft.appendChild(imputText)
-}
-createSectionInput()
-
-function creatSectionBody () {
-    let sectionContainerRight = document.createElement('section')
-    sectionContainerRight.className = 'container-right'
-    sectionContainerRight.setAttribute('id', 'meme-image-container')
-    document.body.appendChild(sectionContainerRight)
-
-    let src = ''
-    let imageMEME = document.createElement('img')
-    imageMEME.setAttribute('src', src)
-    sectionContainerRight.appendChild(imageMEME)
-
-    let paragraphMEME = document.createElement('p')
-    paragraphMEME.setAttribute('id', 'meme-text')
-    paragraphMEME.innerHTML = 'Sua frase!!!'
-}
-creatSectionBody()
-
-    const textInput = document.getElementById("text-input");
-    const memeText = document.getElementById("meme-text");
-    const memeImage = document.getElementById("meme-image");
-
-    textInput.addEventListener("input", function () {
-    memeText.innerText = textInput.value;
-})
-}
+document.body.onload = function () {
+  document.querySelector('#text-input').addEventListener('input', updateText);
+  document.querySelector('#meme-insert').addEventListener('change', (e) => updatePhoto(e.target.files));
+};
