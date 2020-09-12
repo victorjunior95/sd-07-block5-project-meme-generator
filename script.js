50161095289726052cd379accf81abb0dc938535
vini-lima <vini20.henrique@gmail.com>
2020-09-11 23:36:47 -0300

    Imagem e Texto do Meme

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 window.onload = function() {
-  let textMeme;
-  let chooseFile;
-
-  function textMemeImage() {
-    
+  let textMeme = document.querySelectorAll('#text-input');
+  let chooseImage = document.querySelectorAll('#input-image');
+  
+  texteMeme.addEventListener('click', function textDisplay(event) {
+    let textBox = document.getElementById('meme-text');
+    textBox.innerHTML = textMeme;
+  });
+  
+  containerImage.onload = function() {
+    URL.revokeObjectURL(containerImage.src);
   }
+
+  chooseImage.addEventListener('change', function(event) {
+    let containerImage = document.getElementById('meme-image');
+    containerImage.src = URL.createObjectURL(event.target.files[0]);
+  });
+
+  
 
 
 
