1086ab80c9a22291bd1ba9ea35aa6a4b5adc2351
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-12 10:01:31 -0300

    Requisite 6 - Customizing the border style

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ var LoadMemeImage = function(event) {
 	var image = document.getElementById('meme-image');
 	image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+function fire() {
+    document.getElementById("meme-image-container").style.border = "dashed 5px red";
+}
+
+function water() {
+    document.getElementById("meme-image-container").style.border = "double 5px blue";
+}
+
+function earth() {
+    document.getElementById("meme-image-container").style.border = "groove 6px green";
+}
