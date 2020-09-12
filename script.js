b75554a93b22eddcf5e96101d451ca97f6ffe9d3
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-09-12 11:32:15 -0300

    Code Climate

--- script.js
+++ script.js
@@ -2,49 +2,49 @@ const text = document.querySelector('#text-input');
 
 
 text.addEventListener('keyup', function () {
-  let textInput = document.querySelector('#text-input').value;
-  let memeText = document.querySelector('#meme-text');
+  const textInput = document.querySelector('#text-input').value;
+  const memeText = document.querySelector('#meme-text');
   memeText.innerText = textInput;
 });
 const buttonFire = document.querySelector('#fire');
 buttonFire.addEventListener("click", function () {
 
-  let backGroundCont = document.querySelector('#meme-image-container');
+  const backGroundCont = document.querySelector('#meme-image-container');
   backGroundCont.style.border = '3px red dashed';
 });
 
 const buttonWater = document.querySelector('#water');
 buttonWater.addEventListener('click', function () {
-  let backGroundCont = document.querySelector('#meme-image-container');
+  const backGroundCont = document.querySelector('#meme-image-container');
   backGroundCont.style.border = '5px blue double';
 });
 
 const buttonEarth = document.querySelector('#earth');
 buttonEarth.addEventListener('click', function () {
-  let backGroundCont = document.querySelector('#meme-image-container');
+  const backGroundCont = document.querySelector('#meme-image-container');
   backGroundCont.style.border = '6px green groove';
 });
 
 const meme1 = document.querySelector('#meme-1');
 meme1.addEventListener('click', function () {
-  let imageMeme = document.querySelector('#meme-image');
+  const imageMeme = document.querySelector('#meme-image');
   imageMeme.src = meme1.src;
 });
 
 const meme2 = document.querySelector('#meme-2');
 meme2.addEventListener('click', function () {
-  let imageMeme = document.querySelector('#meme-image');
+  const imageMeme = document.querySelector('#meme-image');
   imageMeme.src = meme2.src;
 });
 
 const meme3 = document.querySelector('#meme-3');
 meme3.addEventListener('click', function () {
-  let imageMeme = document.querySelector('#meme-image');
+  const imageMeme = document.querySelector('#meme-image');
   imageMeme.src = meme3.src;
 });
 
 const meme4 = document.querySelector('#meme-4');
 meme4.addEventListener('click', function () {
-  let imageMeme = document.querySelector('#meme-image');
+  const imageMeme = document.querySelector('#meme-image');
   imageMeme.src = meme4.src;
 });
