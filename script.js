0993b9dfff6353349dab51d5e0382cf56ce98321
josielvs <josielvs@gmail.com>
2020-09-12 13:52:50 -0300

    Requisito 2 - Atualizando o CSS.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function () {   
+window.onload = function () {  
     function createReader () {
         let headerPage = document.createElement('header')
         headerPage.className = 'head-class'
