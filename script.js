85875ff3de985e73aee0c61d0a0ea6d0396bcfe6
vini-lima <vini20.henrique@gmail.com>
2020-09-12 01:45:59 -0300

    Arrumando fundo da div

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-window.onload = function() {
-  let textMemeIn = document.getElementById('text-input');
-  let chooseImage = document.getElementById('input-image');
-  let textMemeOut = document.getElementById('meme-text');
-  let containerImage = document.getElementById('meme-image');
-  
-  textMemeIn.addEventListener('keyup', function() {
-    textMemeOut.innerText = textMemeIn.value; });
-  
-  chooseImage.addEventListener('change', function(event) {
+window.onload = function () {
+  const textMemeIn = document.getElementById('text-input');
+  const chooseImage = document.getElementById('meme-insert');
+  const textMemeOut = document.getElementById('meme-text');
+  const containerImage = document.getElementById('meme-image');
+  textMemeIn.addEventListener('keyup', function () {
+    textMemeOut.innerText = textMemeIn.value;
+  });
+  chooseImage.addEventListener('change', function (event) {
     containerImage.src = URL.createObjectURL(event.target.files[0]);
-    containerImage.onload = function() {
-        URL.revokeObjectURL(containerImage.src); }}); 
-
+    containerImage.onload = function () {
+      URL.revokeObjectURL(containerImage.src);
+  }}); 
 }
