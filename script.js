1cc42e0ea6c1661efd755296c886d35dbd4a32cb
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-09-12 17:06:07 -0300

    ajuste container

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onload = function(){
         document.getElementById('meme-text').innerText = textInput.value;
     })
     document.querySelector('#meme-insert').addEventListener('change', function (event) {
-        let img = document.querySelector('#image');
+        let img = document.querySelector('#meme-image');
         img.src = URL.createObjectURL(event.target.files[0]);
       });
       //https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
