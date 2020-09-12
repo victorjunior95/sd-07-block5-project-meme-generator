7b3af9098610533d4d15d915a0b9436cbc849f71
Thalys <thalysmatiascarrara@gmail.com>
2020-09-12 16:20:11 -0400

    requisito 2

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let memeText = document.getElementById('meme-text')
-let inputText = document.getElementById('text-input')
+const memeText = document.getElementById('meme-text')
+const inputText = document.getElementById('text-input')
+const insertFile = document.getElementById('meme-insert')
+const memeImage = document.getElementById('meme-image')
 console.log(inputText.value)
 
 inputText.addEventListener("keyup",function(){
     memeText.innerText = inputText.value
+})
+
+insertFile.addEventListener("change",function(event){
+    let file = event.target.files[0];
+    let url = URL.createObjectURL(file);
+    memeImage.src = url;
 })
