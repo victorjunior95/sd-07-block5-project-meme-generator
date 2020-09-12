cd765dba72d524a1d7a3813826b7ae98e2273a52
Filipi Firmino <contatoic3web@gmail.com>
2020-09-12 18:34:19 -0300

    Ações dos botões prontas

--- script.js
+++ script.js
@@ -3,14 +3,12 @@
 let text = document.querySelector('#text-input');
 let memeText = document.querySelector('#meme-text');
 
-text.addEventListener('change', function (){
+text.addEventListener('keyup', function (){
     memeText.innerText=text.value;
 });
 
 //Image for meme
 let inputPicuture = document.querySelector('#meme-insert');
-
-
 inputPicuture.addEventListener('change', function(){
   let picture = document.querySelector('#meme-image');
   picture.className = 'imgMeme';
@@ -20,3 +18,19 @@ inputPicuture.addEventListener('change', function(){
   }
 });
 
+//Action buttons
+const container = document.getElementById('meme-image-container')
+const fire = document.querySelector('#fire');
+fire.addEventListener('click', function () {
+  container.style.border = "3px dashed red";
+})
+
+const water = document.querySelector('#water');
+water.addEventListener('click', function (){
+  container.style.border = "5px double blue";
+})
+
+const earth = document.querySelector('#earth');
+earth.addEventListener('click', function(){
+  container.style.border = "6px groove green";
+})
