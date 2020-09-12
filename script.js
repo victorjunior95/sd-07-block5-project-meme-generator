df63d5229a2290b3968f0175b7c43b357f2608ad
jvitorof <j.vitoroflores@gmail.com>
2020-09-12 02:38:16 -0300

    Seventh requirement to meme generator project

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function showTheImg(event) {
 
 source.addEventListener('change', showTheImg);
 
-//  7
+//  6
 const butFire = document.querySelector('#fire');
 const butWater = document.querySelector('#water');
 const butEarth = document.querySelector('#earth');
@@ -32,6 +32,34 @@ butWater.addEventListener('click',function() {
 
 butEarth.addEventListener('click',function() {
     container.style.border = '6px groove green';
+});
+
+
+//  7
+const meme1 = document.querySelector('#meme-1');
+const meme2 = document.querySelector('#meme-2');
+const meme3 = document.querySelector('#meme-3');
+const meme4 = document.querySelector('#meme-4');
+const meme5 = document.querySelector('#meme-5');
+
+meme1.addEventListener('click', function() {
+    image.src = meme1.src
+});
+
+meme2.addEventListener('click', function() {
+    image.src = meme2.src
+});
+
+meme3.addEventListener('click', function() {
+    image.src = meme3.src
+});
+
+meme4.addEventListener('click', function() {
+    image.src = meme4.src
+});
+
+meme5.addEventListener('click', function() {
+    image.src = meme5.src
 });
 
 
