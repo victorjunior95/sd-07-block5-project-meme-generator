04a7416317017b25b5d3bd7a1f95c1b6892c913f
vini-lima <vini20.henrique@gmail.com>
2020-09-12 18:09:41 -0300

    Alterando Code Climate 3

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ window.onload = function () {
   chooseImage.addEventListener('change', function (event) {
     containerImage.src = URL.createObjectURL(event.target.files[0]);
     containerImage.onload = function () { URL.revokeObjectURL(containerImage.src); }; });
-  buttonFire.addEventListener('click', function () { 
+  buttonFire.addEventListener('click', function () {
     buttonFire = document.getElementById('meme-image-container');
     buttonFire.style.border = '3px dashed red'; });
-  buttonWater.addEventListener('click', function () { 
+  buttonWater.addEventListener('click', function () {
     buttonWater = document.getElementById('meme-image-container');
     buttonWater.style.border = '5px double blue'; });
-  buttonEarth.addEventListener('click', function () { 
+  buttonEarth.addEventListener('click', function () {
     buttonEarth = document.getElementById('meme-image-container');
     buttonEarth.style.border = '6px groove green'; });
-  
+
     const buttons = document.querySelectorAll('.memes');
   for (let i = 0; i < buttons.length; i += 1) { buttons[i].addEventListener('click', function () { containerImage.src = buttons[i].src; }); }
 };
