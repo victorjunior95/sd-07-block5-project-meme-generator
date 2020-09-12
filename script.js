6520a195420cc6439e92ede2f2329e9f30f45c2b
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-09-12 19:10:24 -0300

    Lutando Code Climate 02 12/09/2020

--- script.js
+++ script.js
@@ -1,69 +1,68 @@
 // MY CONSTANTS
-const containerMeme = document.querySelector("#meme-image-container");
-const img = document.querySelector("#meme-image");
-const loadImg = document.querySelector("#meme-insert");
-const textInput = document.querySelector("#text-input");
-const loadText = document.querySelector("#meme-text");
-const buttonRed = document.querySelector("#fire");
-const buttonBlue = document.querySelector("#water");
-const buttonBrown = document.querySelector("#earth");
+const containerMeme = document.querySelector('#meme-image-container');
+const img = document.querySelector('#meme-image');
+const loadImg = document.querySelector('#meme-insert');
+const textInput = document.querySelector('#text-input');
+const loadText = document.querySelector('#meme-text');
+const buttonRed = document.querySelector('#fire');
+const buttonBlue = document.querySelector('#water');
+const buttonBrown = document.querySelector('#earth');
 
 // CONDITION TO CHANGE THE DISPLAY CONTAINER CSS
 let imgShow = false;
 if (imgShow === false) {
-  containerMeme.style.display = "none";
+  containerMeme.style.display = 'none';
 }
 
 // EVENT TO SELECT A IMAGE IN MY CONTAINER
-loadImg.addEventListener("change", selectImg);
-
 function selectImg(event) {
   img.src = URL.createObjectURL(event.target.files[0]);
   imgShow = true;
   if (imgShow === true) {
-    containerMeme.style.display = "block";
+    containerMeme.style.display = 'block';
   }
 }
+loadImg.addEventListener('change', selectImg);
 
 // EVENT TO ADD TEXT IN MY CONTAINER
-textInput.addEventListener("keyup", addText);
+textInput.addEventListener('keyup', addText);
 
 function addText() {
   loadText.innerText = textInput.value;
   imgShow = true;
   if (imgShow === true) {
-    containerMeme.style.display = "block";
+    containerMeme.style.display = 'block';
   }
 }
 
 // EVENT IN THE BUTTONS BORDERS
-buttonRed.addEventListener("click", changeBorderFire);
+buttonRed.addEventListener('click', changeBorderFire);
 
 function changeBorderFire() {
-  containerMeme.style.border = "3px dashed red";
+  containerMeme.style.border = '3px dashed red';
 }
 
-buttonBlue.addEventListener("click", changeBorderWater);
+buttonBlue.addEventListener('click', changeBorderWater);
 
 function changeBorderWater() {
-  containerMeme.style.border = "5px double blue";
+  containerMeme.style.border = '5px double blue';
 }
 
-buttonBrown.addEventListener("click", changeBorderEarh);
+buttonBrown.addEventListener('click', changeBorderEarh);
 
 function changeBorderEarh() {
-  containerMeme.style.border = "6px groove green";
+  containerMeme.style.border = '6px groove green';
 }
 
-const preImgs = document.querySelectorAll(".miniatureImg");
+const preImgs = document.querySelectorAll('.miniatureImg');
 for (let i = 0; i < preImgs.length; i += 1) {
-  preImgs[i].addEventListener("click", currentImg);
+  preImgs[i].addEventListener('click', currentImg);
 
   function currentImg() {
     img.src = preImgs[i].src;
     imgShow = true;
     if (imgShow === true) {
-      containerMeme.style.display = "block";
+      containerMeme.style.display = 'block';
     }
   }
 }
