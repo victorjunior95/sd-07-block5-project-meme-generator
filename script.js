75dbe7708ad34bd2b8b598d67324d3ac2a3a2bb7
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-12 10:23:03 -0300

    codeclimate spaces in functions

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 const textInput = document.querySelector('.text-input');
 const memeText = document.querySelector('.meme-text');
 const inputImgMeme = document.querySelector('.meme-insert');
-let memeImg = document.querySelector('.meme-image');
-let memeContainer = document.querySelector('.meme-image-container');
-let fire = document.querySelector('.fire');
-let water = document.querySelector('.water');
-let earth = document.querySelector('.earth');
-let meme1 = document.querySelector('.meme-1');
-let meme2 = document.querySelector('.meme-2');
-let meme3 = document.querySelector('.meme-3');
-let meme4 = document.querySelector('.meme-4');
+const memeImg = document.querySelector('.meme-image');
+const memeContainer = document.querySelector('.meme-image-container');
+const fire = document.querySelector('.fire');
+const water = document.querySelector('.water');
+const earth = document.querySelector('.earth');
+const meme1 = document.querySelector('.meme-1');
+const meme2 = document.querySelector('.meme-2');
+const meme3 = document.querySelector('.meme-3');
+const meme4 = document.querySelector('.meme-4');
 
-textInput.addEventListener("keyup", function() {
+textInput.addEventListener("keyup", function () {
     memeText.innerHTML = textInput.value;
 });
 
-inputImgMeme.addEventListener("change", function() {
-    memeImg.src = URL.createObjectURL(event.target.files[0]);
-    memeImg.onload = function() {
-        URL.revokeObjectURL(memeImg.src)
-    }
+inputImgMeme.addEventListener("change", function () {
+  memeImg.src = URL.createObjectURL(event.target.files[0]);
+  memeImg.onload = function() {
+      URL.revokeObjectURL(memeImg.src);
+    };
 });
 
-fire.addEventListener("click", function() {
-    memeContainer.style.border =  "3px dashed rgb(255, 0, 0)"
+fire.addEventListener("click", function () {
+  memeContainer.style.border = '3px dashed rgb(255, 0, 0)';
 });
-water.addEventListener("click", function() {
-    memeContainer.style.border =  "5px double rgb(0, 0, 255)"
+water.addEventListener("click", function () {
+  memeContainer.style.border =  '5px double rgb(0, 0, 255)';
 });
-earth.addEventListener("click", function() {
-    memeContainer.style.border = "6px groove rgb(0, 128 ,0)"
+earth.addEventListener("click", function () {
+  memeContainer.style.border = '6px groove rgb(0, 128 ,0)';
 });
 
-meme1.addEventListener("click", function() {
-    memeImg.src = meme1.src;
-    memeText.innerHTML = "Depois de 15 horas codando";
+meme1.addEventListener("click", function () {
+  memeImg.src = meme1.src;
+  memeText.innerHTML = 'Depois de 15 horas codando';
 });
-meme2.addEventListener("click", function() {
-    memeImg.src = meme2.src;
-    memeText.innerHTML = "Zerei as issues do code climate";
+meme2.addEventListener("click", function () {
+  memeImg.src = meme2.src;
+  memeText.innerHTML = 'Zerei as issues do code climate';
 });
-meme3.addEventListener("click", function() {
-    memeImg.src = meme3.src;
-    memeText.innerHTML = "Eu depois de formar na trybe";
+meme3.addEventListener("click", function () {
+  memeImg.src = meme3.src;
+  memeText.innerHTML = 'Eu depois de formar na trybe';
 });
-meme4.addEventListener("click", function() {
-    memeImg.src = meme4.src;
-    memeText.innerHTML = "Lucas pede para eu não invadir as aulas, pois já formei. EU:";
+meme4.addEventListener("click", function () {
+  memeImg.src = meme4.src;
+  memeText.innerHTML = 'Lucas pede para eu não invadir as aulas, pois já formei. EU:';
 });
 
 
