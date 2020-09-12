fb02b3387668375200d879760a3b401357e1a8fa
MoisesSantana <moisaant@gmail.com>
2020-09-12 09:29:27 -0300

    botoes de borda

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 let message = document.querySelector('#text-input');
 message.addEventListener('keyup', function() {
   let text = message.value;
-  console.log(text);
   printMessage(text);
 });
 
@@ -23,18 +22,30 @@ inputImage.addEventListener('change', function(event) {
   inputImage = inputImage.value;
 
   let image = document.querySelector('#meme-image');
+
   // https://developer.mozilla.org/pt-BR/docs/Web/API/URL/createObjectURL#Syntax <<< Cria um objeto URL(event.targe) vai me retornar caminho do evento change .files me retorna um array de arquivos, o selecionado é o primeiro do array
   
   image.src = URL.createObjectURL(event.target.files[0]);
-  changeSizeContainer(image);
 });
 
-// Alterando o tamanho do container
+// alterando bordas
 
-function changeSizeContainer(image) {
-  let container = document.querySelector('#meme-image-container');
-  console.log(container.style.whidth);
-  console.log(image.setoffWidth);
-  container.style.whidth = image.setoffWidth;
-  container.style.heigth = image.setoffHeight;
-}
+let container = document.querySelector('#meme-image-container');
+
+let buttonFire = document.querySelector('#fire');
+
+buttonFire.addEventListener('click', function() {
+  container.style.border = '3px dashed rgb(255 , 0 , 0)';
+});
+
+let buttonWater = document.querySelector('#water');
+
+buttonWater.addEventListener('click', function() {
+  container.style.border = '5px double rgb(0 , 0 , 255)';
+});
+
+let buttonEarth = document.querySelector('#earth');
+
+buttonEarth.addEventListener('click', function() {
+  container.style.border = '6px groove rgb(0 , 255 , 0)';
+});
