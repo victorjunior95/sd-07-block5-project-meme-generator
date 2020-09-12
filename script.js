16bdac72f2ed52a9b5ac479113179969ef4d80cb
Anderson Santana <anderslash@gmail.com>
2020-09-11 21:06:13 -0300

    Formatado com Prettier

--- script.js
+++ script.js
@@ -2,31 +2,31 @@ let textInputBox = document.getElementById("text-input");
 let memeTextBox = document.getElementById("meme-text");
 let memeContainer = document.getElementById("meme-image-container");
 
-textInputBox.addEventListener("keyup", function() {
-    memeTextBox.innerText = textInputBox.value;
+textInputBox.addEventListener("keyup", function () {
+  memeTextBox.innerText = textInputBox.value;
 });
 
 // Código abaixo inspirado em https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
-let loadFile = function(event) {
-    let memeImage = document.getElementById("meme-image");
-    memeImage.src = URL.createObjectURL(event.target.files[0]);
-    memeImage.onload = function() {
-      URL.revokeObjectURL(memeImage.src) // free memory
-    }
+let loadFile = function (event) {
+  let memeImage = document.getElementById("meme-image");
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = function () {
+    URL.revokeObjectURL(memeImage.src); // free memory
   };
+};
 
-  const buttonGreen = document.getElementById("earth");
-  buttonGreen.addEventListener("click", function() {
-    // console.log()
-    memeContainer.className = "earth-border";
-  });
+const buttonGreen = document.getElementById("earth");
+buttonGreen.addEventListener("click", function () {
+  // console.log()
+  memeContainer.className = "earth-border";
+});
 
-  const buttonRed = document.getElementById("fire");
-  buttonRed.addEventListener("click", function() {
-    memeContainer.className = "fire-border";
-  });
+const buttonRed = document.getElementById("fire");
+buttonRed.addEventListener("click", function () {
+  memeContainer.className = "fire-border";
+});
 
-  const buttonBlue = document.getElementById("water");
-  buttonBlue.addEventListener("click", function() {
-    memeContainer.className = "water-border";
-  });
+const buttonBlue = document.getElementById("water");
+buttonBlue.addEventListener("click", function () {
+  memeContainer.className = "water-border";
+});
