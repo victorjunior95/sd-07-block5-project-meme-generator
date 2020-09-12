1ee58509a0479863f3c4d0807c718a848fbdbb1f
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-12 15:19:55 -0300

    requisitions completed

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ textInput.addEventListener('input', function() {
     memeText.innerText = textInput.value;
 })
 
+// O site deve permitir que quem usa faça upload de uma imagem de seu computador.
+let imgInput = document.getElementById('meme-insert');
+let imgMeme = document.getElementById('meme-image');
+
+imgInput.addEventListener('change',function(event){
+    imgMeme.src = URL.createObjectURL(event.target.files[0]);
+    imgMeme.onload = function () {
+        URL.revokeObjectURL(imgMeme.src);
+    }
+})
