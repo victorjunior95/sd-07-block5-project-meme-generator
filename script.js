21c62d96097b783e18b870deaa938193d178ea9e
Raphael Caputo <faelelf2@gmail.com>
2020-09-12 20:13:26 -0300

    Adicionando JS

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let input = document.querySelector("#text-input");
+const input = document.querySelector("#text-input");
 let memeText = document.querySelector("#meme-text");
 
 input.addEventListener("keyup", function () {
   memeText.innerText = input.value;
 });
+
+
+let carregarImg = function (event) {
+  let memeImg = document.querySelector("#meme-image");
+  memeImg.src = URL.createObjectURL(event.target.files[0]);
+  memeImg.onload = function () {
+    URL.revokeObjectURL(memeImg.src);
+  }
+}
