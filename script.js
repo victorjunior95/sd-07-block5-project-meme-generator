ade36948c2ff20f71f46ac57a939babe381a1f73
Rodolfo <rodolfojr94@gmail.com>
2020-09-12 09:03:53 -0300

    Como diria Jack, o estripador... Vamos por partes. Atualizado ligibilidade e criado pseudocodigo.

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-//A meme-image imagem deve estar totalmente contida dentro do elemento identificado como meme-image-container-->
-//<!--O texto inserido no elemento text-input deve ser inserido sobre a imagem escolhida meme-image.-->  
-/*         <!--Dentro do elemento de container, você deve criar um outro elemento 
-        para mostrar o texto digitado. O elemento de texto deve estar 
-        totalmente contido dentro do container e ter o id denominado meme-text;--> */
 /* 
-        <!--Se não houver imagem inserida, ele deve ser inserido e estar visível
-       dentro do container vazio onde a imagem aparecerá.-->
-      
-       <!--5 - Limite o tamanho do texto que o usuário pode inserir.-->
-        <!--A quantidade máxima de caracteres digitáveis no elemento text-input deve ser 60.--> 
-              <!--Image-->
-      <imag id="meme-image" src="document.querySelector('#meme-insert')">
-        
-4 - O texto que será inserido sobre a imagem deve ter uma cor, 
-sombra e tamanho específicos.-->
+1- Usuário clica em escolher arquivo.
+  -Ele seleciona um arquivo ;
+  -Arquivo é armazenado em meme-insert ;
 
-O texto do elemento meme-text deve ter uma sombra preta, de 5 pixels
-na horizontal, 5 pixels na vertical e um raio de desfoque de 5 pixels;-->
+2- Evento chama função imgPreview.
+  -imgPreview cria (variável ou constant?) memeImg;
+  -imgPreview puxa para variável memeImg a imagem contida em meme-insert;
+  -imgPreview carrega imagem em meme-image;
 
-O texto do elemento meme-text deve ter a fonte com o tamanho de 30 pixels;-->
-          
-O texto do elemento meme-text deve estar na cor branca;-->
+3- Ao Usuário digitar em text-input é chamado função textPreview;
+  -textPreview puxa texto;
+  -textPreview adiciona texto em meme-text;
 
+    
 */
+document.querySelector('DOMContentLoaded', function(){});
+
+document.querySelector('meme-insert');
+
+
