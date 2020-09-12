b1d5cf8e282c22934bd24cf23fbdb40ac00c1973
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-09-11 21:39:33 -0300

    segundo requisito

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 window.onload = function() {
+    // recebe o texto escrito no box e imprime ele na caixa do meme    
+    let textInput = document.querySelector("#text-input");
 
-let textInput = document.querySelector("#text-input");
-let textBox = document.querySelector("#meme-text");
-
-function receberFrase(){
-    textBox.innerHTML = textInput.value;   
+    function recebeFrase() {
+        let textBox = document.querySelector("#meme-text");
+        textBox.innerHTML = textInput.value;   
     }
 
-textInput.addEventListener("keyup", receberFrase);
+    textInput.addEventListener("keyup", recebeFrase);
+}
 
+// recebe upload da imagem e imprime na caixa do meme
+function recebeImagem(event) {
+    let memeImage = document.querySelector("#meme-image");
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
 }
