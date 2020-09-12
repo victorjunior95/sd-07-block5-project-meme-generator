266e3a79bdf50191b1d2814735e308b2da259933
Eduardo Pizorno <pizorno@gmail.com>
2020-09-11 21:48:53 -0300

    Meme generator 0.3

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 window.onload = function () {
-
-function getText() {
-  let textInput = document.getElementById('text-input').value;
-  let memeText = document.getElementById('meme-text');
-  memeText.innerText=textInput.toUpperCase();
+  function getText() {
+    const textInput = document.getElementById('text-input').value;
+    let memeText = document.getElementById('meme-text');
+    memeText.innerText = textInput.toUpperCase();
 }
 
-let userText = document.getElementById('text-input');
-userText.addEventListener('keyup', getText);
+  const userText = document.getElementById('text-input');
+  userText.addEventListener('keyup', getText);
 
-function getImage(event) {
-    let memeImage = document.getElementById('meme-image');
+  function getImage(event) {
+    const memeImage = document.getElementById('meme-image');
     memeImage.src = URL.createObjectURL(event.target.files[0]);
-  }
-
-let memeInsert = document.getElementById('meme-insert');
-memeInsert.addEventListener('change', getImage);
 }
+
+  const memeInsert = document.getElementById('meme-insert');
+  memeInsert.addEventListener('change', getImage);
+};
