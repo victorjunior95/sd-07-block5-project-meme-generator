bcedfc971248d73f176d27a565264b227805f33f
moacyrrln <moacyrrln@gmail.com>
2020-09-12 12:00:46 -0300

    teste33

--- script.js
+++ script.js
@@ -11,42 +11,46 @@ let bmeme2 = document.getElementById("meme-2");
 let bmeme3 = document.getElementById("meme-3");
 let bmeme4 = document.getElementById("meme-4");
 
-container.style.border = "1px rgb(0, 0, 0) solid";
 
-// 1.2
 input.addEventListener('change', function() {
 	img.src =  URL.createObjectURL(input.files[0]);
 })
 
 text.addEventListener("input", function() {
-
     memetext.innerText = text.value;
 });
 
+// As bordas devem ser acrescentadas ao container, identificado como `meme-image-container`;
 fire.addEventListener("click", function() {
     container.className = "fireborder";
 });
 
+// As bordas devem ser acrescentadas ao container, identificado como `meme-image-container`;
 water.addEventListener("click", function() {
     container.className = "waterborder";
 });
 
+// As bordas devem ser acrescentadas ao container, identificado como `meme-image-container`;
 earth.addEventListener("click", function() {
     container.className = "earthborder";
 });
 
+// As imagens devem aparecer dentro do container de forma análoga às imagens enviadas por _upload_ para a página.
 bmeme1.addEventListener("click", function() {
     img.src =  "imgs/meme1.png";
 });
 
+// As imagens devem aparecer dentro do container de forma análoga às imagens enviadas por _upload_ para a página.
 bmeme2.addEventListener("click", function() {
     img.src =  "imgs/meme2.png";
 });
 
+// As imagens devem aparecer dentro do container de forma análoga às imagens enviadas por _upload_ para a página.
 bmeme3.addEventListener("click", function() {
     img.src =  "imgs/meme3.png";
 });
 
+// As imagens devem aparecer dentro do container de forma análoga às imagens enviadas por _upload_ para a página.
 bmeme4.addEventListener("click", function() {
     img.src =  "imgs/meme4.png";
 });
