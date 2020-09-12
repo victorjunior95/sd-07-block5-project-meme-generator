f6a7a1f2dfef8fcca8bf4eb5515e3fa3aff4c661
Renan OLiveira <ren4n.dev@gmail.com>
2020-09-12 09:36:38 -0300

    Resolvendo problemas code climate - parte 3

--- script.js
+++ script.js
@@ -1,66 +1,65 @@
 //  Declarando elemento img que contém a imagem do meme para manipular
 const imageInsert = document.getElementById('meme-image');
 
-//Inserindo imagem por seleção de arquivo no dispositivo
+//	Inserindo imagem por seleção de arquivo no dispositivo
 const memeInsert = document.querySelector('#meme-insert');
-memeInsert.addEventListener('change', function (event){
+memeInsert.addEventListener('change', function (event) {
   imageInsert.src = URL.createObjectURL(event.target.files[0]);
-  imageInsert.onload = function (){
+  imageInsert.onload = function () {
     URL.revokeObjectURL(imageInsert.src);
-  }
-})
+  };
+});
 
 //  Selecionando a miniatura 1 para implementar evento
-let miniatura1 = document.querySelector('#meme-1')
-miniatura1.addEventListener('click', function(event){
+const miniatura1 = document.querySelector('#meme-1');
+miniatura1.addEventListener('click', function (event) {
   imageInsert.src = event.target.src;
-})
+});
 
 //  Selecionando a miniatura 2 para implementar evento
-let miniatura2 = document.querySelector('#meme-2')
-miniatura2.addEventListener('click', function(event){
+const miniatura2 = document.querySelector('#meme-2');
+miniatura2.addEventListener('click', function (event) {
   imageInsert.src = event.target.src;
-})
+});
 
 //  Selecionando a miniatura 3 para implementar evento
-let miniatura3 = document.querySelector('#meme-3')
-miniatura3.addEventListener('click', function(event){
+const miniatura3 = document.querySelector('#meme-3');
+miniatura3.addEventListener('click', function (event) {
   imageInsert.src = event.target.src;
-})
+});
 
 //  Selecionando a miniatura 4 para implementar evento
-let miniatura4 = document.querySelector('#meme-4')
-miniatura4.addEventListener('click', function(event){
+const miniatura4 = document.querySelector('#meme-4');
+miniatura4.addEventListener('click', function (event) {
   imageInsert.src = event.target.src;
-})
+});
 
 //  Inserindo texto no meme
-let textInput = document.querySelector('#text-input');
-textInput.addEventListener('input', function() {
+const textInput = document.querySelector('#text-input');
+textInput.addEventListener('input', function () {
   if (textInput.value.length <= 60) {
-      let memeText = document.querySelector('#meme-text').innerText = textInput.value; 
-  } else {
-      console.log("Você ultrapassou o limite de caracteres");
-  } 
-})
+		const memeText = document.querySelector('#meme-text');
+		memeText.innerText = textInput.value;
+  };
+});
 
 //  Selecionando a DIV que será estilizada pelos botões
 let divMeme = document.querySelector('#meme-image-container');
 
 //  Implementando a função do botão fire
 let fire = document.querySelector('#fire');
-fire.addEventListener('click', function(){
+fire.addEventListener('click', function () {
   divMeme.style.border = "3px dashed red";
-})
+});
 
 //  Implementando a função do botão water
 let water = document.querySelector('#water');
-water.addEventListener('click', function(){
+water.addEventListener('click', function () {
   divMeme.style.border = "5px double blue";
-})
+});
 
 //  IMplementando a função do botão Earth
 let earth = document.querySelector('#earth');
-earth.addEventListener('click', function(){
+earth.addEventListener('click', function () {
   divMeme.style.border = "6px groove green";
-})
+});
