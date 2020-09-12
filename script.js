dfe92d86ec13092325b12df6df9011616d405e50
berchon <berchon@gmail.com>
2020-09-12 04:35:10 -0300

    Envio do item 3 v2

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-window.onload = function() {
-  let inputText = document.getElementById("text-input");
-  let memeText = document.getElementById("meme-text");
+window.onload = function () {
+  const inputText = document.getElementById('text-input');
+  let memeText = document.getElementById('meme-text');
 
-  console.log(inputText.value);
-  inputText.addEventListener("input", function() {
+  inputText.addEventListener('input', function () {
     memeText.innerHTML = inputText.value;
   });
-}
+};
