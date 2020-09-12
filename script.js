d19dd24abfb3e147811cc25a1a0679e89300b8a3
moacyrrln <moacyrrln@gmail.com>
2020-09-12 09:57:45 -0300

    teste29

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ let bmeme2 = document.getElementById("meme-2");
 let bmeme3 = document.getElementById("meme-3");
 let bmeme4 = document.getElementById("meme-4");
 
+container.style.border = "1px black solid";
+
 // 1.2
 input.addEventListener('change', function() {
-	
-    img.src =  URL.createObjectURL(input.files[0]);
+	img.src =  URL.createObjectURL(input.files[0]);
 })
-
 
 text.addEventListener("input", function() {
 
