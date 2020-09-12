e31272537973f1b19210f1cfd7d696f1d255e71d
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-12 19:48:12 -0400

    correções diversas

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-window.onload=function () {
+window.onload = function () {
   // Função para colocar foto no meme
-  const carregaFoto=document.getElementById('insere-foto'); 
+  const carregaFoto = document.getElementById('meme-insert');
   carregaFoto.addEventListener('input', function(event) {
-    const url=URL.createObjectURL(event.target.files[0]);
-    const saida=document.getElementById('imagem-meme');
-    saida.src=url;
+    const url = URL.createObjectURL(event.target.files[0]);
+    const saida = document.getElementById('meme-image');
+    saida.src = url;
+    saida.style.height = '450px';
+    saida.style.width = '450px';
   });
   // Função para colocar texto no meme
-  const lbltexto=document.getElementById('text-input');
+  const lbltexto = document.getElementById('text-input');
   lbltexto.addEventListener('change', function () {
-    const texto=document.getElementById('text-input').value;
-    document.getElementById('texto-meme').innerHTML = texto;
+    const texto = document.getElementById('text-input').value;
+    document.getElementById('meme-text').innerHTML = texto;
   });
-}
+};
