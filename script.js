56dbe7633c756cb82286eb21f12927d25917cb88
Arthur Nunes <coletivosuburbano@gmail.com>
2020-09-12 16:02:14 -0300

    input ok

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const textInput = document.getElementById("text-input");
-const memeText = document.getElementById("meme-text");
-const memeImage = document.getElementById("meme-image");
+const textInput = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
 
-textInput.addEventListener("input", function () {
+textInput.addEventListener('input', function () {
   memeText.innerText = textInput.value;
-})
+});
+
+let carregarImagem = function (event) {
+  let leitor = new FileReader();
+  leitor.onload = function () {
+    let saida = document.getElementById('meme-image');
+    saida.src = leitor.result;
+  };
+  leitor.readAsDataURL(event.target.files[0]);
+};
