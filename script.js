cafd51537c68404c102ebc69dabc6a5049a79030
vini-lima <vini20.henrique@gmail.com>
2020-09-12 02:37:08 -0300

    Adicionando as Customizaçoes

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ window.onload = function () {
   const chooseImage = document.getElementById('meme-insert');
   const textMemeOut = document.getElementById('meme-text');
   const containerImage = document.getElementById('meme-image');
-  textMemeIn.addEventListener('keyup', function () {
-    textMemeOut.innerText = textMemeIn.value;
-  });
+  let buttonFire = document.getElementById('fire');
+  let buttonWater = document.getElementById('water');
+  let buttonEarth = document.getElementById('earth');
+  textMemeIn.addEventListener('keyup', function () { textMemeOut.innerText = textMemeIn.value; });
   chooseImage.addEventListener('change', function (event) {
     containerImage.src = URL.createObjectURL(event.target.files[0]);
-    containerImage.onload = function () {
-      URL.revokeObjectURL(containerImage.src);
-  }}); 
-}
+    containerImage.onload = function () { URL.revokeObjectURL(containerImage.src); }});
+  buttonFire.addEventListener('click', function () { buttonFire = document.getElementById('meme-image-container');
+      buttonFire.style.border = '3px dashed red'; })
+  buttonWater.addEventListener('click', function () { buttonWater = document.getElementById('meme-image-container');
+      buttonWater.style.border = '5px double blue'; })
+  buttonEarth.addEventListener('click', function () { buttonEarth = document.getElementById('meme-image-container');
+      buttonEarth.style.border = '6px groove green'; })
+};
