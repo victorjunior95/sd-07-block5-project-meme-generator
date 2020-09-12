90070864468c0cd572f399c80fec3f18ed7c7949
LAURO DE SA <itrafor@gmail.com>
2020-09-12 09:50:26 -0300

    alterações 06

--- script.js
+++ script.js
@@ -7,13 +7,18 @@ window.onload = function() {
     const btFire = document.getElementById("fire");
     const btWater = document.getElementById("water");
     const btEarth = document.getElementById("earth");
+    const meme1 = document.getElementById("meme-1");
+    const meme2 = document.getElementById("meme-2");
+    const meme3 = document.getElementById("meme-3");
+    const meme4 = document.getElementById("meme-4");
 
     // configurações de estilos
     memeImage.style.backgroundColor='white';
     memeImage.style.width='400px';
     memeImage.style.height='350px';
     memeImage.style.border='solid 1px black';
-
+    
+    
     // configurações de estilos
     memeText.style.textShadow ='#000 5px 5px 5px';
     memeText.style.fontSize='30px';
@@ -22,25 +27,28 @@ window.onload = function() {
     memeText.style.maxWidth='inherit'; /*Tamanho maximo do pai*/
     
   
-
-   
+    /*Upload de imagem*/
+   /*aprendi com esse video:https://www.youtube.com/watch?v=VElnT8EoEEM*/
     memeInsert.addEventListener("change", function (){
-        let patchImg = memeInsert.value;    
-        let onlyImg = patchImg.slice(12);
-        memeImage.style.backgroundImage='url('+onlyImg+')';
-        memeImage.style.backgroundSize='cover'; 
-        //image.setAttribute("src",onlyImg);
-        //memeImage.appendChild(image);
-        
+        const file = this.files[0];
+        if (file) {
+           const reader = new FileReader();           
+           reader.addEventListener("load", function(){            
+                let urlImage = this.result;
+                memeImage.style.backgroundImage='url('+urlImage+')';
+                memeImage.style.backgroundSize='100% 100%';
+           });
+           reader.readAsDataURL(file);
+          
+        }
+       
     });
-
     txtInput.addEventListener("keyup", function (){
         let txt = this.value;
         txt.upperCase;        
         memeText.innerText=txt; 
              
     });
-
     btFire.addEventListener("click", function (){
         memeImage.style.border='dashed 3px red';     
     });
@@ -52,6 +60,32 @@ window.onload = function() {
     btEarth.addEventListener("click", function (){
         memeImage.style.border='groove 6px green';     
     });
+
+    meme1.addEventListener("click", function (){
+        let patchImg = this.getAttribute('src');
+        memeImage.style.backgroundImage='url('+patchImg+')';
+        memeImage.style.backgroundSize='100% 100%'; 
+        
+    });
+    meme2.addEventListener("click", function (){
+        let patchImg = this.getAttribute('src');
+        memeImage.style.backgroundImage='url('+patchImg+')';
+        memeImage.style.backgroundSize='100% 100%'; 
+    });
+    meme3.addEventListener("click", function (){
+        let patchImg = this.getAttribute('src');
+        memeImage.style.backgroundImage='url('+patchImg+')';
+        memeImage.style.backgroundSize='100% 100%'; 
+    });
+    meme4.addEventListener("click", function (){
+        let patchImg = this.getAttribute('src');
+        memeImage.style.backgroundImage='url('+patchImg+')';
+        memeImage.style.backgroundSize='100% 100%'; 
+    });
+
+    
+
+    
 
 }
 
