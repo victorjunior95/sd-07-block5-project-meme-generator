7a9ac0219aeffc667c9c3f35f09ea46798a64fb6
wellington <profwellington17@gmail.com>
2020-09-12 22:25:39 -0300

    atividade 1

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function frase () {
-
+    let text= 
     alert("teste");
+}
+
+function Enviar() {
+
+    var nome = document.getElementById("text-input");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
 }
