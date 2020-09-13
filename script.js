b6b5bae44c8f2967bcc6643f583b4c272096675e
josielvs <josielvs@gmail.com>
2020-09-12 21:56:07 -0300

    Code Climate 4.

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 function createReader() {
-  const headerPage = document.createElement('header')
-  headerPage.className = 'head-class'
-  document.body.appendChild(headerPage)
+  const headerPage = document.createElement('header');
+  headerPage.className = 'head-class';
+  document.body.appendChild(headerPage);
 
-  let titleProject = document.createElement('h1')
-  titleProject.innerHTML = "MEME GENERATOR"
-  titleProject.className = 'title'
-  headerPage.appendChild(titleProject)
+  const titleProject = document.createElement('h1')
+  titleProject.innerHTML = 'MEME GENERATOR';
+  titleProject.className = 'title';
+  headerPage.appendChild(titleProject);
 }
 
 const nameNewDiv = ['textInputDiv', 'fileInputDiv', 'memeContainer', 'buttonDiv', 'memeMiniDiv'];
 const nameClassNewDiv = ['div-input-text', 'div-input-text', 'meme-container', 'div-button', 'div-mini-meme'];
-function createPageStructure (textInputDiv, nameClassDiv) {
-  let sectionMain = document.createElement('section')
-  sectionMain.className = 'main'
+
+function createPageStructure(textInputDiv, nameClassDiv) {
+  const sectionMain = document.createElement('section');
+  sectionMain.className = 'main';
   document.body.appendChild(sectionMain)
 
   for (let index = 0; index < textInputDiv.length; index += 1) {
     if (textInputDiv[index] === 'memeContainer') {
-      textInputDiv[index] = document.createElement('div')
-      textInputDiv[index].className = nameClassDiv[index]
-      textInputDiv[index].setAttribute('id', 'meme-image-container')
-      sectionMain.appendChild(textInputDiv[index])
+      textInputDiv[index] = document.createElement('div');
+      textInputDiv[index].className = nameClassDiv[index];
+      textInputDiv[index].setAttribute('id', 'meme-image-container');
+      sectionMain.appendChild(textInputDiv[index]);
     } else {
-      textInputDiv[index] = document.createElement('div')
-      textInputDiv[index].className = nameClassDiv[index]
-      sectionMain.appendChild(textInputDiv[index])
+      textInputDiv[index] = document.createElement('div');
+      textInputDiv[index].className = nameClassDiv[index];
+      sectionMain.appendChild(textInputDiv[index]);
     }
   }
 }
 
-function createPageElementsText (elementsForText) {
-  let inputLabelText = document.createElement('label')
+function createPageElementsText(elementsForText) {
+  const inputLabelText = document.createElement('label')
   inputLabelText.setAttribute('for', 'text-input')
   inputLabelText.innerHTML = 'Insira o texto: '
   elementsForText[0].appendChild(inputLabelText)
 
-  let inputText = document.createElement('input')
+  const inputText = document.createElement('input')
   inputText.setAttribute('id', 'text-input')
   inputText.setAttribute('type', 'text')
   inputText.setAttribute('maxlength', 60)
   elementsForText[0].appendChild(inputText)
 
-  let textImage = document.createElement('p')
+  const textImage = document.createElement('p')
   textImage.className = "meme-text"
   textImage.setAttribute('id', 'meme-text')
   textImage.innerText = 'Texto do MEME!'
@@ -50,17 +51,17 @@ function createPageElementsText (elementsForText) {
 }
 
 function createPageElementsImage (elementsForImage) {
-  let inputLabelFile = document.createElement('label')
+  const inputLabelFile = document.createElement('label')
   inputLabelFile.setAttribute('for', 'file-input')
   inputLabelFile.innerHTML = 'Inserir imagem: '
   elementsForImage[1].appendChild(inputLabelFile)
 
-  let inputFileButton = document.createElement('input')
+  const inputFileButton = document.createElement('input')
   inputFileButton.setAttribute('id', 'meme-insert')
   inputFileButton.setAttribute('type', 'file')
   elementsForImage[1].appendChild(inputFileButton)
 
-  let viewImage = document.createElement('img')
+  const viewImage = document.createElement('img')
   viewImage.className = 'meme-image'
   viewImage.setAttribute('id', 'meme-image')
   viewImage.setAttribute('src', 'https://picsum.photos/500/400')
