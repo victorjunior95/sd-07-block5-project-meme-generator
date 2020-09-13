8cb703cd424044dc55e7dff1367d33c1cbcfa3f6
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-13 20:07:54 -0300

    inserido botẽs de estilo

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const script = document.querySelector('#text-input');
 let setImage = document.querySelector('#meme-insert');
 const picture = document.querySelector('#meme-image');
 
-let earth = document.querySelector('#changeButtons').childNodes[7];
-let water = document.querySelector('#changeButtons').childNodes[5];
-let fire = document.querySelector('#changeButtons').childNodes[3];
-let container = document.querySelector('#meme-image-container');
+
 
 //Recupera o local onde quero minha string
 script.addEventListener('keyup', function () {
@@ -16,6 +13,11 @@ setImage.addEventListener('change', function(event){
     setImage = setImage.value;
     picture.src = URL.createObjectURL(event.target.files[0]);
 });
+
+let earth = document.querySelector('#changeButtons').childNodes[7];
+let water = document.querySelector('#changeButtons').childNodes[5];
+let fire = document.querySelector('#changeButtons').childNodes[3];
+let container = document.querySelector('#meme-image-container');
 
 fire.addEventListener('click', function () {
     container.style.border = '3px dashed #DC143C';
