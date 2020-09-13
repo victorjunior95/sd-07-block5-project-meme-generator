9db7a147c715d41077d1461501c2cf98492764ed
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-13 09:43:03 -0300

    Ajusting code climate

--- script.js
+++ script.js
@@ -8,35 +8,35 @@ let LoadMemeImage = function (event) {
 };
 
 textInputBox.addEventListener('keyup', function () {
-  const textInput = document.querySelector('#text-input').value;
-  const textMeme = document.querySelector('#meme-text');
+  const textInput = document.querySelector("#text-input").value;
+  const textMeme = document.querySelector("#meme-text");
   textMeme.innerText = textInput;
 });
 
 function frog() {
-  document.getElementById("meme-image").src = "/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme1.png"
+  document.getElementById("meme-image").src = '/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme1.png';
 }
 
 function lol() {
-  document.getElementById("meme-image").src = "/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme2.png"
+  document.getElementById("meme-image").src = '/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme2.png';
 }
 
 function homer() {
-  document.getElementById("meme-image").src = "/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme3.png"
+  document.getElementById("meme-image").src = '/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme3.png';
 }
 
 function omg() {
-  document.getElementById("meme-image").src = "/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme4.png"
+  document.getElementById("meme-image").src = '/projects/block-05/sd-07-block5-project-meme-generator/imgs/meme4.png';
 }
 
 function fire() {
-  document.getElementById("meme-image-container").style.border = "dashed 3px red";
+  document.getElementById("meme-image-container").style.border = 'dashed 3px red';
 }
 
 function water() {
-  document.getElementById("meme-image-container").style.border = "double 5px blue";
+  document.getElementById("meme-image-container").style.border = 'double 5px blue';
 }
 
 function earth() {
-  document.getElementById("meme-image-container").style.border = "groove 6px green";
+  document.getElementById("meme-image-container").style.border = 'groove 6px green';
 }
