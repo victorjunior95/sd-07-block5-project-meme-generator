65ed23cd69c912736714d85cee2c87b59519f100
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-09-13 14:06:26 -0300

    Corrigindo espaços codeclimate

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
-window.onload = function(){
-
-  //adicionando imagem
-  let input = document.getElementById('meme-insert');
-
+window.onload = function() {
+  const input = document.getElementById('meme-insert').addEventListener('change',fileLoad);
   function fileLoad(event) {
-    let memeImage = document.getElementById("meme-image");
+    const memeImage = document.getElementById("meme-image");
     memeImage.src = URL.createObjectURL(event.target.files[0]);
     memeImage.onload = function() {
     URL.revokeObjectURL(memeImage.src) // free memory
     }
   }
+  const textMeme = document.getElementById('text-input');
+  textMeme.addEventListener('keyup', function(){
+    const texto = document.getElementById('meme-text');
+      texto.textContent = textMeme.value
 
-  input.addEventListener('change',fileLoad);
-  //fim
-
-  //adicionado texto
-  let textMeme = document.getElementById('text-input');
-
-  //funcao para trocar texto
-  function AdicionaTextoImg(){
-      let texto = document.getElementById('meme-text');
-      texto.textContent = textMeme.value;
-  }
-  textMeme.addEventListener('keyup', AdicionaTextoImg);
-  //fim funcao Adicionar texto a imagem
-
-  //adicionando bordas
-
-
+  });
   let backgroundColorButtons = document.querySelectorAll("#cores>button")
   for (let i = 0; i < backgroundColorButtons.length; i += 1) {
     backgroundColorButtons[i].addEventListener("click", function(event) {
@@ -38,9 +23,7 @@ window.onload = function(){
   function setBackgroundColor(color) {
 
       let container = document.getElementById('meme-image-container');
-
       let caixaAlta = color.toUpperCase();
-
       if(caixaAlta == 'FIRE') {
 
           container.style.borderColor = "rgb(255, 0, 0)";
@@ -54,21 +37,17 @@ window.onload = function(){
 
       }
       else if(caixaAlta == 'EARTH') {
-          container.style.borderColor = "rgb(0, 128, 0)";
+          container.style.border = "rgb(0, 128, 0)";
           container.style.borderWidth = '6px';
           container.style.borderStyle = 'groove';
 
       }
     }
-
-    //imagens pré-carregadas
       document.getElementById("meme-1").src = "imgs/meme1.png";
       document.getElementById("meme-2").src = "imgs/meme2.png";
       document.getElementById("meme-3").src = "imgs/meme3.png";
       document.getElementById("meme-4").src = "imgs/meme4.png";
-
       let imgsClick = document.querySelectorAll("#memePronto>img");
-
       for (let i = 0; i < imgsClick.length; i += 1) {
 
           imgsClick[i].addEventListener("click", function(event) {
@@ -76,13 +55,9 @@ window.onload = function(){
           })
 
        }
-
-
        function inserirImg(eventSrc) {
 
           let memeImage = document.getElementById("meme-image");
           memeImage.src = eventSrc;
      }
-
-
 }
