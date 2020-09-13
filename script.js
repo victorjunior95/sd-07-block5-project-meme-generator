03b72de0fe0c665c8010b9c77f8c917b55f93fc4
murilopereira006 <muriloedu006@gmail.com>
2020-09-12 23:29:35 -0300

    botao e bordas funcionando

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var texto = document.querySelector("#text-input");
 var textoMeme = document.querySelector("#meme-text");
+var fogo = document.querySelector("#fire");
+var agua = document.querySelector("#water");
+var terra =document.querySelector("#earth");
+var borda = document.querySelector(".container");
 
 function textoAqui() {
     textoMeme.innerText = texto.value;
+}
+
+function botaoFogo () {
+    borda.style.border = '3px dashed red';
+}
+function botaoAgua () {
+    borda.style.border = '5px double blue'; //borda azul, com 5 pixels do tipo double
+}
+function botaoTerra () {
+    borda.style.border = '6px groove green'; //groove, verde e com 6 pixels.
 }
 
 var loadFile = function (event) {
@@ -14,3 +28,6 @@ var loadFile = function (event) {
 };
 
 texto.addEventListener('input', textoAqui);
+fogo.addEventListener('click', botaoFogo);
+agua.addEventListener('click', botaoAgua);
+terra.addEventListener('click', botaoTerra);
