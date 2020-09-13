f2bfd53cb1f3d2aa88d337825afd9a3d5417a100
Vitor Amorims <vtamorims@gmail.com>
2020-09-13 13:24:45 -0300

    Deus me ajude!

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function loadImage(event) {
 function cathFile () {
   const imgInput = document.querySelector("#meme-insert");
   imgInput.addEventListener('change', loadImage);
+  catchImageMemes ()
 }
 //MDN https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications 
 /*const fileSelect = document.getElementById("meme-insert"),
