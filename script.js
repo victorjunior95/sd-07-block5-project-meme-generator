55233b4ba29366397ee2e9a07fa445de0084a6c8
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-09-13 18:05:06 -0300

    Requisito bônus adicionado e estilos CSS adicionados.

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
 // Recupera os elementos HTML por ID.
-const textInput = document.querySelector('#text-input');
-const textOutput = document.querySelector('#meme-text');
-const imgInput = document.getElementById('meme-insert');
-const imgOutput = document.getElementById('meme-image');
+const textInput = document.querySelector("#text-input");
+const textOutput = document.querySelector("#meme-text");
+const imgInput = document.getElementById("meme-insert");
+const imgOutput = document.getElementById("meme-image");
+const btnFire = document.querySelector("#btnFire");
+const btnWater = document.querySelector("#btnWater");
+const btnEarth = document.querySelector("#btnEarth");
+const divMemeImageContainer = document.getElementById("meme-image-container");
+const divBorderButtons = document.getElementById("divBorderButtons");
 
 // Cria URL única para documento local recebendo no parametro dados do evento onchange.
 function loadImage(event) {
   imgOutput.src = URL.createObjectURL(event.target.files[0]);
-// Assim que os dados do arquivo estiverem carregados a URL é descartada .
+  // Assim que os dados do arquivo estiverem carregados a URL é descartada .
   imgOutput.onload = () => {
     URL.revokeObjectURL(imgOutput.src);
   };
 }
 // Adiciona escutador de eventos no input da imagem.
-imgInput.addEventListener('change', loadImage);
+imgInput.addEventListener("change", loadImage);
 // Adiciona escutador de eventos no input do texto.
-textInput.addEventListener('keyup', () => {
+textInput.addEventListener("keyup", () => {
   textOutput.innerText = textInput.value;
+});
+
+btnFire.addEventListener("click", () => {
+  divMemeImageContainer.className = "meme-image-container fire-border";
+});
+btnFire.addEventListener("mousemove", () => {
+  divBorderButtons.className = "div-border-buttons fire-border";
+});
+btnFire.addEventListener("mouseout", () => {
+  divBorderButtons.className = "div-border-buttons";
+});
+
+btnWater.addEventListener("click", () => {
+  divMemeImageContainer.className = "meme-image-container water-border";
+});
+btnWater.addEventListener("mousemove", () => {
+  divBorderButtons.className = "div-border-buttons water-border";
+});
+btnWater.addEventListener("mouseout", () => {
+  divBorderButtons.className = "div-border-buttons";
+});
+
+btnEarth.addEventListener("click", () => {
+  divMemeImageContainer.className = "meme-image-container earth-border";
+});
+btnEarth.addEventListener("mousemove", () => {
+  divBorderButtons.className = "div-border-buttons earth-border";
+});
+btnEarth.addEventListener("mouseout", () => {
+  divBorderButtons.className = "div-border-buttons";
 });
