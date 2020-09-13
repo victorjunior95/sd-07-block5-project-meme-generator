fe6308d9a96515f83e8b492a81a7ed7f09d0acc6
Thays Costa <thayscosta3@gmail.com>
2020-09-13 03:44:36 -0300

    alterações nas cores das margens

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-let inputText = document.querySelector("#text-input");
-let inputImage = document.querySelector("#meme-insert");
-let containerBorder = document.querySelector("#meme-image-container");
+let inputText = document.querySelector('#text-input');
+let inputImage = document.querySelector('#meme-insert');
+let containerBorder = document.querySelector('#meme-image-container');
 
-inputText.addEventListener("keyup", function(){
-  document.querySelector("#meme-text").innerText = inputText.value;
+inputText.addEventListener('keyup', function(){
+  document.querySelector('#meme-text').innerText = inputText.value;
 });
 
-let image = document.querySelector("#meme-image");
+let image = document.querySelector('#meme-image');
 
-document.querySelector("#meme-1").addEventListener("click", function(){
-  image.src = "./imgs/meme-1.png";
+document.querySelector('#meme-1').addEventListener("click", function(){
+  image.src = './imgs/meme-1.png';
 });
 
-document.querySelector("#meme-2").addEventListener("click", function(){
-  image.src = "./imgs/meme-2.png";
+document.querySelector('#meme-2').addEventListener("click", function(){
+  image.src = './imgs/meme-2.png';
 })
 
-document.querySelector("#meme-3").addEventListener("click", function(){
-  image.src = "./imgs/meme-3.png";
+document.querySelector('#meme-3').addEventListener("click", function(){
+  image.src = './imgs/meme-3.png';
 });
 
-document.querySelector("#meme-4").addEventListener("click", function(){
-  image.src = "./imgs/meme-4.png";
+document.querySelector('#meme-4').addEventListener('click', function(){
+  image.src = './imgs/meme-4.png';
 });
 
 inputImage.addEventListener('input', () => {
   image.src = URL.createObjectURL(inputImage.files[0]);
 });
 
-document.querySelector("#fire").addEventListener("click", function(){
-  containerBorder.style.border = "dashed 3px red"; 
+document.querySelector('#fire').addEventListener("click", function(){
+  containerBorder.style.border = 'dashed 3px red'; 
 })
 
-document.querySelector("#water").addEventListener("click", function(){
-  containerBorder.style.border = "double 5px blue";
+document.querySelector('#water').addEventListener("click", function(){
+  containerBorder.style.border = 'double 5px rgb(0 0 225)';
 })
 
-document.querySelector("#earth").addEventListener("click", function(){
-  containerBorder.style.border = "groove 6px green";
+document.querySelector('#earth').addEventListener("click", function(){
+  containerBorder.style.border = 'groove 6px rgb(0 128 0)';
 })
 
