2438779d074c2694fa75966c64793f20cf03a067
josielvs <josielvs@gmail.com>
2020-09-12 21:00:41 -0300

    Refatorando arquivo JS.

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function createReader () {
     headerPage.appendChild(titleProject)
 }
 
-function createPageStructure () {
+let nameNewDiv = ['textInputDiv', 'fileInputDiv', 'memeContainer']
+let nameClassNewDiv = ['div-input-text', 'div-input-text', 'meme-container']
+function createPageStructure (textInputDiv, nameClassDiv) {
     let sectionMain = document.createElement('section')
     sectionMain.className = 'main'
     document.body.appendChild(sectionMain)
 
-    let textInputDiv = ['textInputDiv', 'fileInputDiv', 'memeContainer']
-    let nameClassDiv = ['div-input-text', 'div-input-text', 'meme-container']
     for (let index = 0; index < textInputDiv.length; index += 1) {
         if (textInputDiv[index] === 'memeContainer') {
             textInputDiv[index] = document.createElement('div')
@@ -29,58 +29,66 @@ function createPageStructure () {
             sectionMain.appendChild(textInputDiv[index])
         }
     }
-    
+}
+function createPageElements (divId) {  
     let inputLabelText = document.createElement('label')
     inputLabelText.setAttribute('for', 'text-input')
     inputLabelText.innerHTML = 'Insira o texto: '
-    textInputDiv[0].appendChild(inputLabelText)
+    divId[0].appendChild(inputLabelText)
 
     let inputText = document.createElement('input')
     inputText.setAttribute('id', 'text-input')
     inputText.setAttribute('type', 'text')
     inputText.setAttribute('maxlength', 60)
-    textInputDiv[0].appendChild(inputText)
+    divId[0].appendChild(inputText)
 
     let inputLabelFile = document.createElement('label')
     inputLabelFile.setAttribute('for', 'file-input')
     inputLabelFile.innerHTML = 'Adicione a imagem: '
-    textInputDiv[1].appendChild(inputLabelFile)
+    divId[1].appendChild(inputLabelFile)
 
     let inputFileButton = document.createElement('input')
     inputFileButton.setAttribute('id', 'meme-insert')
     inputFileButton.setAttribute('type', 'file')
-    textInputDiv[1].appendChild(inputFileButton)
+    divId[1].appendChild(inputFileButton)
 
     let viewImage = document.createElement('img')
     viewImage.className = 'meme-image'
     viewImage.setAttribute('id', 'meme-image')
     viewImage.setAttribute('src', 'https://picsum.photos/500/400')
-    textInputDiv[2].appendChild(viewImage)
+    divId[2].appendChild(viewImage)
 
     let textImage = document.createElement('p')
     textImage.className = "meme-text"
     textImage.setAttribute('id', 'meme-text')
     textImage.innerText = 'Texto do MEME!'
-    textInputDiv[2].appendChild(textImage)
+    divId[2].appendChild(textImage)
 }
 createReader()
-createPageStructure()
+createPageStructure(nameNewDiv, nameClassNewDiv)
+createPageElements(nameNewDiv)
 
-const newTextInsert = document.querySelector('#text-input')
-newTextInsert.addEventListener('input', function () {
-    if (newTextInsert.value.length <= 60) {
-        let pushText = document.querySelector('#meme-text')
-        pushText.innerText = newTextInsert.value
-    }
-})
+function insertText () {
+    const newTextInsert = document.querySelector('#text-input')
+    newTextInsert.addEventListener('input', function () {
+        if (newTextInsert.value.length <= 60) {
+            let pushText = document.querySelector('#meme-text')
+            pushText.innerText = newTextInsert.value
+        }
+    })
+}
 
-const viewImageInsert = document.getElementById('meme-image')
-
-const newImageInsert = document.getElementById('meme-insert')
-newImageInsert.addEventListener('change', function (event) {
-    viewImageInsert.src = URL.createObjectURL(event.target.files[0])
-    viewImageInsert.onload = function () {
-      URL.revokeObjectURL(viewImageInsert.src);
-    }
-  })
-  
+function insertImage () {
+    const viewImageInsert = document.getElementById('meme-image')
+    const newImageInsert = document.getElementById('meme-insert')
+    newImageInsert.addEventListener('change', function (event) {
+        // URL.createObjectURL() = Adicionar a referência ao Ojeto do arquivo
+        viewImageInsert.src = URL.createObjectURL(event.target.files[0])
+        viewImageInsert.onload = function () {
+        // Remover a referência ao arquivo - Verificar real necessidade
+        URL.revokeObjectURL(viewImageInsert.src);
+        }
+    })
+}
+insertText()
+insertImage()
