0e2444263a2713d62e780dafbb833ff87753c727
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-09-13 18:18:33 -0300

    Correção cypress e codeclimate.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Recupera os elementos HTML por ID.
-const textInput = document.querySelector("#text-input");
-const textOutput = document.querySelector("#meme-text");
-const imgInput = document.getElementById("meme-insert");
-const imgOutput = document.getElementById("meme-image");
-const btnFire = document.querySelector("#btnFire");
-const btnWater = document.querySelector("#btnWater");
-const btnEarth = document.querySelector("#btnEarth");
-const divMemeImageContainer = document.getElementById("meme-image-container");
-const divBorderButtons = document.getElementById("divBorderButtons");
+const textInput = document.querySelector('#text-input');
+const textOutput = document.querySelector('#meme-text');
+const imgInput = document.getElementById('meme-insert');
+const imgOutput = document.getElementById('meme-image');
+const btnFire = document.querySelector('#fire');
+const btnWater = document.querySelector('#water');
+const btnEarth = document.querySelector('#earth');
+const divMemeImageContainer = document.getElementById('meme-image-container');
+const divBorderButtons = document.getElementById('divBorderButtons');
 
 // Cria URL única para documento local recebendo no parametro dados do evento onchange.
 function loadImage(event) {
@@ -18,38 +18,38 @@ function loadImage(event) {
   };
 }
 // Adiciona escutador de eventos no input da imagem.
-imgInput.addEventListener("change", loadImage);
+imgInput.addEventListener('change', loadImage);
 // Adiciona escutador de eventos no input do texto.
-textInput.addEventListener("keyup", () => {
+textInput.addEventListener('keyup', () => {
   textOutput.innerText = textInput.value;
 });
 
-btnFire.addEventListener("click", () => {
-  divMemeImageContainer.className = "meme-image-container fire-border";
+btnFire.addEventListener('click', () => {
+  divMemeImageContainer.className = 'meme-image-container fire-border';
 });
-btnFire.addEventListener("mousemove", () => {
-  divBorderButtons.className = "div-border-buttons fire-border";
+btnFire.addEventListener('mousemove', () => {
+  divBorderButtons.className = 'div-border-buttons fire-border';
 });
-btnFire.addEventListener("mouseout", () => {
-  divBorderButtons.className = "div-border-buttons";
-});
-
-btnWater.addEventListener("click", () => {
-  divMemeImageContainer.className = "meme-image-container water-border";
-});
-btnWater.addEventListener("mousemove", () => {
-  divBorderButtons.className = "div-border-buttons water-border";
-});
-btnWater.addEventListener("mouseout", () => {
-  divBorderButtons.className = "div-border-buttons";
+btnFire.addEventListener('mouseout', () => {
+  divBorderButtons.className = 'div-border-buttons';
 });
 
-btnEarth.addEventListener("click", () => {
-  divMemeImageContainer.className = "meme-image-container earth-border";
+btnWater.addEventListener('click', () => {
+  divMemeImageContainer.className = 'meme-image-container water-border';
 });
-btnEarth.addEventListener("mousemove", () => {
-  divBorderButtons.className = "div-border-buttons earth-border";
+btnWater.addEventListener('mousemove', () => {
+  divBorderButtons.className = 'div-border-buttons water-border';
 });
-btnEarth.addEventListener("mouseout", () => {
-  divBorderButtons.className = "div-border-buttons";
+btnWater.addEventListener('mouseout', () => {
+  divBorderButtons.className = 'div-border-buttons';
+});
+
+btnEarth.addEventListener('click', () => {
+  divMemeImageContainer.className = 'meme-image-container earth-border';
+});
+btnEarth.addEventListener('mousemove', () => {
+  divBorderButtons.className = 'div-border-buttons earth-border';
+});
+btnEarth.addEventListener('mouseout', () => {
+  divBorderButtons.className = 'div-border-buttons';
 });
