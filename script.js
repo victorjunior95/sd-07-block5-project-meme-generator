411124a5e8f27a30db22e55f405c32723a66c80c
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-13 02:53:02 -0300

    criando campos

--- script.js
+++ script.js
@@ -14,20 +14,23 @@ window.onload = function () {
 
 
     function fetch() {
-        // document.querySelector("#btnFileUpload").addEventListener("click", function () {
-        //     let path = document.querySelector("#meme-image-container").childNodes[1]; //encontra meu form acesando seu pai
-        //     let input = document.querySelector("#myFile"); //obtenho onde está meu input que busca o arquivo
-        //     let myImg = document.createElement("IMG"); //crio uma tag img que irá conter minha imagem
-        //     myImg.id = "imageId"; //crio uma id para minha img
-        //     path.appendChild(myImg); // insiro minha tag img no arquivo html de forma dinamica
-        //     let fReader = new FileReader(); // O objeto Filereader foi utilizado para ler o path do arquivo ou Blob,  mais sobre blob aquie https://developer.mozilla.org/en-US/docs/Web/API/Blob
-        //     console.log(fReader);
-        //     fReader.readAsDataURL(input.files[0]); //busca a url do arquivo e retorna 'DONE' caso a encontre
-        //     fReader.onloadend = function (event){  // exemplo de código retirado de https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
-        //         let img = document.querySelector("#imageId");
-        //         img.src = event.target.result;
-        //     }
-        // });
+        document.querySelector("#btnFileUpload").addEventListener("click", function () {
+            let path = document.querySelector("#meme-image-container").childNodes[1]; //encontra meu form acesando seu pai
+            let input = document.querySelector("#meme-insert"); //obtenho onde está meu input que busca o arquivo
+            let myImg = document.createElement("IMG"); //crio uma tag img que irá conter minha imagem
+
+            myImg.id = "imageId"; //crio uma id para minha img
+            path.appendChild(myImg); // insiro minha tag img no arquivo html de forma dinamica
+            let fReader = new FileReader(); // O objeto Filereader foi utilizado para ler o path do arquivo ou Blob,  mais sobre blob aquie https://developer.mozilla.org/en-US/docs/Web/API/Blob
+
+            console.log(fReader);
+            fReader.readAsDataURL(input.files[0]); //busca a url do arquivo e retorna 'DONE' caso a encontre
+            fReader.onloadend = function (event){  // exemplo de código retirado de https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
+
+                let img = document.querySelector("#imageId");
+                img.src = event.target.result;
+            }
+        });
     }
       function insertText() {
          document.querySelector("#text-input").addEventListener("click", function () {
