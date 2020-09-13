ba77f68f28756f4c9c0e03df79e6257f5821a214
Alstent <rafaelasltent@gmail.com>
2020-09-13 09:46:22 +0200

    resolvendo code climate

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-let inpText = document.querySelector('#text-input');
-let previewText = document.querySelector('#meme-text');
-let inpFile = document.querySelector('#meme-insert');
-let memeContainer = document.querySelector('#meme-image-container');
-let previewImage = memeContainer.querySelector('#meme-image');
-let spanText = memeContainer.querySelector('.span-text');
+const inpText = document.querySelector('#text-input');
+ const previewText = document.querySelector('#meme-text');
+const inpFile = document.querySelector('#meme-insert');
+const memeContainer = document.querySelector('#meme-image-container');
+const previewImage = memeContainer.querySelector('#meme-image');
+const spanText = memeContainer.querySelector('.span-text');
 
-inpFile.addEventListener('change', function() {
+inpFile.addEventListener('change', function () {
   if (this.files && this.files[0]) {
-    spanText.style.display = 'none'
-    previewImage.style.display = 'flex'
-    previewImage.src = URL.createObjectURL(this.files[0])
-    console.log(this.files[0])
+    spanText.style.display = 'none';
+    previewImage.style.display = 'flex';
+    previewImage.src = URL.createObjectURL(this.files[0]);
   } else {
-    spanText.style.display = null
-    previewImage.style.display = null
-    previewImage.setAttribute('src', '')
+    spanText.style.display = null;
+    previewImage.style.display = null;
+    previewImage.setAttribute('src', '');
   }
 });
 
 /* adiciona a imagem de forma mais complicada no me gusta
 inpFile.addEventListener('change', function() {
-	const file = this.files[0]
-	if (file) {
-		const reader = new FileReader()
-		spanText.style.display = 'none'
-		previewImage.style.display = 'flex'
-		reader.addEventListener('load', function() {
-			previewImage.setAttribute('src', this.result)
-		})
-		reader.readAsDataURL(file)
-	} else {
-		spanText.style.display = null
-		previewImage.style.display = null
-		previewImage.setAttribute('src', '')
-	}
+  const file = this.files[0]
+  if (file) {
+    const reader = new FileReader()
+    spanText.style.display = 'none'
+    previewImage.style.display = 'flex'
+    reader.addEventListener('load', function() {
+      previewImage.setAttribute('src', this.result)
+    })
+    reader.readAsDataURL(file)
+  } else {
+    spanText.style.display = null
+    previewImage.style.display = null
+    previewImage.setAttribute('src', '')
+  }
 }) */
 
-inpText.addEventListener('keyup', function() {
-  if(inpText.value) {
-    previewText.innerHTML = inpText.value
+inpText.addEventListener('keyup', function () {
+  if (inpText.value) {
+    previewText.innerHTML = inpText.value;
   }
 });
