ca0619961143f06d5ac1d442d24e525c3b2026dd
Eduardo Pizorno <pizorno@gmail.com>
2020-09-13 09:20:58 -0300

    Meme generator Refeito 0.0.4 - Adcionado elemento p a div meme-text

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getInput() {
   const inputText = document.getElementById('text-input').value;
-  document.getElementById('meme-text').innerHTML = inputText.toUpperCase();
+  document.getElementsByTagName('p').innerHTML = inputText.toUpperCase();
 }
 
 document.getElementById('text-input').addEventListener('keyup', getInput);
