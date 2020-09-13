c25c8c18f2ffde4e1aed3efa17523b66d616138f
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-13 16:17:55 -0300

    criando caixa para imagem

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const script = document.querySelector('#text-input');
+let setImage = document.querySelector('#meme-insert');
+const picture = document.querySelector('#meme-image');
+
+
 
 //Recupera o local onde quero minha string
 script.addEventListener('keyup', function () {
     document.querySelector('#meme-text').innerText = script.value;
+});
+
+setImage.addEventListener('change', function(event){
+    setImage = setImage.value;
+    picture.src = URL.createObjectURL(event.target.files[0]);
 });
