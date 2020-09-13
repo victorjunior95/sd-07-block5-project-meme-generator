be934b13089e85351f09cb02f14fa3ce3f11a31e
vini-lima <vini20.henrique@gmail.com>
2020-09-12 22:33:24 -0300

    Alterando o Code Climate 9

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 window.onload = function () {
-  const textMemeIn = document.getElementById('text-input'); //Texto digitado
-  const chooseImage = document.getElementById('meme-insert'); //Imagem Selecionada
-  const textMemeOut = document.getElementById('meme-text'); //Texto digitado na imagem
-  const containerImage = document.getElementById('meme-image');//Imagem na caixa
-  let bF = document.getElementById('fire'); //bF = buttonFire
-  let bW = document.getElementById('water'); //bW = buttonWater
-  let bE = document.getElementById('earth'); //bE = buttonEarth
+  const textMemeIn = document.getElementById('text-input');
+  const chooseImage = document.getElementById('meme-insert');
+  const textMemeOut = document.getElementById('meme-text');
+  const containerImage = document.getElementById('meme-image');
+  let bF = document.getElementById('fire');
+  let bW = document.getElementById('water');
+  let bE = document.getElementById('earth');
   textMemeIn.addEventListener('keyup', function () { textMemeOut.innerText = textMemeIn.value; });
   chooseImage.addEventListener('change', function (event) {
     containerImage.src = URL.createObjectURL(event.target.files[0]);
-    containerImage.onload = function () { URL.revokeObjectURL(containerImage.src); }
-  });  
-    bF.addEventListener('click', function () {
-    bF = document.getElementById('meme-image-container'); bF.style.border = '3px dashed red';
+    containerImage.onload = function () { URL.revokeObjectURL(containerImage.src); };
   });
+  bF.addEventListener('click', function () {
+    bF = document.getElementById('meme-image-container'); bF.style.border = '3px dashed red';
+    });
   bW.addEventListener('click', function () {
     bW = document.getElementById('meme-image-container'); bW.style.border = '5px double blue';
   });
