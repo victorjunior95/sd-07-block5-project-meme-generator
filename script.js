52871599a489da4dfe8cefda33b94282003fb934
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-09-13 16:10:35 -0300

    Removendo display 13/09/2020

--- script.js
+++ script.js
@@ -8,29 +8,15 @@ const buttonRed = document.querySelector('#fire');
 const buttonBlue = document.querySelector('#water');
 const buttonBrown = document.querySelector('#earth');
 
-// CONDITION TO CHANGE THE DISPLAY CONTAINER CSS
-let imgShow = false;
-if (imgShow === false) {
-  containerMeme.style.display = 'none';
-}
-
 // EVENT TO SELECT A IMAGE IN MY CONTAINER
 function selectImg(event) {
   img.src = URL.createObjectURL(event.target.files[0]);
-  imgShow = true;
-  if (imgShow === true) {
-    containerMeme.style.display = 'block';
-  }
 }
 loadImg.addEventListener('change', selectImg);
 
 // EVENT TO ADD TEXT IN MY CONTAINER
 function addText() {
   loadText.innerText = textInput.value;
-  imgShow = true;
-  if (imgShow === true) {
-    containerMeme.style.display = 'block';
-  }
 }
 textInput.addEventListener('keyup', addText);
 
@@ -51,44 +37,24 @@ function changeBorderEarh() {
 buttonBrown.addEventListener('click', changeBorderEarh);
 
 // FUNCTION FOR PRE-IMAGES
-const preImgs = document.querySelectorAll('.miniatureImg');
+let preImgs = document.querySelectorAll('.miniatureImg');
 
 preImgs[0].addEventListener('click', function () {
   img.src = preImgs[0].src;
-  imgShow = true;
-  if (imgShow === true) {
-    containerMeme.style.display = 'block';
-  }
 });
 
 preImgs[1].addEventListener('click', function () {
   img.src = preImgs[1].src;
-  imgShow = true;
-  if (imgShow === true) {
-    containerMeme.style.display = 'block';
-  }
 });
 
 preImgs[2].addEventListener('click', function () {
   img.src = preImgs[2].src;
-  imgShow = true;
-  if (imgShow === true) {
-    containerMeme.style.display = 'block';
-  }
 });
 
 preImgs[3].addEventListener('click', function () {
   img.src = preImgs[3].src;
-  imgShow = true;
-  if (imgShow === true) {
-    containerMeme.style.display = 'block';
-  }
 });
 
 preImgs[4].addEventListener('click', function () {
   img.src = preImgs[4].src;
-  imgShow = true;
-  if (imgShow === true) {
-    containerMeme.style.display = 'block';
-  }
 });
