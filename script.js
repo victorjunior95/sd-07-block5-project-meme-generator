b1b184a5231dfe85feb3943b7e1e52dcdd9c9edd
Ana Karine <anakarine.ti@gmail.com>
2020-09-13 16:26:04 -0300

    requisito 6: customizando a borda do meme

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-// Input
 const textInput = document.querySelector('#text-input');
-
-// Output
 const memeText = document.querySelector('#meme-text');
-
-// Function: input --> output
+textInput.addEventListener('keyup', inOut);
 function inOut() {
   memeText.innerHTML = textInput.value;
 }
 
-// Event
-textInput.addEventListener('keyup', inOut);
 
 // Reference: https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
+const image = document.getElementById('meme-image');
 const loadFile = function (event) {
-  const image = document.getElementById('meme-image');
   image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+
+const fireButton = document.getElementById('fire');
+fireButton.addEventListener('click', function () {
+  document.getElementById('meme-image-container').style.border = '3px dashed red';
+});
+
+const waterButton = document.getElementById('water');
+waterButton.addEventListener('click', function () {
+  document.getElementById('meme-image-container').style.border = '5px double blue';
+});
+
+const earthButton = document.getElementById('earth');
+earthButton.addEventListener('click', function () {
+  document.getElementById('meme-image-container').style.border = '6px groove green';
+});
