d51aef1a3c6afd0074e70fe80d804d9ead4b3778
Ana Karine <anakarine.ti@gmail.com>
2020-09-13 20:46:20 -0300

    requisito 7: memes prontos

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ function inOut() {
 }
 
 
-// Reference: https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
-const image = document.getElementById('meme-image');
-const loadFile = function (event) {
-  image.src = URL.createObjectURL(event.target.files[0]);
+// https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+const loadFile = function(event) {
+  const output = document.getElementById('meme-image');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function() {
+    URL.revokeObjectURL(output.src)
+  }
 };
 
 
@@ -26,4 +29,25 @@ waterButton.addEventListener('click', function () {
 const earthButton = document.getElementById('earth');
 earthButton.addEventListener('click', function () {
   document.getElementById('meme-image-container').style.border = '6px groove green';
+});
+
+
+const meme1 = document.getElementById('meme-1');
+meme1.addEventListener('click', function () {
+  document.getElementById('meme-image').src = './imgs/meme1.png';
+});
+
+const meme2 = document.getElementById('meme-2');
+meme2.addEventListener('click', function () {
+  document.getElementById('meme-image').src = './imgs/meme2.png';
+});
+
+const meme3 = document.getElementById('meme-3');
+meme3.addEventListener('click', function () {
+  document.getElementById('meme-image').src = './imgs/meme3.png';
+});
+
+const meme4 = document.getElementById('meme-4');
+meme4.addEventListener('click', function () {
+  document.getElementById('meme-image').src = './imgs/meme4.png';
 });
