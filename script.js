68c9862fb3d63d4e992b34ec60c156e6c594106c
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-13 15:31:47 -0300

    criando campo container e posicionando texto

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const script = document.querySelector('#text-input');
 
+//Recupera o local onde quero minha string
+script.addEventListener('keyup', function () {
+    document.querySelector('#meme-text').innerText = script.value;
+});
