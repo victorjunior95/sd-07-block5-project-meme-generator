3ea8470dbd780632c7c1ab5c90963a9240b1126f
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-09-13 23:55:24 -0300

    Code Climate Try 0.1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let defaultMeme = document.querySelector('#meme-image');
+const defaultMeme = document.querySelector('#meme-image');
 
-//Getting the input value and placing it in the image.
-document.querySelector('input#text-input').addEventListener('input', function() {
+//  Getting the input value and placing it in the image.
+document.querySelector('input#text-input').addEventListener('input', function () {
   document.querySelector('#meme-text').innerText = document.querySelector('input#text-input').value;
 });
 
-//Changing the border.
-let containerBorder = document.querySelector('#meme-image-container');
+//  Changing the border.
+const containerBorder = document.querySelector('#meme-image-container');
 
 /*
 ---Buttons
@@ -15,19 +15,19 @@ let containerBorder = document.querySelector('#meme-image-container');
 --Earth
 --Reset
 */
-document.querySelector('#fire').addEventListener('click', function() {
+document.querySelector('#fire').addEventListener('click', function () {
   containerBorder.style.border = '3px dashed red';
 });
 
-document.querySelector('#water').addEventListener('click', function() {
+document.querySelector('#water').addEventListener('click', function () {
   containerBorder.style.border = '5px double blue';
 });
 
-document.querySelector('#earth').addEventListener('click', function() {
+document.querySelector('#earth').addEventListener('click', function () {
   containerBorder.style.border = '6px groove green';
 });
 
-document.querySelector('#reset').addEventListener('click', function() {
+document.querySelector('#reset').addEventListener('click', function () {
   containerBorder.style.border = '1px solid black';
 });
 
@@ -35,18 +35,18 @@ document.querySelector('#reset').addEventListener('click', function() {
 ---Default Imgs
 */
 
-document.querySelector('#meme-1').addEventListener('click', function() {
+document.querySelector('#meme-1').addEventListener('click', function () {
   defaultMeme.src = document.querySelector('#meme-1').src;
 });
 
-document.querySelector('#meme-2').addEventListener('click', function() {
+document.querySelector('#meme-2').addEventListener('click', function () {
   defaultMeme.src = document.querySelector('#meme-2').src;
 });
 
-document.querySelector('#meme-3').addEventListener('click', function() {
+document.querySelector('#meme-3').addEventListener('click', function () {
   defaultMeme.src = document.querySelector('#meme-3').src;
 });
 
-document.querySelector('#meme-4').addEventListener('click', function() {
-  defaultMeme.src = document.querySelector('#meme-4').src
+document.querySelector('#meme-4').addEventListener('click', function () {
+  defaultMeme.src = document.querySelector('#meme-4').src;
 });
