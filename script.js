322ddec8330498d9daade8a4422a8095745e0016
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-14 11:35:58 -0300

    Fix miniaturas

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-let text = document.querySelector("#text-input")
-let textOnPhoto = document.querySelector("#meme-text")
-let imagem = document.querySelector("#meme-insert")
-let imagemPhoto = document.querySelector("#meme-image")
-let howToUse = document.querySelector(".howToUse")
-let image1 = document.querySelectorAll("img")[1]
-let image2 = document.querySelectorAll("img")[2]
-let image3 = document.querySelectorAll("img")[3]
-let image4 = document.querySelectorAll("img")[4]
+const text = document.querySelector('#text-input')
+const textOnPhoto = document.querySelector('#meme-text')
+const imagem = document.querySelector('#meme-insert')
+const imagemPhoto = document.querySelector('#meme-image')
+const howToUse = document.querySelector('.howToUse')
+const image1 = document.querySelectorAll('img')[1]
+const image2 = document.querySelectorAll('img')[2]
+const image3 = document.querySelectorAll('img')[3]
+const image4 = document.querySelectorAll('img')[4]
 
 
-text.addEventListener("keyup", function(){
-    return textOnPhoto.innerText = text.value
-})
-imagem.addEventListener("change", function(){
-    return imagemPhoto.src = URL.createObjectURL(event.target.files[0])
-})
+text.addEventListener('keyup', function (){
+  textOnPhoto.innerText = text.value;
+});
+imagem.addEventListener('change', function (){
+  imagemPhoto.src = URL.createObjectURL(event.target.files[0]);
+});
 
-image1.addEventListener("click", function(){
-    imagemPhoto.src = image1.src
-})
-image2.addEventListener("click", function(){
-    imagemPhoto.src = image2.src
-})
-image3.addEventListener("click", function(){
-    imagemPhoto.src = image3.src
-})
+image1.addEventListener('click', function (){
+  imagemPhoto.src = image1.src;
+});
+image2.addEventListener('click', function (){
+  imagemPhoto.src = image2.src;
+});
+image3.addEventListener('click', function (){
+  imagemPhoto.src = image3.src;
+});
 image4.addEventListener("click", function(){
-    imagemPhoto.src = image4.src
-})
+  imagemPhoto.src = image4.src;
+});
