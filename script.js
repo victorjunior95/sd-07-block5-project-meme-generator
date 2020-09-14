833428d34af19e34025b313d4146193f7eb92cf5
Eduardo Pizorno <pizorno@gmail.com>
2020-09-13 23:35:26 -0300

    Meme generator Refeito 0.0.6

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function getInput() {
-  const textInput = document.getElementById("text-input").value;
-  const textImage = document.getElementsByTagName("p")[0];
-  textImage.innerHTML = textInput.toUpperCase()
+  document.getElementsByTagName("p")[0].innerHTML = document.getElementById("text-input").value.toUpperCase();
 }
 
 document.getElementById("text-input").addEventListener("change", getInput);
 
-
 function getImage(e) {
-  const memeImage = document.getElementById('meme-image');
+  const memeImage = document.getElementById("meme-image");
   memeImage.src = URL.createObjectURL(e.target.files[0]);
 }
 
-document.getElementById('meme-insert').addEventListener('change', getImage);
+document.getElementById('meme-insert').addEventListener("change", getImage);
