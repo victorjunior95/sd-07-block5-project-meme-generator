a6dc671b52463ff64fd66144200c435f8da1d551
Rita Jeveaux <ritajeveaux@gmail.com>
2020-09-13 23:11:16 -0300

    revendo o JS

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+function loadFile(event) {
+  const loadImage = document.getElementById("meme-image");
+  loadImage.src = URL.createObjectURL(event.target.files[0]);
+}
+
 const textInput = document.getElementById("text-input");
 
 textInput.addEventListener("input", function () {
-const memeTextImage = document.getElementById("meme-text");
-memeTextImage.innerHTML = textInput.value
-})    
+  const memeTextImage = document.getElementById("meme-text");
+  memeTextImage.innerHTML = textInput.value
+});
+
+const buttonChoice = document.getElementById("meme-insert");
+
+buttonChoice.addEventListener("change", function(event) {
+  loadFile(event);
+});
