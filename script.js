8443fecd4257387c7a58fbeb897fe979545b26b9
luc-zago <luc.zago@hotmail.com>
2020-09-14 18:20:55 -0300

    Tirando a função de deixar o texto do meme em caixa alta para ver se o testador encontra o texto

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
-window.onload = function(){
+window.onload = function() {
 
   function memeTxt() {
-    const memeText = document.getElementById('text-input').value.toUpperCase();
+    const memeText = document.getElementById('text-input').value //.toUpperCase();
     document.getElementById('meme-text').innerText = memeText;
     return document.getElementById('meme-text').innerText;
-}
+  }
 
   function memeImg() {
     const memeFile = document.getElementsByTagName('input')[1].files[0];
     const memeURL = URL.createObjectURL(memeFile);
     document.getElementsByTagName('img')[0].src = memeURL;
     return document.getElementsByTagName('img')[0].src;
-}
+  }
 
   document.getElementById('text-input').addEventListener('keyup', memeTxt);
   document.getElementById('meme-insert').addEventListener('change', memeImg);
-}
+};
