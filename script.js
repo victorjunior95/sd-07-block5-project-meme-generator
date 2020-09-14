302e512cad0854822b025d76a01128bfc30ff0c4
Rodrigo <rd64103@gmail.com>
2020-09-14 13:47:41 -0300

    dessa vez vai.... com ajuda do plantao

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function porFrase() {
-    let frase = document.getElementById('text-input').value
-    document.getElementById('meme-text').innerText = frase
-}
-let botaoTexto = document.getElementById('enviarTexto');
-botaoTexto.addEventListener('click', porFrase)
+const frase = document.getElementById('text-input');
+const linha = document.getElementById('meme-text');
+frase.addEventListener('keyup', function (event) {
+    linha.innerHTML = event.target.value;
+})
