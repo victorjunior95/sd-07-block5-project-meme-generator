d59e616e9ffd3c8e1fa70d1d74ad6f414f4bd336
Vitor Rodrigues <vitor_rc@outlook.com>
2020-09-14 13:53:14 -0300

    Ajuste no script.js

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-window.onload = function () {
-  let textInput = document.querySelector('#text-input');
-  let memeText = document.querySelector('#meme-text');
-  let memeImage = document.querySelector('#meme-image');
-  let memeInsert = document.querySelector('#meme-insert');
-  let memeContainer = document.querySelector('#meme-image-container');
-  let fire = document.querySelector('#fire');
-  let water = document.querySelector('#water');
-  let earth = document.querySelector('#earth');
-  let famousMemes = document.querySelectorAll('#famous-memes img');
+const textInput = document.querySelector('#text-input');
+const memeText = document.querySelector('#meme-text');
+const memeImage = document.querySelector('#meme-image');
+const memeInsert = document.querySelector('#meme-insert');
+const memeContainer = document.querySelector('#meme-image-container');
+const fire = document.querySelector('#fire');
+const water = document.querySelector('#water');
+const earth = document.querySelector('#earth');
+const famousMemes1 = document.querySelectorAll('#famous-memes img')[0];
+const famousMemes2 = document.querySelectorAll('#famous-memes img')[1];
+const famousMemes3 = document.querySelectorAll('#famous-memes img')[2];
+const famousMemes4 = document.querySelectorAll('#famous-memes img')[3];
 
-  textInput.addEventListener('input', function () { memeText.innerText = textInput.value; });
 
-  memeInsert.addEventListener('change', function () {
-    // codigo abaixo tirei como referecia da Seção Dicas para mostrar a imagem
-    // https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
-    memeImage.src = URL.createObjectURL(memeInsert.files[0]);
-    memeImage.onload = function () { URL.revokeObjectURL(memeImage.src); };
-  });
+textInput.addEventListener('input', function () { memeText.innerText = textInput.value; });
 
-  fire.addEventListener('click', function () { memeContainer.style.border = '3px dashed red'; });
-  water.addEventListener('click', function () { memeContainer.style.border = '5px double blue'; });
-  earth.addEventListener('click', function () { memeContainer.style.border = '6px groove green'; });
+memeInsert.addEventListener('change', function () {
+  // codigo abaixo tirei como referecia da Seção Dicas para mostrar a imagem
+  // https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+  memeImage.src = URL.createObjectURL(memeInsert.files[0]);
+  memeImage.onload = function () { URL.revokeObjectURL(memeImage.src); };
+});
 
-  for (let count = 0; count < famousMemes.length; count += 1) {
-    famousMemes[count].addEventListener('click', function (event) {
-      memeImage.src = event.target.src;
-    });
-  }
-};
+fire.addEventListener('click', function () { memeContainer.style.border = '3px dashed red'; });
+water.addEventListener('click', function () { memeContainer.style.border = '5px double blue'; });
+earth.addEventListener('click', function () { memeContainer.style.border = '6px groove green'; });
+
+famousMemes1.addEventListener('click', function () {
+  memeImage.src = famousMemes1.src;
+});
+
+famousMemes2.addEventListener('click', function () {
+  memeImage.src = famousMemes2.src;
+});
+
+famousMemes3.addEventListener('click', function () {
+  memeImage.src = famousMemes3.src;
+});
+
+famousMemes4.addEventListener('click', function () {
+  memeImage.src = famousMemes4.src;
+});
