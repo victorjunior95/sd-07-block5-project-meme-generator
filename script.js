cd3f66ea7535064e0361f792b4a8da599228a94d
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-13 23:39:05 -0300

    execução do requisito 02

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let getInputText = document.getElementById('text-input');
+const getInputText = document.getElementById('text-input');
+const getDivImage = document.getElementById('meme-image-container');
 
 getInputText.oninput = function () {
-  let getParagraph = document.getElementById('meme-text');
-  let textInput = getInputText.value;
+  const getParagraph = document.getElementById('meme-text');
+  const textInput = getInputText.value;
   getParagraph.innerText = textInput;
+};
+
+const loadFile = function(event) {
+  const outputImage = document.getElementById('meme-image');
+  outputImage.src = URL.createObjectURL(event.target.files[0]);
+  outputImage.onload = function() {
+    URL.revokeObjectURL(outputImage.src);
+  }
+  getDivImage.style.backgroundImage = outputImage.src;
 }
