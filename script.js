06dd5102276b48c80637d6b19ab726a28814eea7
menezesandre <dehhmenezes87@gmail.com>
2020-09-14 20:28:08 -0300

    Border buttons

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function insertText() {
     textOutput.innerText = textInput.value;
   });
 }
+
+function borderFire() {
+  let btnFire = document.querySelector("#fire");
+  btnFire.addEventListener("click", function() {
+    let border = document.querySelector("#meme-image-container");
+    border.className = "border-fire";
+  });
+}
+
+function borderWater() {
+  let btnWater = document.querySelector("#water");
+  btnWater.addEventListener("click", function() {
+    let border = document.querySelector("#meme-image-container");
+    border.className = "border-water";
+  });
+}
+
+function borderEarth() {
+  let btnEarth = document.querySelector("#earth");
+  btnEarth.addEventListener("click", function() {
+    let border = document.querySelector("#meme-image-container");
+    border.className = "border-earth";
+  });
+}
