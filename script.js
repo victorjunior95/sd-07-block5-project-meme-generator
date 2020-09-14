7c9ed67fddf4e840e7ce03b6e414c6ac27710017
richardfazeredo <richardcompletetour@gmail.com>
2020-09-14 11:49:42 -0300

    Adicionando recurso para fazer upload de uma imagem local e renderizando-a dentro de uma div, com texto digitado sobre a mesma

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let textMeme = document.getElementById('text-input');
 let memeText = document.getElementById('meme-text');
+let memeInsert = document.getElementById('meme-insert');
 
 textMeme.addEventListener('keyup', function (){
   memeText.innerText = textMeme.value;
 });
+
