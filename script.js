1fa4ee59932c070a198caec1bb593d23f1d63f73
Galaraz <jvictor.si.2013@gmail.com>
2020-09-14 01:59:00 -0300

    js ajustado agora rumo aos requisitos

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
   function carregarTexto() {
-    const textInput = document.getElementById("text-input").value;
-    const memeText = document.getElementById("meme-text");
+    const textInput = document.getElementById('text-input').value;
+    const memeText = document.getElementById('meme-text');
     memeText.innerText = textInput;
   }
 
   function carregarImagemPc(event) {
-    const loadImg = document.getElementById("meme-image");
+    const loadImg = document.getElementById('meme-image');
     loadImg.src = URL.createObjectURL(event.target.files[0]);
     loadImg.onload = function () {
       URL.revokeObjectURL(loadImg.src);
@@ -14,7 +14,7 @@
   }
 
   function CarregaMeme(event) {
-    const loadImg = document.getElementById("meme-image");
+    const loadImg = document.getElementById('meme-image');
     loadImg.src = event.target.src;
   }
   document
@@ -29,26 +29,27 @@
 
 
   function editarBorda(event) {
-    let mudaBorda = "";
+    let mudaBorda = '';
     switch (event.target.id) {
-      case "fire":
-        mudaBorda = "meme-image-container fire-border";
+      case 'fire':
+        mudaBorda = 'meme-image-container fire-border';
         break;
-      case "water":
-        mudaBorda = "meme-image-container water-border";
+      case 'water':
+        mudaBorda = 'meme-image-container water-border';
         break;
-      case "earth":
-        mudaBorda = "meme-image-container earth-border";
+      case 'earth':
+        mudaBorda = 'meme-image-container earth-border';
+        break;
       default:
-        mudaBorda = "meme-image-container default-border";
+        mudaBorda = 'meme-image-container default-border';
     }
 
-    document.getElementById("meme-image-container").className = mudaBorda;
+    document.getElementById('meme-image-container').className = mudaBorda;
   }
 
   //falta fazer a busca buscar
 
-  const buttonElementos = document.getElementById('button');
+  const buttonElementos = document.getElementsByTagName('button');
   for (let index = 0; index < buttonElementos.length; index+=1) {
     buttonElementos[index].addEventListener('click',editarBorda);
   }
