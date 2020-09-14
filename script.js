fd7ad8eea4e2388ac59bfbfaa4136b48a26db90f
Renato Silva <renatosdev@gmail.com>
2020-09-14 18:35:30 -0300

    Requisito Obrigatorio 1 e 2 - Correçao 2 do item 1

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-window.onload = function () {
+window.onload = function textMeme() {
+    let textImput = document.getElementById("text-input").value;
+    let memeText = document.getElementById("meme-text");
+    memeText.innerHTML = textImput
+
+    let memeText = document.getElementById("text-input");
+    memeText.addEventListener("keyup", textMeme);
+
+    function imageMeme(event) {
+        let memeIsert= document.getElementById("meme-insert");
+        memeIsert.src = URL.createObjetctURL(event.target.files[0]);
+        memeInsert.onload = function () {
+            URL.revokeObjectURL(memeIsert.src);
+        };
+    } ;
     
+    let meme = document.getElementById("image");
+    meme.addEventListener(change, imageMeme);
 }
+
