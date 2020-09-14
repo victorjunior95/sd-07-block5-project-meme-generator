2ad4f9db3304a7b117a098251a1ea028ce89369d
luc-zago <luc.zago@hotmail.com>
2020-09-14 17:14:42 -0300

    Criada a possibilidade de se fazer upload de imagem e inserir texto sobre a imagem

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 
 window.onload = function() {
 document.getElementById('txt-input').addEventListener('keyup', memeTxt);
+document.getElementById('meme-insert').addEventListener('change', memeImg);
 
 function memeTxt() {
-    memeText = document.getElementById('txt-input').value.toUpperCase();
+    let memeText = document.getElementById('txt-input').value.toUpperCase();
     return document.getElementById('meme-text').innerText = memeText;
+}
+
+function memeImg() {
+    let memeFile = document.getElementsByTagName('input')[1].files[0];
+    let memeURL = URL.createObjectURL(memeFile);
+    document.getElementsByTagName('img')[0].style.width = 500 + 'px';
+    document.getElementsByTagName('img')[0].style.height = 550 + 'px';
+    return document.getElementsByTagName('img')[0].src = memeURL;
 }
 }
