419b400c01494171ae51450d4366a36e7217638b
luc-zago <luc.zago@hotmail.com>
2020-09-14 17:50:02 -0300

    Feitas adequações ao code climate

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 
-window.onload = function() {
-document.getElementById('txt-input').addEventListener('keyup', memeTxt);
-document.getElementById('meme-insert').addEventListener('change', memeImg);
+window.onload = function(){
 
-function memeTxt() {
-    let memeText = document.getElementById('txt-input').value.toUpperCase();
-    return document.getElementById('meme-text').innerText = memeText;
+  function memeTxt() {
+    const memeText = document.getElementById('txt-input').value.toUpperCase();
+    document.getElementById('meme-text').innerText = memeText;
+    return document.getElementById('meme-text').innerText;
 }
 
-function memeImg() {
-    let memeFile = document.getElementsByTagName('input')[1].files[0];
-    let memeURL = URL.createObjectURL(memeFile);
-    document.getElementsByTagName('img')[0].style.width = 500 + 'px';
-    document.getElementsByTagName('img')[0].style.height = 550 + 'px';
-    return document.getElementsByTagName('img')[0].src = memeURL;
+  function memeImg() {
+    const memeFile = document.getElementsByTagName('input')[1].files[0];
+    const memeURL = URL.createObjectURL(memeFile);
+    document.getElementsByTagName('img')[0].src = memeURL;
+    return document.getElementsByTagName('img')[0].src;
 }
+
+  document.getElementById('txt-input').addEventListener('keyup', memeTxt);
+  document.getElementById('meme-insert').addEventListener('change', memeImg);
 }
