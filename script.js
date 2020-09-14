69d215281358378e416fa91af495390d393ce467
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-14 11:15:52 -0300

    Adicionado miniaturas

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let text = document.querySelector("#text-input")
 let textOnPhoto = document.querySelector("#meme-text")
 let imagem = document.querySelector("#meme-insert")
 let imagemPhoto = document.querySelector("#meme-image")
+let howToUse = document.querySelector(".howToUse")
+let image1 = document.querySelectorAll("img")[1]
+let image2 = document.querySelectorAll("img")[2]
+let image3 = document.querySelectorAll("img")[3]
+let image4 = document.querySelectorAll("img")[4]
 
 
 text.addEventListener("keyup", function(){
@@ -9,4 +14,17 @@ text.addEventListener("keyup", function(){
 })
 imagem.addEventListener("change", function(){
     return imagemPhoto.src = URL.createObjectURL(event.target.files[0])
+})
+
+image1.addEventListener("click", function(){
+    imagemPhoto.src = image1.src
+})
+image2.addEventListener("click", function(){
+    imagemPhoto.src = image2.src
+})
+image3.addEventListener("click", function(){
+    imagemPhoto.src = image3.src
+})
+image4.addEventListener("click", function(){
+    imagemPhoto.src = image4.src
 })
