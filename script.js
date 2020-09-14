d2353f512316765ce7f18665c54eb885b0434f34
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-09-14 12:37:44 -0300

    Iniciando o projeto #VQV

---
+++ script.js
@@ -0,0 +1,5 @@
+let textoEntrada = document.getElementById("text-input").value;
+let imagem = document.getElementById("meme-insert").value;
+let textoSaida = document.getElementById("meme-text");
+
+textoSaida.innerText= textoEntrada
