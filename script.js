7fa33956d669c3881d86d4642f53c089f04c0495
MarceloBorges <melombm@hotmail.com>
2020-09-14 18:58:55 -0500

    exercise 2 - adicinar função para direcionar  a imagem para  id que desejo

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+// criado variaveis
 const yourtext = document.getElementById("text-input");
 let youtcontent = document.getElementById("meme-text");
+
 /*
 configura uma função que será chamada sempre que o evento especificado for entregue ao destino.
 referencia 
 https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener 
 */
+
 yourtext.addEventListener("keyup", function(typeLetters){
     youtcontent.innerText = typeLetters.target.value;
 })
 
+// criado variavel para guarda a imagem e a url
+let yourImg = document.getElementById("meme-insert");
+let imageContent = document.getElementById("meme-image");
+
+
+// função para enviar a image para o id meme-image
+yourImg.addEventListener("change", function(){
+    imageContent.src = window.URL.createObjectURL(this.files[0]);
+})
