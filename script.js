66eecd94a87674fe8da6fdc35eb885955ed1ee65
Vitor Rodrigues <vitor_rc@outlook.com>
2020-09-14 13:28:38 -0300

    Refatoração no script.js, alteração do src do memes predefinidos e ajuste para ordem alfabetica no style

--- script.js
+++ script.js
@@ -1,44 +1,30 @@
 window.onload = function () {
-  let textInput = document.querySelector('#text-input');
-  let memeText = document.querySelector('#meme-text');
-  let memeImage = document.querySelector('#meme-image');
-  let memeInsert = document.querySelector('#meme-insert');
-  let memeContainer = document.querySelector('#meme-image-container');
-  let fire = document.querySelector('#fire');
-  let water = document.querySelector('#water');
-  let earth = document.querySelector('#earth');
-  let famousMemes = document.querySelectorAll('#famous-memes img')
+  const textInput = document.querySelector('#text-input');
+  const memeText = document.querySelector('#meme-text').innerText;
+  const memeImage = document.querySelector('#meme-image');
+  const memeInsert = document.querySelector('#meme-insert');
+  const memeContainer = document.querySelector('#meme-image-container').style.border;
+  const fire = document.querySelector('#fire');
+  const water = document.querySelector('#water');
+  const earth = document.querySelector('#earth');
+  const famousMemes = document.querySelectorAll('#famous-memes img');
 
-  textInput.addEventListener('input', function () {
-    memeText.innerText = textInput.value;
-  });
+  textInput.addEventListener('input', function () { memeText = textInput.value; });
 
   memeInsert.addEventListener('change', function () {
     // codigo abaixo tirei como referecia da Seção Dicas para mostrar a imagem
     // https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
     memeImage.src = URL.createObjectURL(memeInsert.files[0]);
-    memeImage.onload = function () {
-      URL.revokeObjectURL(memeImage.src);
-    };
+    memeImage.onload = function () { URL.revokeObjectURL(memeImage.src); };
   });
 
-  fire.addEventListener('click', function () {
-    memeContainer.style.border = '3px dashed red';
-  });
+  fire.addEventListener('click', function () { memeContainer = '3px dashed red'; });
+  water.addEventListener('click', function () { memeContainer = '5px double blue'; });
+  earth.addEventListener('click', function () { memeContainer = '6px groove green'; });
 
-  water.addEventListener('click', function () {
-    memeContainer.style.border = '5px double blue';
-  });
-
-  earth.addEventListener('click', function () {
-    memeContainer.style.border = '6px groove green';
-  });
-
-  function insertFamousMeme (event) {
-    memeImage.src = event.target.src
-  }
-
-  for(let count = 0 ; count < famousMemes.length; count += 1) {
-    famousMemes[count].addEventListener('click', insertFamousMeme)
+  for (let count = 0; count < famousMemes.length; count += 1) {
+    famousMemes[count].addEventListener('click', function (event) {
+      memeImage.src = event.target.src;
+    });
   }
 };
