f6b853c559063ad83b0f0a17493465757bfe834c
Arthur Nunes <coletivosuburbano@gmail.com>
2020-09-14 02:56:33 -0300

    opcional 1

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
+const imgContainer = document.getElementById('meme-image-container');
 
 textInput.addEventListener('input', function () {
   memeText.innerText = textInput.value;
@@ -13,3 +17,15 @@ const carregarImagem = function (event) {
   };
   leitor.readAsDataURL(event.target.files[0]);
 };
+
+fire.addEventListener('click', function () {
+  imgContainer.style.border = '3px dashed red';
+});
+
+water.addEventListener('click', function () {
+  imgContainer.style.border = '5px double blue';
+});
+
+earth.addEventListener('click', function () {
+  imgContainer.style, border = '6px groove green';
+});
