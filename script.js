1d21357567dab23ebfc765020cb2578d8e11a4c0
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-13 21:35:14 -0400

    corrigida função insere texto

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ window.onload = function () {
   // Função para colocar foto no meme
   const carregaFoto = document.getElementById('meme-insert');
   carregaFoto.addEventListener('input', function (event) {
-    const url = URL.createObjectURL(event.target.files[0]);
-    const saida = document.getElementById('meme-image');
-    saida.src = url;
-    saida.style.height = '450px';
-    saida.style.width = '450px';
-  });
+      const url = URL.createObjectURL(event.target.files[0]);
+      const saida = document.getElementById('meme-image');
+      saida.src = url;
+      saida.style.height = '450px';
+      saida.style.width = '450px';
+    });
   // Função para colocar texto no meme
   const lbltexto = document.getElementById('text-input');
-  lbltexto.addEventListener('change', function () {
+  lbltexto.addEventListener('keyup', function () {
     const texto = document.getElementById('text-input').value;
-    document.getElementById('meme-text').innerHTML = texto;
+    document.getElementById('meme-text').innerText = texto;
   });
 };
