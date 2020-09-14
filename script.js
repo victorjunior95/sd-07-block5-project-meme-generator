048cf59df3245611cff73090efd35261fb9b5f5e
Leonardo <lecastro16@gmail.com>
2020-09-14 20:59:17 -0300

    Leonardo Sardinha: Estruturando V7

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+let memeInsert = document.getElementById('meme-insert');
 
 textInput.addEventListener('input', function () {
   memeText.innerText = textInput.value;
 });
+
+//Resolução baseada na dica do exercício
+let loadFile = function(event){
+    let reader = new FileReader();
+    reader.onload = function () {
+        let memeImage = document.getElementById('meme-image');
+        memeImage.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+};
