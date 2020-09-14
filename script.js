c9daa9702c2d2bc96aef6ae7c3a8c428e7f97aad
menezesandre <dehhmenezes87@gmail.com>
2020-09-14 17:21:45 -0300

    Corrigido CSS e JS

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-function insertText() {
-  let textInput = document.querySelector("#text-input");
-  let textOutput = document.createElement("span");
-  document.querySelector("#meme-image-container").appendChild(textOutput);  
-  textOutput.id = "meme-text";
-  textOutput.style.position = "absolute";
-  textOutput.style.textAlign = "center";
-  textOutput.style.color = "white";
-  textOutput.style.fontSize = "20pt";
-  textOutput.style.paddingBottom = "10px";
-  textOutput.style.bottom = "0";
-  textOutput.style.textShadow = "3px 3px 3px black";
-  textInput.addEventListener("keyup", function() {
-    textOutput.innerText = textInput.value;
-  });
-}
-
 function insertImage() {
   let imageInput = document.querySelector("#meme-insert");
   
   imageInput.addEventListener("change", function() {
     let imageOutput = document.createElement("img");
     imageOutput.id = "meme-image";
-    imageOutput.alt = "Imagem do meme";
-    imageOutput.style.width = "100%";
-    imageOutput.style.height = "100%";
+    imageOutput.className = "imageBox";
+    imageOutput.alt = "Imagem do meme";    
     imageOutput.src = URL.createObjectURL(this.files[0]);
     document.querySelector("#meme-image-container").appendChild(imageOutput);
+  });
+}
+
+function insertText() {
+  let textInput = document.querySelector("#text-input");
+  let textOutput = document.createElement("span");
+  document.querySelector("#meme-image-container").appendChild(textOutput);  
+  textOutput.id = "meme-text";
+  textOutput.className = "textBox";
+  
+  textInput.addEventListener("keyup", function() {
+    textOutput.innerText = textInput.value;
   });
 }
