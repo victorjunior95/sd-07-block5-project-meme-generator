0174979c7afe628bd759f9cfb19c4af72c14a924
Rodrigo <rd64103@gmail.com>
2020-09-14 17:08:11 -0300

    segundo passo

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const frase = document.getElementById('text-input');
 const linha = document.getElementById('meme-text');
-frase.addEventListener('keyup', function (event) {
+frase.addEventListener('keyup', function(event) {
     linha.innerHTML = event.target.value;
+})
+const foto = document.getElementById('meme-insert');
+const painel = document.getElementById('meme-image');
+
+foto.addEventListener('change', function() {
+    painel.src = window.URL.createObjectURL(this.files[0]);
 })
