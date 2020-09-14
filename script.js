5da3d10b8895fe713684a1af95ca779c7c3cb5dc
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-09-13 23:35:31 -0300

    Finalizando html e inserindo css e variaveis js

---
+++ script.js
@@ -0,0 +1,5 @@
+//Inserindo variáveis (let) nas tag's selecionadas do html
+let inputTag = document.querySelector("#meme-insert");
+let inputText = document.querySelector("#text-input");
+let imgMeme = document.querySelector("#meme-image");
+let memeText = document.querySelector("#meme-text");
