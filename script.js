70ad80ca6a01161bedf336cd403f063612f62ef2
Demistocles Hiago <demi.treze@gmail.com>
2020-09-14 18:21:40 -0300

    fazendo últimas alterações

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function loadFile(event) {
-   const Image = document.getElementById("meme-image");
-   Image.src = URL.createObjectURL(event.target.files[0]);
+   const loadImage = document.getElementById("meme-image");
+   loadImage.src = URL.createObjectURL(event.target.files[0]);
  }
-
-const textInput = document.getElementById('text-input');
-
-textInput.addEventListener('input', function() {
-   memeText = document.getElementById('meme-text')
-   memeText.innerHTML = textInput.value
-});
-
-const buttonChoice = document.getElementById("meme-insert");
-
-buttonChoice.addEventListener("change", function(event) {
-  loadFile(event);
-});
+ 
+ const textInput = document.getElementById("text-input");
+ 
+ textInput.addEventListener("input", function () {
+   const memeTextImage = document.getElementById("meme-text");
+   memeTextImage.innerHTML = textInput.value
+ });
+ 
+ const buttonChoice = document.getElementById("meme-insert");
+ 
+ buttonChoice.addEventListener("change", function(event) {
+   loadFile(event);
+ });
