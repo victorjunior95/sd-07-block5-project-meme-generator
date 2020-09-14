3cbf4149fb989a6c13f3b8467c9432085b00a9b9
Mauro Bergonzoni Junqueira <mauro@pop-os.localdomain>
2020-09-14 15:47:04 -0300

    Colocando texto e fotos na div

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
 window.onload = function(){
     const containerMeme = document.querySelector('#meme-image-container');
     const textInput = document.querySelector('#text-input');
+    const textToPhoto = document.querySelector('#meme-text');
     const fotoInput = document.querySelector('#input-foto');
     const imgPhoto = document.querySelector('#meme-image');
     fotoInput.addEventListener('change',function(event){
         let file = event.target.files[0];
-        console.log(file);
         let url = URL.createObjectURL(file);
         imgPhoto.src=url;
-    })
+    });
+    textInput.addEventListener('keyup',function(event){
+        textToPhoto.innerHTML = textInput.value;
+
+    });
+
 }
 
-function openImage(){
-    fotoInput = URL.createObjectURL(file);
+    
+function openText(){
+    //textInput = document.getElementById(meme-text);
+    const textInput = document.querySelector('#text-input');
+    
 }
-
-openImage();
