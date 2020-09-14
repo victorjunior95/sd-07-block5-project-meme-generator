e143c82d51b56857fe21de10bb4d97c3298628b8
nyxnil <nsmnilson@hotmail.com>
2020-09-14 15:59:57 -0300

    ajustando a img

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Acompanhando a explicação do GUGA junto com o colega Ander
 let inputText = document.querySelector("#text-input");
 let memeText = document.querySelector("#meme-text");
+let imageMeme = document.querySelector("#meme-image");
+let memeBorder = document.getElementById("meme-image-container");
 
 inputText.addEventListener("keyup", function() {
     memeText.innerHTML = inputText.value;
@@ -11,5 +13,10 @@ let updateFile = function(event) {
     imageMeme.src = URL.createObjectURL(event.target.files[0]);
     imageMeme.onload = function() {
         URL.revokeObjectURL(imageMeme.src)
-    }
+    };
 };
+
+const btnRed = document.getElementById("fire")
+btnRed.addEventListener("click", function() {
+    memeBorder,className = "redborder";
+});
