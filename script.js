051f0440d72a4098f5b89c47ff64debd9526f225
Camila Valentim <camilaifpb@hotmail.com>
2020-09-14 11:20:07 -0300

    upload de uma imagem

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+let image = document.getElementById('meme-image');
+
+let inputImage =document.getElementById('meme-insert');
+
+
 function myFunction() {
   var x = document.getElementById("text-input").value;
   document.getElementById("meme-text").innerHTML = x;
 
 }
+
+
+inputImage.addEventListener('change', function() {
+  inputImage = inputImage.value;
+
+  image.src = URL.createObjectURL(event.target.files[0]);
+})
