ac9ab3894e195b84d65a726ee7363411ab1392ad
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-14 07:11:04 -0300

    execução dos requisitos 06

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const getInputText = document.getElementById('text-input');
 const getDivImage = document.getElementById('meme-image-container');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
 
 getInputText.oninput = function () {
   const getParagraph = document.getElementById('meme-text');
@@ -15,3 +18,18 @@ const loadFile = function (event) {
   };
   getDivImage.style.backgroundImage = outputImage.src;
 };
+
+btnFire.addEventListener('click', function (){
+  let styles = '3px dashed red';
+  getDivImage.style.border = styles;
+});
+
+btnWater.addEventListener('click', function (){
+  let styles = '5px double blue';
+  getDivImage.style.border = styles;
+});
+
+btnEarth.addEventListener('click', function (){
+  let styles = '6px groove green';
+  getDivImage.style.border = styles;
+});
