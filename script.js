80589cddd8820915b70f88a10a5d60232ec395a8
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-13 22:05:50 -0400

    correção na função insere imagem

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ window.onload = function () {
   // Função para colocar foto no meme
   const carregaFoto = document.getElementById('meme-insert');
   carregaFoto.addEventListener('input', function (event) {
-      const url = URL.createObjectURL(event.target.files[0]);
-      const saida = document.getElementById('meme-image');
-      saida.src = url;
-      saida.style.height = '450px';
-      saida.style.width = '450px';
-    });
+    const url = URL.createObjectURL(event.target.files[0]);
+    const saida = document.getElementById('meme-image');
+    saida.src = url;
+  });
   // Função para colocar texto no meme
   const lbltexto = document.getElementById('text-input');
   lbltexto.addEventListener('keyup', function () {
