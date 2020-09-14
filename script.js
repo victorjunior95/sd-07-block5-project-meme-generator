92008b2fd76bf5df03c7594f5c305b181471153f
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-13 21:36:12 -0300

    refluxo

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ const script = document.querySelector('#text-input');
 let setImage = document.querySelector('#meme-insert');
 const picture = document.querySelector('#meme-image');
 
-// adiciona os botões de estilo
-let earth = document.querySelector('#changeButtons').childNodes[7];
-let water = document.querySelector('#changeButtons').childNodes[5];
-let fire = document.querySelector('#changeButtons').childNodes[3];
-let container = document.querySelector('#meme-image-container');
-
 //Recupera o local onde quero minha string
 script.addEventListener('keyup', function () {
     document.querySelector('#meme-text').innerText = script.value;
@@ -17,6 +11,11 @@ setImage.addEventListener('change', function(event){
     setImage = setImage.value;
     picture.src = URL.createObjectURL(event.target.files[0]);
 });
+// adiciona os botões de estilo
+let earth = document.querySelector('.changeButtons').childNodes[7];
+let water = document.querySelector('.changeButtons').childNodes[5];
+let fire = document.querySelector('.changeButtons').childNodes[3];
+let container = document.querySelector('#meme-image-container');
 
 fire.addEventListener('click', function () {
     container.style.border = '3px dashed #DC143C';
