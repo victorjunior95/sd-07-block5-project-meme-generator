78c8ed86acf9edaf173fca14a302eed901e783f7
Camila Valentim <camilaifpb@hotmail.com>
2020-09-14 16:32:50 -0300

    memes pre prontos

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let image = document.getElementById('meme-image');
 let inputImage =document.getElementById('meme-insert');
 
 
+let meme1 = document.getElementById('meme-1');
+let meme2 = document.getElementById('meme-2');
+let meme3 = document.getElementById('meme-3');
+let meme4 = document.getElementById('meme-4');
+
+
 function myFunction() {
   var x = document.getElementById("text-input").value;
   document.getElementById("meme-text").innerHTML = x;
@@ -19,14 +25,34 @@ let buttonEarth = document.getElementById('earth');
 
 buttonFire.addEventListener('click', function() {
   container.style.border = '3px dashed rgb(255, 0, 0)';
-
 });
 
 buttonWater.addEventListener('click', function() {
   container.style.border = '5px double rgb(0, 0, 255)';
-
 });
+
 buttonEarth.addEventListener('click', function() {
   container.style.border = '6px groove rgb(0, 128, 0)'
+});
 
-  });
+meme1.addEventListener('click', function(){
+  image.src = meme1.src;
+
+})
+
+meme2.addEventListener('click', function(){
+  image.src = meme2.src;
+
+})
+
+meme3.addEventListener('click', function(){
+  image.src = meme3.src;
+
+})
+
+meme4.addEventListener('click', function(){
+  image.src = meme4.src;
+
+})
+
+
