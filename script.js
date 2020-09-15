5d41fabb023f41dc7f296e053037afb76691b67a
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-09-15 00:36:53 -0300

    Finalização layout

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let textInput = document.getElementById("text-input");
-textInput.addEventListener("input", function () {
-  let memeText = document.getElementById("meme-text");
-  memeText.innerHTML = textInput.value;
-});
+
+  textInput.addEventListener("input", function () {
+    let memeText = document.getElementById("meme-text");
+
+    memeText.innerHTML = textInput.value;
+  });
