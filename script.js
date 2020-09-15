f2dd4edcef36afebc25da7da9cbc05837e9bd509
Geziel <geziel.fernandez@pucp.pe>
2020-09-15 17:27:28 -0300

    commits iniciais

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
 // Geziel
 
-let memeInsert = document.querySelector("#meme-insert");
-let img = document.querySelector("img");
+let textInput = document.getElementById("text-input");
 
-let button1 = document.getElementById("button1");
-let button2 = document.getElementById("button2");
-let button3 = document.getElementById("button3");
-let container = document.getElementById("meme-image-container");
-let imageContainer = document.querySelector('#meme-text');
+textInput.addEventListener('input', function () {
+  let textImage = document.getElementById("meme-text");
+  textImage.innerHTML = textInput.value;
+});
 
-let text = document.querySelector('#text-input');
 
-// Write on img, oninput HTML
-function writeOnImg() {
-  imageContainer.innerHTML = text.value;
-  }
 
-// Uploading image
-memeInsert.addEventListener("input", function() {
-  img.id = "meme-img";
-  img.src = URL.createObjectURL(this.files[0]);
-  })
 
-// Button 1 - border type
-button1.addEventListener("click", function(){
-  container.style.border = "3px dashed red";
-})
-// Button 2 - border type
-button2.addEventListener("click", function(){
-  container.style.border = "5px double blue";
-})
-// Button 3 - border type
-button3.addEventListener("click", function(){
-  container.style.border = "6px groove green";
-})
+
+
+
+
+// let memeInsert = document.querySelector("#meme-insert");
+// let img = document.querySelector("img");
+
+// let button1 = document.getElementById("button1");
+// let button2 = document.getElementById("button2");
+// let button3 = document.getElementById("button3");
+// let container = document.getElementById("meme-image-container");
+// let imageContainer = document.querySelector('#meme-text');
+
+// let text = document.querySelector('#text-input');
+
+// // Write on img, oninput HTML
+// function writeOnImg() {
+//   imageContainer.innerHTML = text.value;
+//   }
+
+// // Uploading image
+// memeInsert.addEventListener("input", function() {
+//   img.id = "meme-img";
+//   img.src = URL.createObjectURL(this.files[0]);
+//   })
+
+// // Button 1 - border type
+// button1.addEventListener("click", function(){
+//   container.style.border = "3px dashed red";
+// })
+// // Button 2 - border type
+// button2.addEventListener("click", function(){
+//   container.style.border = "5px double blue";
+// })
+// // Button 3 - border type
+// button3.addEventListener("click", function(){
+//   container.style.border = "6px groove green";
+// })
