b08f243818acb2ccca9e656e94f12d38f8566117
Mauro Bergonzoni Junqueira <mauro@pop-os.localdomain>
2020-09-14 21:30:58 -0300

    ajustando parametros css

--- scripts.js
+++ scripts.js
@@ -1,37 +1,32 @@
 window.onload = function(){
-    const containerMeme = document.querySelector('#meme-image-container');
-    const textInput = document.querySelector('#text-input');
-    const textToPhoto = document.querySelector('#meme-text');
-    const fotoInput = document.querySelector('#input-foto');
-    const imgPhoto = document.querySelector('#meme-insert');
-    const btFire = document.querySelector('#fire');
-    const btWater = document.querySelector('#water');
-    const btEarth = document.querySelector('#earth');
-    const sectionButtons = document.querySelector('#buttons')
-    fotoInput.addEventListener('change',function(event){
+    const containerMeme = document.querySelector("#meme-image-container");
+    const textInput = document.querySelector("#text-input");
+    const textToPhoto = document.querySelector("#meme-text");
+    const fotoInput = document.querySelector("#input-foto");
+    const imgPhoto = document.querySelector("#meme-insert");
+    const btFire = document.querySelector("#fire");
+    const btWater = document.querySelector("#water");
+    const btEarth = document.querySelector("#earth");
+    const sectionButtons = document.querySelector("#buttons")
+    fotoInput.addEventListener("change",function(event){
         let file = event.target.files[0];
         let url = URL.createObjectURL(file);
         imgPhoto.src=url;
     });
-    textInput.addEventListener('keyup',function(event){
+    textInput.addEventListener("keyup",function(event){
         textToPhoto.innerHTML = textInput.value;
 
     });
-    fire.addEventListener('click', function(){
+    fire.addEventListener("click", function(){
         containerMeme.style.border = '3px dashed red';
     });
-    water.addEventListener('click', function(){
+    water.addEventListener("click", function(){
         containerMeme.style.border = '3px dashed blue';
     });
-    earth.addEventListener('click', function(){
+    earth.addEventListener("click", function(){
         containerMeme.style.border = '3px dashed brown';
     });
 }
-    
-function openText(){
-    //textInput = document.getElementById(meme-text);
-    const textInput = document.querySelector('#text-input');
-    
-}
+
 
 
