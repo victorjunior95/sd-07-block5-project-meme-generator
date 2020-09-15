925f839a444f30fe486508882596239714135546
Eriquy Jhordan <eriquy@gmail.com>
2020-09-15 18:18:19 -0300

    refatorando metodo de inserção do texto na imagem

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+let meme_image = document.getElementById('meme-image');
+let textInput = document.getElementById('text-input');
+let text = document.getElementById('meme-text');
+
 function loadFile(event){
-  let meme_image = document.getElementById('meme-image');
   meme_image.src = URL.createObjectURL(event.target.files[0]);
   meme_image.onload = function() {
     URL.revokeObjectURL(output.src);
   }
 }
 
-function loadtext(event){
-  let text = document.getElementById('meme-text');
-  text.innerText = event.target.value;
-}
+textInput.addEventListener('keyup', function loadtext(){
+  text.innerText = textInput.value;
+});
+
+
