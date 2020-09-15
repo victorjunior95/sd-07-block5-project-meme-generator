3a306e189da3868d8062615e7012ca081f08a6a7
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-14 20:03:06 -0400

    criadas funções para troca de bordas

--- script.js
+++ script.js
@@ -12,4 +12,22 @@ window.onload = function () {
     const texto = document.getElementById('text-input').value;
     document.getElementById('meme-text').innerText = texto;
   });
+  // Função para borda vermelha tracejada
+  const borda1 = document.getElementById('fire');
+  borda1.addEventListener('click', function () {
+    const borda = document.getElementById('meme-image');
+    borda.style.border = '3px dashed red';
+  });
+  // Função para borda dupla azul
+  const borda2 = document.getElementById('water');
+  borda2.addEventListener('click', function () {
+    const borda = document.getElementById('meme-image');
+    borda.style.border = '5px double blue';
+  });
+  // Função para borda verde groove
+  const borda3 = document.getElementById('earth');
+  borda3.addEventListener('click', function () {
+    const borda = document.getElementById('meme-image');
+    borda.style.border = '6px groove green';
+  });
 };
