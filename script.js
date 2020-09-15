94c108faa0147939bbbbbbbac411ac5132f7258a
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-09-15 18:45:49 -0300

    Alterações para o quarto requisito

---
+++ script.js
@@ -0,0 +1,16 @@
+let loadFile = function(event) {
+    let output = document.getElementById("meme-image")
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+    URL.revokeObjectURL(output.src)
+    }
+};
+
+let textInputInit = document.querySelector("#text-input");
+textInputInit.addEventListener("keyup", loadText);
+
+function loadText() {
+    let textInput = document.querySelector("#text-input").value;
+    let textOutput = document.querySelector("#meme-text");
+    textOutput.innerText = textInput ;
+};
