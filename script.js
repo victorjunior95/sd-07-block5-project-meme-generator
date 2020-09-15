1769e06874af17062505fac50425b8294dba99f5
Wendrick <wendrick.b.c1@gmail.com>
2020-09-15 18:15:35 -0300

    Outros requisitos

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+// Entregáveis
+
+// 1
 let inputedText = document.querySelector("#text-input");
 
 inputedText.addEventListener("input", function() {
     let memeText = document.querySelector("#meme-text");
     memeText.innerText = inputedText.value;
 });
+
+// 2
+
+// Bónus
+
+//6
+
+let buttonFire = document.querySelector("#fire");
+let buttonWater = document.querySelector("#water");
+let buttonEarth = document.querySelector("#earth");
+
+buttonFire.addEventListener("click", function() {
+    let divMeme = document.querySelector("#meme-image-container");
+    divMeme.style.border = "3px dashed red"
+});
+
+buttonWater.addEventListener("click", function() {
+    let divMeme = document.querySelector("#meme-image-container");
+    divMeme.style.border = "5px double blue"
+});
+
+buttonEarth.addEventListener("click", function() {
+    let divMeme = document.querySelector("#meme-image-container");
+    divMeme.style.border = "6px groove green"
+});
+
+// 7
