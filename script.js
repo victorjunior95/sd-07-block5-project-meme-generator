ce24245073f41b494dd81e6f9c9ab8787f832623
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-09-15 16:35:12 -0300

    Reparando codeclimate 16 of 16 inssues

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const frase = document.getElementById('text-input');
 const linha = document.getElementById('meme-text');
 frase.addEventListener('keyup', function (event) {
   linha.innerHTML = event.target.value;
-})
+}
+)
 
 const foto = document.getElementById('meme-insert');
 const painel = document.getElementById('meme-image');
 
 foto.addEventListener('change', function () {
   painel.src = window.URL.createObjectURL(this.files[0])
-})
+}
+)
