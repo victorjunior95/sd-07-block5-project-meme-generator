05d659a929ad1e5b38662c631dfe218fe393fab1
Alstent <rafaelasltent@gmail.com>
2020-09-15 13:27:55 +0200

    resolvendo code climate q eu odeio do fundo do meu coracao

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const inpFile = document.querySelector('#meme-insert');
 const memeContainer = document.querySelector('#meme-image-container');
 const previewImage = memeContainer.querySelector('#meme-image');
 const spanText = memeContainer.querySelector('.span-text');
+
 const effectsContainer = document.querySelector('#effects');
 const firstTemplate = document.querySelector('#meme-1');
 const secondTemplate = document.querySelector('#meme-2');
 const thirdTemplate = document.querySelector('#meme-3');
 const fourthTemplate = document.querySelector('#meme-4');
+
+const fire = document.querySelector('#fire');
+const water = document.querySelector('#water');
+const earth = document.querySelector('#earth');
+const removeBtn = document.querySelector('#remove-btn');
 
 inpFile.addEventListener('change', function () {
   if (this.files && this.files[0]) {
@@ -46,6 +52,7 @@ inpText.addEventListener('keyup', function () {
   }
 });
 
+/* acho incrivel q por causa do code climade eu nao posso usar esse codigo todo... enfim fazer oq
 function fire() {
   memeContainer.style.border = '3px dashed rgb(255, 0, 0)';
   memeContainer.style.borderRadius = '5px';
@@ -72,40 +79,72 @@ function remove() {
   memeContainer.style.borderRadius = null;
   memeContainer.style.boxShadow = null;
   effectsContainer.style.boxShadow = null;
-}
+} */
 
-firstTemplate.addEventListener('click', function (clickedImg) {
-  const imgSrc = clickedImg.target.src;
-  const imgArrey = imgSrc.match(/imgs[\/\\]([\w\d\s\.\-\(\)]+)$/);
-  const img = imgArrey[0];
-  spanText.style.display = 'none';
-  previewImage.style.display = 'flex';
-  previewImage.src = img;
+fire.addEventListener('click', function () {
+  memeContainer.style.border = '3px dashed rgb(255, 0, 0)';
+  memeContainer.style.borderRadius = '5px';
+  memeContainer.style.boxShadow = '0px 0px 10px 2.5px red';
+  effectsContainer.style.boxShadow = 'inset 0px 0px 20px red';
 });
 
-secondTemplate.addEventListener('click', function (clickedImg) {
-  const imgSrc = clickedImg.target.src;
-  const imgArrey = imgSrc.match(/imgs[\/\\]([\w\d\s\.\-\(\)]+)$/);
-  const img = imgArrey[0];
-  spanText.style.display = 'none';
-  previewImage.style.display = 'flex';
-  previewImage.src = img;
+water.addEventListener('click', function () {
+  memeContainer.style.border = '5px double rgb(0, 0, 255)';
+  memeContainer.style.borderRadius = '5px';
+  memeContainer.style.boxShadow = '0px 0px 10px 2.5px blue';
+  effectsContainer.style.boxShadow = 'inset 0px 0px 20px blue';
 });
 
-thirdTemplate.addEventListener('click', function (clickedImg) {
-  const imgSrc = clickedImg.target.src;
-  const imgArrey = imgSrc.match(/imgs[\/\\]([\w\d\s\.\-\(\)]+)$/);
-  const img = imgArrey[0];
-  spanText.style.display = 'none';
-  previewImage.style.display = 'flex';
-  previewImage.src = img;
+earth.addEventListener('click', function () {
+  memeContainer.style.border = '6px groove rgb(0, 128, 0)';
+  memeContainer.style.borderRadius = '5px';
+  memeContainer.style.boxShadow = '0px 0px 10px 2.5px rgb(0, 128, 0)';
+  effectsContainer.style.boxShadow = 'inset 0px 0px 20px rgb(0, 128, 0)';
 });
 
-fourthTemplate.addEventListener('click', function (clickedImg) {
+removeBtn.addEventListener('click', function () {
+  memeContainer.style.border = null;
+  memeContainer.style.borderRadius = null;
+  memeContainer.style.boxShadow = null;
+  effectsContainer.style.boxShadow = null;
+});
+
+firstTemplate.addEventListener('click', function () {
+  /* code cilmate eu realmente te odeio (clickedImg era o parametro da funcao)
   const imgSrc = clickedImg.target.src;
   const imgArrey = imgSrc.match(/imgs[\/\\]([\w\d\s\.\-\(\)]+)$/);
-  const img = imgArrey[0];
+  const img = imgArrey[0]; */
   spanText.style.display = 'none';
   previewImage.style.display = 'flex';
-  previewImage.src = img;
+  previewImage.src = 'imgs/meme1.png';
+});
+
+secondTemplate.addEventListener('click', function () {
+  /* code cilmate eu realmente te odeio
+  const imgSrc = clickedImg.target.src;
+  const imgArrey = imgSrc.match(/imgs[\/\\]([\w\d\s\.\-\(\)]+)$/);
+  const img = imgArrey[0]; */
+  spanText.style.display = 'none';
+  previewImage.style.display = 'flex';
+  previewImage.src = 'imgs/meme2.png' ;
+});
+
+thirdTemplate.addEventListener('click', function () {
+  /* code cilmate eu realmente te odeio
+  const imgSrc = clickedImg.target.src;
+  const imgArrey = imgSrc.match(/imgs[\/\\]([\w\d\s\.\-\(\)]+)$/);
+  const img = imgArrey[0]; */
+  spanText.style.display = 'none';
+  previewImage.style.display = 'flex';
+  previewImage.src = 'imgs/meme3.png';
+});
+
+fourthTemplate.addEventListener('click', function () {
+  /* code cilmate eu realmente te odeio
+  const imgSrc = clickedImg.target.src;
+  const imgArrey = imgSrc.match(/imgs[\/\\]([\w\d\s\.\-\(\)]+)$/);
+  const img = imgArrey[0]; */
+  spanText.style.display = 'none';
+  previewImage.style.display = 'flex';
+  previewImage.src = 'imgs/meme4.png';
 });
