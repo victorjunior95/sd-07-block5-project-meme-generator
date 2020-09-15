21618ce8d3a4f9d2208f747c977031b271d0e215
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-09-15 16:27:48 -0300

    Reparando codeclimate 25 of 25 inssues

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let frase = document.getElementById('text-input');
-let linha = document.getElementById('meme-text');
+const frase = document.getElementById('text-input');
+const linha = document.getElementById('meme-text');
 frase.addEventListener('keyup', function (event) {
-    linha.innerHTML = event.target.value;
+  linha.innerHTML = event.target.value;
 })
 
-let foto = document.getElementById('meme-insert');
-let painel = document.getElementById('meme-image');
+const foto = document.getElementById('meme-insert');
+const painel = document.getElementById('meme-image');
 
 foto.addEventListener('change', function () {
-    painel.src = window.URL.createObjectURL(this.files[0])
+  painel.src = window.URL.createObjectURL(this.files[0])
 })
