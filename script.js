b7519d93ca623c6bd4c52bcf4574045decede01b
Pedro MF <pedromarques1011@gmail.com>
2020-09-15 16:15:08 -0300

    req6

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const textOutput = document.querySelector('#meme-text');
+const imageBox = document.querySelector('#meme-image-container');
 document.querySelector('#text-input').addEventListener('keyup', (event) => {
   textOutput.innerHTML = event.target.value;
 });
@@ -6,4 +7,14 @@ document.querySelector('#text-input').addEventListener('keyup', (event) => {
 document.querySelector('#meme-insert').addEventListener('change', (event) => {
   const image = document.querySelector('#meme-image');
   image.src = URL.createObjectURL(event.target.files[0]);
+});
+
+document.querySelector('.f').addEventListener('click', () => {
+  imageBox.style.border = '3px dashed red';
+});
+document.querySelector('.w').addEventListener('click', () => {
+  imageBox.style.border = '5px double blue';
+});
+document.querySelector('.e').addEventListener('click', () => {
+  imageBox.style.border = '6px groove green';
 });
