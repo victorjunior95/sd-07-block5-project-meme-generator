286640d76362fc1501f7de00344f8da9f46aa7aa
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-09-16 16:44:39 -0300

    Tentativa 2 requisitos obrigatórios

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let textInput = document.getElementById("text-input");
 
-textInput.addEventListener('keyup', function () {
+textInput.addEventListener("keyup", function () {
   let memeText = document.getElementById("meme-text");
 
   memeText.innerHTML = textInput.value;
@@ -9,9 +9,9 @@ textInput.addEventListener('keyup', function () {
 let upload = document.getElementById("meme-insert");
 let image = document.getElementById("meme-image");
 
-upload.addEventListener('change', function(event){
+upload.addEventListener("change", function (event) {
   image.src = URL.createObjectURL(event.target.files[0]);
-  image.onload = function(){
+  image.onload = function () {
     URL.revokeObjectURL(image.src);
-  }
-})
+  };
+});
