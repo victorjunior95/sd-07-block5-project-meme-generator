27a85d2a1508b0347032e4fffe2db0b6c8932013
Eduardo Souza <dado.eletrons@gmail.com>
2020-09-16 19:34:31 -0300

    update 5° requisito e 1° bonus requisito

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function () {
+
   setupMemeTextInput();
+  addBorderStyle();
 
 }
 
@@ -9,5 +11,19 @@ function setupMemeTextInput() {
     let text = event.target.value;
     let memeTextP = document.querySelector("#meme-text")
     memeTextP.innerText = text;
+  })
+}
+
+
+function addBorderStyle() {
+  let memeImageContainer = document.querySelector("#meme-image-container")
+  document.addEventListener("click", (event) => {
+    if (event.target.id === "fire") {
+      memeImageContainer.setAttribute("style", "border: 3px dashed red")
+    } else if (event.target.id === "water") {
+      memeImageContainer.setAttribute("style", "border: 5px double blue")
+    } else if (event.target.id === "earth") {
+      memeImageContainer.setAttribute("style", "border: 6px groove green")
+    }
   })
 }
