6c50a39d33b957608cc2bc5d95155adb23fa9e41
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-16 16:04:01 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-//let container = document.getElementById("meme-image-container");
+let container = document.getElementById("meme-image-container");
 
 let imgInput = document.querySelector("#meme-insert");
 let imgMeme = document.querySelector("#meme-image");
 
 let inputText = document.querySelector("#text-input");
 let memeText = document.querySelector("#meme-text");
+
+let buttonFire = document.querySelector("#fire");
+let buttonWater = document.querySelector("#water");
+let buttonEarth = document.querySelector("#earth");
 
 inputText.addEventListener('input', function() {
     memeText.innerText = inputText.value;
@@ -16,4 +20,16 @@ imgInput.addEventListener('change', function(event){
     imgMeme.onload = function() {
       URL.revokeObjectURL(imgMeme.src) 
     }
+});
+
+buttonFire.addEventListener("click", function(){
+  container.style.border = "3px dashed red";
+});
+
+buttonWater.addEventListener("click", function(){
+  container.style.border = "5px double blue";
+});
+
+buttonEarth.addEventListener("click", function(){
+  container.style.border = "6px groove green";
 });
