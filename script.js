a06eaf5902bbe30cd35335be6d560617b95426ff
Victor Junior <victorjr.muchacho@gmail.com>
2020-09-16 14:49:25 -0400

    Corrigido e iterado as funçoes do Requisito 1

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-document.getElementById()
+window.onload = function () {
+    setupTextInput();
+}
+
+function setupTextInput() {
+    let textInput = document.querySelector("#text-input");
+    //textInput.addEventListener("keydown", function(event) -- form alternativa
+    textInput.addEventListener("input", function(event) {
+        // recuperar o texto digitado
+        let text = event.target.value;
+        // recuperar o elemento html
+        let memeTextParagraph = document.querySelector("#meme-text");
+        // atribuir o texto no elemento html
+        memeTextParagraph.innerText = text;
+    })
+}
