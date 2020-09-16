6bc4e1585f5ff50304a84ec51bb111dcd39e6f0b
Eduardo Souza <dado.eletrons@gmail.com>
2020-09-16 20:40:22 -0300

    update 1° bonus

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 window.onload = function () {
 
-  setupMemeTextInput();
   addBorderStyle();
+  setupMemeTextInput();
+
 
 }
 
 function setupMemeTextInput() {
   let inputText = document.querySelector("#text-input");
-  inputText.addEventListener("input", function (event) {
-    let text = event.target.value;
-    let memeTextP = document.querySelector("#meme-text")
-    memeTextP.innerText = text;
+  inputText.addEventListener("click", function (event) {
+    console.log("voce me clickou ")
+    event.target.addEventListener("input", function () {
+
+      let text = event.target.value;
+      let memeTextP = document.querySelector("#meme-text")
+      memeTextP.innerText = text;
+    })
+    event.target.addEventListener("dblclick", function () {
+      console.log("duas vezes")
+    })
   })
 }
 
 
+
+// let buttonFire = document.querySelector("#fire")
+//   buttonFire.addEventListener("click", function () {
+//   memeImageContainer.style.border = "3px dashed red"
+// })
+
 function addBorderStyle() {
+
   let memeImageContainer = document.querySelector("#meme-image-container")
+
   document.addEventListener("click", (event) => {
     if (event.target.id === "fire") {
-      memeImageContainer.setAttribute("style", "border: 3px dashed red")
+
+      memeImageContainer.style.border = "3px dashed red"
+
     } else if (event.target.id === "water") {
-      memeImageContainer.setAttribute("style", "border: 5px double blue")
+
+      memeImageContainer.style.border = "5px double blue"
     } else if (event.target.id === "earth") {
-      memeImageContainer.setAttribute("style", "border: 6px groove green")
+
+      memeImageContainer.style.border = "6px groove green"
     }
   })
 }
