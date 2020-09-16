ebba37b8ebfc92cf6e4cdf4cb4fb1aa573fc278d
barrrosomarcio <barrrosomarcio@gmail.com>
2020-09-16 17:35:45 -0300

    commit de correções

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let textMeme = document.getElementById("text-input")
 let imgMeme = document.getElementById("meme-insert")
 
-let memeImg = document.getElementById("meme-image-container")
+
 
 
 textMeme.addEventListener("input", function(event){
@@ -9,5 +9,10 @@ textMeme.addEventListener("input", function(event){
     let memeFinal = document.getElementById("meme-text");
     console.log(memeFinal)
     memeFinal.innerText = memeText;
+})
+imgMeme.addEventListener("select",function(event){
+    let memeImg = event.target.value;
+    let memeFinalImage = document.getElementById("meme-image-container")
+    memeFinalImage.style.backgroundImage = memeImg;
 })
     
