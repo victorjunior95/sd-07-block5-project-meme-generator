24ec97a3cfafa458ce4baffa2b16381aaedb1055
samantabelow <samanta.below@gmail.com>
2020-09-16 18:27:39 -0300

    Adicionando opções de borda

--- script.js
+++ script.js
@@ -8,14 +8,29 @@ textInput.addEventListener("keyup", addText);
 
 function addText()
 {
-    memeText.innerText = textInput.value;
+  memeText.innerText = textInput.value;
 }
 
 imageInput.addEventListener("change", loadImage);
 
 function loadImage(event) {
-    memeImage.src = URL.createObjectURL(event.target.files[0]);
-    memeImage.onload = function() {
-      URL.revokeObjectURL(memeImage.src);
-    }
-  };
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = function() {
+    URL.revokeObjectURL(memeImage.src);
+  }
+};
+
+let fireButton = document.getElementById('fire');
+fireButton.addEventListener("click", function() {
+  container.style.border = "3px dashed red";
+});
+
+let waterButton = document.getElementById('water');
+waterButton.addEventListener("click", function() {
+  container.style.border = "5px double blue";
+});
+
+let earthButton = document.getElementById('earth');
+earthButton.addEventListener("click", function() {
+  container.style.border = "6px groove green";
+});
