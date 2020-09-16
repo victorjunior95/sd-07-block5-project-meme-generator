f545985181211e22c4dd01ea5a9279b8805ac120
Wendrick <wendrick.b.c1@gmail.com>
2020-09-16 16:59:50 -0300

    Talvez último

--- script.js
+++ script.js
@@ -40,3 +40,24 @@ buttonEarth.addEventListener("click", function() {
 });
 
 // 7
+let meme1 = document.querySelector("#meme-1");
+let meme2 = document.querySelector("#meme-2");
+let meme3 = document.querySelector("#meme-3");
+let meme4 = document.querySelector("#meme-4");
+
+meme1.addEventListener("click", function() {
+    let image = document.querySelector("#meme-image");
+    image.src = "/imgs/meme1.png";
+});
+meme2.addEventListener("click", function() {
+    let image = document.querySelector("#meme-image");
+    image.src = "/imgs/meme2.png";
+});
+meme3.addEventListener("click", function() {
+    let image = document.querySelector("#meme-image");
+    image.src = "/imgs/meme3.png";
+});
+meme4.addEventListener("click", function() {
+    let image = document.querySelector("#meme-image");
+    image.src = "/imgs/meme4.png";
+});
