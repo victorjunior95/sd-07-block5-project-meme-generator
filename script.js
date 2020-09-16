57e45ea5d9afc1e0e960e48789862fe7d09320cf
Rodolfo <rodolfojr94@gmail.com>
2020-09-16 08:32:25 -0300

    agora vai

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+document.querySelector('load', () => {
+
+  loadFile();
+  textInput();
+
+});
+
 /* Nao entendi ... fonte:
 https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
 */
-let loadFile = function(event) {
+let loadFile = event => {
   let output = document.getElementById('meme-image');
   output.src = URL.createObjectURL(event.target.files[0]);
   output.onload = function() {
@@ -10,18 +17,8 @@ let loadFile = function(event) {
 };
 
 
-let textInput = function() {
+let textInput = () => {
 
   document.querySelector("#meme-text").innerHTML = document.querySelector("#text-input").value;
 
 };
-
-// funções que iniciarão quando o Dom estiver carregado
-document.querySelector('DOMContentLoaded', function () {
-
-  //Abre imagem adicionada pelo usuário no Container
-  loadFile();
-  //Carrega texto digitado sobre a imagem;
-  textInput();
-
-});
