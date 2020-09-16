534139dd99afa646ab97e04d6d7e28be03a87a9e
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-09-16 16:30:11 -0300

    Resolução requisitos obrigatórios

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let textInput = document.getElementById("text-input");
 
-textInput.addEventListener("keyup", function () {
+textInput.addEventListener('keyup', function () {
   let memeText = document.getElementById("meme-text");
 
   memeText.innerHTML = textInput.value;
 });
+
+let upload = document.getElementById("meme-insert");
+let image = document.getElementById("meme-image");
+
+upload.addEventListener('change', function(event){
+  image.src = URL.createObjectURL(event.target.files[0]);
+  image.onload = function(){
+    URL.revokeObjectURL(image.src);
+  }
+})
