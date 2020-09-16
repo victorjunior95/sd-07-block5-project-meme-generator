9d8b7ee2839b70f85fe40e9c768af06257e50a19
Raphael Caputo <faelelf2@gmail.com>
2020-09-16 19:23:40 -0300

    Adicionando bordas coloridas

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const input = document.querySelector('#text-input');
-const memeText = document.querySelector('#meme-text');
+const input = document.querySelector("#text-input");
+const memeText = document.querySelector("#meme-text");
 
-input.addEventListener('keyup', function () {
+input.addEventListener("keyup", function () {
   memeText.innerText = input.value;
+});
+
+const button = document.querySelector(".red-border");
+button.addEventListener("click", function () {
+  let conteiner = (document.getElementById(
+    "meme-image-container"
+  ).style.border = "solid red");
+});
+
+const button2 = document.querySelector(".blue-border");
+button2.addEventListener("click", function () {
+  let conteiner = (document.getElementById(
+    "meme-image-container"
+  ).style.border = "solid blue");
+});
+
+const button3 = document.querySelector(".yellow-border");
+button3.addEventListener("click", function () {
+  let conteiner = (document.getElementById(
+    "meme-image-container"
+  ).style.border = "solid yellow");
+});
+
+const button4 = document.querySelector(".black-border");
+button4.addEventListener("click", function () {
+  let conteiner = (document.getElementById(
+    "meme-image-container"
+  ).style.border = "solid black");
 });
