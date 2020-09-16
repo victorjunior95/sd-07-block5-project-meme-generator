cd41603d8acf535b973382783341330f648bbb82
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-09-16 16:32:20 -0300

    issues sendo resolvidas

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let inputText = document.getElementById("text-input");
-inputText.addEventListener('input', function(){
-let memeText = document.getElementById("meme-text");
-memeText.innerHTML = inputText.value; 
-}); 
+const inputText = document.getElementById('text-input');
+inputText.addEventListener('input', function () {
+  let memeText = document.getElementById('meme-text');
+  memeText.innerHTML = inputText.value; 
+  }); 
 
-let inputImg = document.getElementById('meme-insert')
-let imgElement = document.getElementById('meme-image');
+const inputImg = document.getElementById('meme-insert')
+const imgElement = document.getElementById('meme-image');
 
-inputImg.addEventListener('change', function() {
-  var url = URL.createObjectURL(inputImg.files[0]);
+inputImg.addEventListener('change', function () {
+  let url = URL.createObjectURL(inputImg.files[0]);
   imgElement.src = url;
 });
