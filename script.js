ddab96eeb1fbc9c6b7202be83fe10c6d0e424004
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-09-16 16:46:52 -0300

    dificil agradas o climate

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ const inputText = document.getElementById('text-input');
 inputText.addEventListener('input', function () {
   let memeText = document.getElementById('meme-text');
   memeText.innerHTML = inputText.value;
-}); 
-
+});
 const inputImg = document.getElementById('meme-insert');
 const imgElement = document.getElementById('meme-image');
-
 inputImg.addEventListener('change', function () {
   let url = URL.createObjectURL(inputImg.files[0]);
   imgElement.src = url;
