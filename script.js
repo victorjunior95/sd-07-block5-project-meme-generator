4294dd5935265655df56da3fbdcd1c942e734173
Gustavo Bregalda <gus.bregalda@gmail.com>
2020-09-17 20:36:43 -0300

    Novo commit para testes de botão e imagem

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ window.onload = function(){
 function setupMemeTextInput (){
   let textInput = document.querySelector("#text-input");
     textInput.addEventListener("input", function(event){
-      let text = event.target.value;
-      let memeTextP = document.querySelector("#meme-text");
-      memeTextP.innerText = text;
+    let text = event.target.value;
+    let memeTextP = document.querySelector("#meme-text");
+    memeTextP.innerText = text;
   });
+}
+
+function setupImage(){
+  let imageInput = document.querySelector("#meme-insert");
+  imageInput.addEventListener("click", function(){
+    let memeImage = document.querySelector("#meme-image");
+  })
 }
 
