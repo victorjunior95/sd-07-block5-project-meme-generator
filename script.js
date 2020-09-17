7a0e443365b1db8920b90f6a5b0c7e8e80c0163a
mayconfelipes <mayconfelipe.dev@gmail.com>
2020-09-16 22:26:16 -0300

    requisitos 3 e 4. VAMOS COM TUDO :rocket:

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputText = document.getElementById('text-input');
+const imageMeme = document.getElementById('meme-image');
+const imageContainer = document.getElementById('meme-image-container');
+
+inputText.oninput = function () {
+  const getParagraph = document.getElementById('meme-text');
+  const textInput = inputText.value;
+  getParagraph.innerText = textInput;
+};
+
+function loadFile(event) {
+    imageMeme.src = URL.createObjectURL(event.target.files[0]);
+    imageMeme.onload = function () {
+    URL.revokeObjectURL(imageMeme.src);
+  };
+  getDivImage.style.backgroundImage = imageMeme.src;
+}
+
+document.getElementById('meme-insert').addEventListener('change', loadFile);
