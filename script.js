69d1422c6ad2193665356fb0d805d08273afbda1
Eduardo Souza <dado.eletrons@gmail.com>
2020-09-16 22:31:17 -0300

    update teste 2°

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 window.onload = function () {
 
-  addBorderStyle();
-  setupMemeTextInput();
+  //addBorderStyle();
+
+  handleEvent("click", "input", "change");
+
 
 
 }
 
-function setupMemeTextInput() {
+/* function setupMemeTextInput() {
   let inputText = document.querySelector("#text-input");
   inputText.addEventListener("input", function (event) {
     let text = event.target.value;
     let memeTextP = document.querySelector("#meme-text")
     memeTextP.innerText = text;
   })
+} */
+
+function setupMemeTextInput(event) {
+
+  let text = event.target.value;
+  let memeTextP = document.querySelector("#meme-text")
+  memeTextP.innerText = text;
+  console.log(event)
+
 }
 
 
-function addBorderStyle() {
+/* function addBorderStyle() {
 
   let memeImageContainer = document.querySelector("#meme-image-container")
 
@@ -33,4 +44,53 @@ function addBorderStyle() {
       memeImageContainer.style.border = "6px groove green"
     }
   })
+} */
+
+function handleEvent(...types) {
+  for (let type of types) {
+    document.addEventListener(type, (event) => {
+      if (type === "click") {
+        if (event.target.classList.contains("button")) {
+          addBorderStyle(event);
+        }
+      } else if (type === "input") {
+        if (event.target.classList.contains("textInput")) {
+          setupMemeTextInput(event);
+        }
+      } else if (type === "change") {
+        if (event.target.classList.contains("memeInsert")) {
+          insertImage(event);
+        }
+      }
+
+    })
+  }
+
+}
+
+function addBorderStyle(event) {
+
+  let memeImageContainer = document.querySelector("#meme-image-container")
+
+
+  if (event.target.id === "fire") {
+
+    memeImageContainer.style.border = "3px dashed red"
+    console.log(memeImageContainer.style.border)
+
+  } else if (event.target.id === "water") {
+
+    memeImageContainer.style.border = "5px double blue"
+
+  } else if (event.target.id === "earth") {
+
+    memeImageContainer.style.border = "6px groove green"
+  }
+
+}
+
+function insertImage(event) {
+  let memeImage = document.querySelector("#meme-image");
+  memeImage.src = window.URL.createObjectURL(event.target.files[0])
+
 }
