8e433f9f8e78ce6f454bc663d5f04c4f7475a21f
Francisco <franciscoliratorres@gmail.com>
2020-09-17 12:02:59 -0300

    Fix bug

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let inputText = document.querySelector("#text-input");
+const inputText = document.querySelector('#text-input');
 
-inputText.addEventListener("input", function(){
-let showText = document.querySelector("#meme-text");
-showText.innerHTML = inputText.value;   
-});     
+inputText.addEventListener ('input', function (){
+    const showText = document.querySelector('#meme-text');
+showText.innerHTML = inputText.value;
+});
 
-let loadFile = function(event) {
-    var output = document.querySelector("#meme-image")
-    output.src = URL.createObjectURL(event.target.files[0]);
+const loadFile = function(event) {
+  let output = document.querySelector("#meme-image")
+    output.src = URL.createObjectURL (event.target.files[0]);
     output.onload = function () {
         URL.revokeObjectURL(output.src);
 }
@@ -18,8 +18,8 @@ chooseFile.addEventListener('change', loadFile);
 
 let buttonFire = querySelector("#fire");
 let buttonWater = querySelector("#water");
-let buttonEarth = gquerySelector("#earth");
-let memeImageContainer = gquerySelector("#meme-image-container")
+let buttonEarth = querySelector("#earth");
+let memeImageContainer = querySelector("#meme-image-container")
 
 buttonFire.addEventListener("click", function () {
     memeImageContainer.style.border = "3px dashed red";
@@ -32,4 +32,3 @@ buttonWater.addEventListener("click", function () {
 buttonEarth.addEventListener("click", function () {
     memeImageContainer.style.border = "6px groove green";
 })
-
