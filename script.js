ac05d5632074ecc3663db5562f21a3305375eaf9
spawnred <bruno.spawnhell@gmail.com>
2020-09-17 21:44:30 -0300

    corrigindo item 3

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 
-const textInput = document.getElementById('text-input');
-const memeText = document.getElementById('meme-text');
-textInput.addEventListener('input', function(){
-memeText.innerHTML = textInput.value;
-});
+// const textInput = document.getElementById('text-input');
+// const memeText = document.getElementById('meme-text');
+// textInput.addEventListener ('input', function( ) {
+//         memeText.innerHTML = textInput.value;
+// });
 
-const memeInsert = document.getElementById('meme-insert');
-const memeImage = document.getElementById('meme-image');
-memeInsert.addEventListener('change', function(event){
-    memeImage.src = URL.createObjectURL(event.target.files[0])
-    memeImage.onload = function (){
-        URL.revokeObjectURL(memeImage.src)
-    }
-})
+// const memeInsert = document.getElementById('meme-insert');
+// const memeImage = document.getElementById('meme-image');
+// memeInsert.addEventListener ('change', function (event) {
+//         memeImage.src = URL.createObjectURL(event.target.files[0]);
+//         memeImage.onload = function () {
+//             URL.revokeObjectURL(memeImage.src);
+//     };
+// });
+
+window.onload = function(){
+        setupMemeTextInput();
+}
+
+function setupMemeTextInput() {
+        let textInput = document.querySelector("#text-input");
+        textInput.addEventListener("input", function(event){
+        let text = event.target.value;
+        let memeTextP = document.querySelector("#meme-text")
+        memeTextP.innerText = text;
+        });
+}
