891ce2bab4c11b32cfea0451b53e5a2e6dcb8cef
spawnred <bruno.spawnhell@gmail.com>
2020-09-17 21:50:40 -0300

    corrigindo item 2 e 3

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-
-// const textInput = document.getElementById('text-input');
-// const memeText = document.getElementById('meme-text');
-// textInput.addEventListener ('input', function( ) {
-//         memeText.innerHTML = textInput.value;
-// });
-
-// const memeInsert = document.getElementById('meme-insert');
-// const memeImage = document.getElementById('meme-image');
-// memeInsert.addEventListener ('change', function (event) {
-//         memeImage.src = URL.createObjectURL(event.target.files[0]);
-//         memeImage.onload = function () {
-//             URL.revokeObjectURL(memeImage.src);
-//     };
-// });
-
+ 
 window.onload = function(){
         setupMemeTextInput();
 }
@@ -26,3 +11,11 @@ function setupMemeTextInput() {
         memeTextP.innerText = text;
         });
 }
+const memeInsert = document.getElementById('meme-insert');
+ const memeImage = document.getElementById('meme-image');
+ memeInsert.addEventListener ('change', function (event) {
+         memeImage.src = URL.createObjectURL(event.target.files[0]);
+         memeImage.onload = function () {
+             URL.revokeObjectURL(memeImage.src);
+     };
+ });
