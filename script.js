96d5799da8406a88f330745616269de8b46aeec5
Eduardo Souza <dado.eletrons@gmail.com>
2020-09-17 21:45:31 -0300

    refatoracao 3°

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-function insertTextOnImage(event) {
-  console.log("entre insertTexOnImage")
-
+const insertTextOnImage = (event) => {
   const text = event.target.value;
   const memeTextP = document.querySelector("#meme-text")
   memeTextP.innerText = text;
-
 }
 
-
-
-function addBorderStyle(event) {
-  console.log("entre addborderStyle")
-
+const addBorderStyle = (event) => {
   const memeImageContainer = document.querySelector("#meme-image-container")
-
-  if (event.target.id === "fire") {
-
-    memeImageContainer.style.border = "3px dashed red"
-
-  } else if (event.target.id === "water") {
-
-    memeImageContainer.style.border = "5px double blue"
-
-  } else if (event.target.id === "earth") {
-
-    memeImageContainer.style.border = "6px groove green"
+  const eventOriginById = event.target.id
+  switch (eventOriginById) {
+    case "fire":
+      memeImageContainer.style.border = "3px dashed red"
+      break;
+    case "water":
+      memeImageContainer.style.border = "5px double blue"
+      break;
+    case "earth":
+      memeImageContainer.style.border = "6px groove green"
+      break;
   }
 }
 
 
-function insertImage(event) {
-  console.log("entre insertImage")
-
+const insertImage = (event) => {
   const memeImageImg = document.querySelector("#meme-image");
   memeImageImg.src = window.URL.createObjectURL(event.target.files[0])
   memeImageImg.onload = () => {
@@ -39,62 +29,48 @@ function insertImage(event) {
   }
 }
 
-function insertDefaultImage(event) {
+const insertDefaultImage = (event) => {
   const memeImageImg = document.querySelector("#meme-image")
-  const idImageCase = event.target.id;
-  switch (idImageCase) {
+  const eventOriginById = event.target.id;
+  switch (eventOriginById) {
     case "meme-1":
       memeImageImg.src = "/imgs/meme1.png"
       break;
     case "meme-2":
       memeImageImg.src = "/imgs/meme2.png"
-
       break;
     case "meme-3":
       memeImageImg.src = "/imgs/meme3.png"
-
       break;
     case "meme-4":
       memeImageImg.src = "/imgs/meme4.png"
-
       break;
-
   }
 }
 
-function handleControllerEvents(...types) {
-
+const handleControllerEvents = (...types) => {
   for (const type of types) {
     switch (type) {
       case "click":
-        allEventsOnClicks(type)
+        controllerEventsOnClicks(type)
         break;
-
       case "input":
-        allEventOnInput(type);
+        controllerEventsOnInputs(type);
         break;
-
       case "change":
-        allEventOnChange(type);
-        break;
-
-      default:
+        controllerEventsOnChanges(type);
         break;
     }
   }
-
 }
 
-function allEventsOnClicks(type) {
-
+const controllerEventsOnClicks = (type) => {
   document.addEventListener(type, (event) => {
-    const switchEvent = event.target.dataset.event;
-    switch (switchEvent) {
-
+    const eventOriginByData = event.target.dataset.event;
+    switch (eventOriginByData) {
       case "addBorderButton":
         addBorderStyle(event);
         break;
-
       case "addDefaultImage":
         insertDefaultImage(event);
         break;
@@ -102,36 +78,28 @@ function allEventsOnClicks(type) {
   })
 }
 
-function allEventOnInput(type) {
-
+const controllerEventsOnInputs = (type) => {
   document.addEventListener(type, (event) => {
-    const switchEvent = event.target.dataset.event;
-    switch (switchEvent) {
-
+    const eventOriginByData = event.target.dataset.event;
+    switch (eventOriginByData) {
       case "insertTextInput":
         insertTextOnImage(event);
         break;
     }
   })
-
 }
 
-function allEventOnChange(type) {
-
+const controllerEventsOnChanges = (type) => {
   document.addEventListener(type, (event) => {
-    const switchEvent = event.target.dataset.event;
-    switch (switchEvent) {
-
+    const eventOriginByData = event.target.dataset.event;
+    switch (eventOriginByData) {
       case "insertMemeInput":
         insertImage(event);
         break;
-
     }
   })
-
 }
 
 window.onload = () => {
-
   handleControllerEvents("click", "input", "change")
 }
