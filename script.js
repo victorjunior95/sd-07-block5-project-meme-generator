3da9f36b36ab7e1621b8e5b1b491c9af4a7b06fa
Breenolf <blf@ic.ufal.br>
2020-09-19 17:35:26 -0300

    Corrigindo requisito 1

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-window.onload = function() {
-    let inputText = document.querySelector('#text-input');
-    let memeText = document.querySelector('#meme-text');
-    inputText.addEventListener('keyup', function(){
-        memeText.innerText = inputText.value;
-    })
-    
+window.onload = function () {
+  memeTextInput();
+};
+
+function memeTextInput() {
+  let inputText = document.querySelector("#text-input");
+  let memeText = document.querySelector("#meme-text");
+  inputText.addEventListener("keyup", function () {
+    memeText.innerText = inputText.value;
+  });
 }
-
-
