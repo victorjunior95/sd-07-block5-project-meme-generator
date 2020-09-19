562a895f8b9c23c6a200a868fd44d26a75612755
Breenolf <blf@ic.ufal.br>
2020-09-19 15:44:37 -0300

    Completando requisito 1

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.onload = function() {
     let inputText = document.querySelector('#text-input');
+    let memeText = document.querySelector('#meme-text');
+    inputText.addEventListener('keyup', function(){
+        memeText.innerText = inputText.value;
+    })
+    
 }
+
+
