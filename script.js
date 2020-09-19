af208b5da40771940a6925a00f8a5abaa1c0bb7a
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-18 22:24:12 -0300

    Commit 11 - Meme Generator

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-// let text_input = document.getElementById('text-input');
-// let text = document.getElementById('meme-text');
+let text_input = document.getElementById('text-input');
+let text = document.getElementById('meme-text');
 let container = document.getElementById('meme-image-container');
+let botao_earth = document.getElementById('earth');
+let botao_fire = document.getElementById('fire');
+let botao_water = document.getElementById('water');
+
 function carregarArquivo(event)
 {
     image.src = URL.createObjectURL(event.target.files[0]);
@@ -9,33 +13,18 @@ function carregarArquivo(event)
         URL.revokeObjectURL(output.src);
     }
 }
-let botao_earth = document.getElementById("earth");
-let botao_fire = document.getElementById("fire");
-let botao_water = document.getElementById("water");
 
-botao_earth.addEventListener('click', earth());
-function earth()
-{
-    container.style.border = "6px groove rgb(0, 128, 0)";
-}
+botao_earth.addEventListener('click', function(){
+    container.style.border = '6px groove rgb(0, 128, 0)';
+});
 
-botão_fire.addEventListener('click', fire());
-function fire()
-{
-    container.style.border = "3px dashed rgb(255, 0, 0)";
-}
+botão_fire.addEventListener('click', function(){
+    container.style.border = '3px dashed rgb(255, 0, 0)';
+});
 
-botao_water.addEventListener('click', water());
-function water()
-{
-    container.style.border = "5px double rgb(0, 0, 255)";
-}
-
-
-
-
-
-
+botao_water.addEventListener('click', function(){
+    container.style.border = '5px double rgb(0, 0, 255)';
+});
 
 text_input.addEventListener('keyup', carrega_texto())
 
@@ -43,5 +32,4 @@ function carrega_texto()
 {
     let x = document.getElementById("text-input").value;
     document.getElementById("meme-text").innerHTML = x;
-
 }
