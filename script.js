841fb2ee8c6fc60a8f1b0ffbae3fd831a24aaebf
Rals <ricardo.rals.dev@gmail.com>
2020-09-19 19:23:51 -0300

    Acrescentando script

---
+++ script.js
@@ -0,0 +1,13 @@
+const textImage = document.getElementById('text-input');
+const imageMeme = document.getElementById('meme-image');
+const insertImage = document.getElementById('meme-insert')
+
+textImage.addEventListener('keyup', function(){
+    const text = textImage.value;
+    document.getElementById('meme-text').innerText = text;
+});
+
+insertImage.addEventListener('change', function(event){
+    insertImage = insertImage.value;
+    imageMeme.src = URL.createObjectURL(event.target.files[0]);
+});
