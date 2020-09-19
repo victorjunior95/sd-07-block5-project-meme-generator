ad7695fa91f52b56383c47bc81d0a97ab6ff1252
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-18 21:05:34 -0300

    Commit 7 - Meme Generator

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let text_input = document.getElementById('text-input');
 let text = document.getElementById('meme-text');
 let image = document.getElementById('meme-image');
+let container = document.getElementById('meme-image-container');
 function carregarArquivo(event)
 {
     image.src = URL.createObjectURL(event.target.files[0]);
@@ -9,6 +10,29 @@ function carregarArquivo(event)
         URL.revokeObjectURL(output.src);
     }
 }
+let botao_earth = document.getElementById('earth');
+let botao_fire = document.getElementById('fire');
+let botao_water = document.getElementById('water');
+
+botao_earth.addEventListener('click', earth());
+botão_fire.addEventListener('click', fire());
+botao_water.addEventListener('click', water());
+
+function earth()
+{
+    container.style.border = '6px groove rgb(0, 128, 0)';
+}
+
+function fire()
+{
+    container.style.border = '3px dashed rgb(255, 0, 0)';
+}
+
+function water()
+{
+    container.style.border = '5px double rgb(0, 0, 255)';
+}
+
 text_input.addEventListener('keyup', carrega_texto())
 
 function carrega_texto()
