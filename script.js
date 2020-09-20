bd137173778aa558147fce2ddc6b844ee7ce572f
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-09-20 19:13:40 -0300

    Escolher imagem e escrever sobre a mesma

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let textoDigitado = document.getElementById("text-input");
 
-textoDigitado.addEventListener("input", function(event){
+textoDigitado.addEventListener("input", function(event) {
     let textoMeme = document.getElementById("meme-text");
     let texto = event.target.value;
     textoMeme.innerText = texto;
-})
+});
+
+let  imagemInserida = document.getElementById("meme-insert");
+
+function carregarImagem(event) {
+    let imagemCarregada = document.getElementById("meme-image");
+    imagemCarregada.src = URL.createObjectURL(event.target.files[0]);
+}
+
+imagemInserida.addEventListener("change", function(event) {
+    carregarImagem(event)
+ });
