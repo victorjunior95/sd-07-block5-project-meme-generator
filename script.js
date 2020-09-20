9f27b220e2d957f6e04af8c79273b47b7ad6d2b0
Gustavo Bregalda <gus.bregalda@gmail.com>
2020-09-20 00:42:32 -0300

    alterações na inclusão de imagem

--- script.js
+++ script.js
@@ -14,10 +14,13 @@ function setupMemeTextInput (){
 function setupImage(){
   let imageInput = document.querySelector("#meme-insert");
   imageInput.addEventListener("click", function(){
+    
     let memeImage = document.querySelector("#meme-image");
+    /*
+    .style.backgroundColor = window.getComputedStyle(meme-image-container).backgroundColor;
+    */
   })
 }
-
 
 var loadFile = function(event) {
   var reader = new FileReader();
