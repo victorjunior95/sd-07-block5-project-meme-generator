e45bfbb8764117ac2572aa69ad72bedace7300fd
Renato Silva <renatosdev@gmail.com>
2020-09-20 23:52:15 -0300

    Recomeçando. Solucçao item 1. Feito conforme plantao do GO.

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-function textMeme() {
-    let textImput = document.getElementById("text-input").value;
-    let memeText = document.getElementById("meme-text");
-    memeText.innerHTML = textImput;
-
-    let memeText = document.getElementById("text-input");
-    memeText.addEventListener("keyup", textMeme);
-
-    function imageMeme(event) {
-        let memeIsert= document.getElementById("meme-insert");
-        memeIsert.src = URL.createObjetctURL(event.target.files[0]);
-        memeInsert.onload = function () {
-            URL.revokeObjectURL(memeIsert.src);
-        };
-    } ;
-    
-    let meme = document.getElementById("image");
-    meme.addEventListener(change, imageMeme);
+window.onload = function() {
+textMeme();
 }
 
+function textMeme() {
+  let textInput = document.getElementById('text-input');
+  textInput.addEventListener('input', function(event) {
+  let text = event.target.value;
+  let memeText = document.getElementById('meme-text');
+  memeText.innerText = text;  
+  });
+}
+  
+
+
+// let textInput = document.getElementById('text-input');
+// memeText.addEventListener('keyup', textMeme);
+
+// function imageMeme(event) {
+//   let memeIsert = document.getElementById('meme-insert');
+//   memeIsert.src = URL.createObjetctURL(event.target.files[0]);
+//   memeInsert.onload = function () {
+//     URL.revokeObjectURL(memeIsert.src);
+//   };
+// }
+
+// let meme = document.getElementById('image');
+// meme.addEventListener('change', imageMeme);
