28c2646533928a48b88f648fee2acc95016e5c07
Renato Silva <renatosdev@gmail.com>
2020-09-21 00:06:50 -0300

    Item 3

--- script.js
+++ script.js
@@ -10,19 +10,14 @@ function textMeme() {
   memeText.innerText = text;  
   });
 }
-  
-
-
-// let textInput = document.getElementById('text-input');
-// memeText.addEventListener('keyup', textMeme);
-
-// function imageMeme(event) {
-//   let memeIsert = document.getElementById('meme-insert');
-//   memeIsert.src = URL.createObjetctURL(event.target.files[0]);
-//   memeInsert.onload = function () {
-//     URL.revokeObjectURL(memeIsert.src);
-//   };
-// }
+ 
+function imageMeme(event) {
+  let memeIsert = document.getElementById('meme-insert');
+  memeIsert.src = URL.createObjetctURL(event.target.files[0]);
+  memeInsert.onload = function () {
+    URL.revokeObjectURL(memeIsert.src);
+  };
+}
 
 // let meme = document.getElementById('image');
 // meme.addEventListener('change', imageMeme);
