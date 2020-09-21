611b5ee76b4303ad2d79544208f1846f0a2611eb
Renato Silva <renatosdev@gmail.com>
2020-09-21 19:32:21 -0300

    Modificacoes

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-window.onload = function() {
-
+window.onload = function () {;
+textMeme();
 }
 
 function textMeme() {
   let textInput = document.getElementById('text-input');
-  textInput.addEventListener('input', function(event) {
-  let text = event.target.value;
-  let memeText = document.getElementById('meme-text');
-  memeText.innerText = text;  
+  textInput.addEventListener('input', function (event) {
+    // recuperar o valor digitado
+    let text = event.target.value;
+    // recuperar onde vou colocar o texto
+    let memeText = document.getElementById('meme-text');
+    // colocar o texto no lugar
+    memeText.innerText = text;
   });
 }
 
+
 let imageMeme = function (event) {
-    let memeInsert = document.getElementById('meme-insert');
-   memeInsert.src = URL.createObjetctURL(event.target.files[0]);
-   memeInsert.onload = function () {
-      URL.revokeObjectURL (memeInsert.src);
-    };
-  }
- 
-  let memeImage = document.getElementById('meme-image');
-  memeImage.addEventListener('change', imageMeme);
+  let memeInsert = document.getElementById('meme-insert');
+  memeInsert.src = URL.createObjetctURL(event.target.files[0]);
+  memeInsert.onload = function () {
+    URL.revokeObjectURL(memeInsert.src);
+  };
+};
+
+let meme = document.getElementById('image');
+meme.addEventListener('change', imageMeme);
+
