49c0d20da751c85f18d22d65583f3c897b7f842a
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-20 23:36:26 -0300

    resolvendo problema da imagem no container

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
 function loadFile(event) {
-    const divImage = document.getElementById("meme-image-container");
-    const source = URL.createObjectURL(event.target.files[0]);
-    divImage.innerHTML = `<img  class='background-image'
-                              src='${source}'
-                              id='imageChosen'>
+    let divImage = document.getElementById("meme-image");
+    let source = URL.createObjectURL(event.target.files[0]);
+    divImage.innerHTML = `<img  src='${source}'                            
                         </img>`;
 }
 
 function insertImage(src) {
-    const divImage = document.getElementById("meme-image-container");
-    divImage.innerHTML = `<img  class='background-image'
-                              src='/imgs/${src}' >
-                             
+    let divImage = document.getElementById("meme-image");
+    divImage.innerHTML = `<img src='/imgs/${src}' >                             
                         </img>`;
 }
 window.onload = function() {
-    const inputText = document.getElementById("text-input");
-    const memeText = document.getElementById("meme-text");
+    let inputText = document.getElementById("text-input");
+    let memeText = document.getElementById("meme-text");
     inputText.addEventListener("input", function() {
         memeText.innerHTML = inputText.value;
     });
 
-    const buttonImage = document.getElementById("meme-insert");
+    let buttonImage = document.getElementById("meme-insert");
     buttonImage.addEventListener("change", function(event) {
         loadFile(event);
     });
 };
 //consultei o pull requeste de Luciano Brechon - Turma 07 da Trybe para compreender melhor as funções.
-//Minha  dificuldade era fazer a imagem do upload aparecer na tela.
-//Fiz uma espécie de engenharia reversa primeiro, retirando parte por parte a fim de compreender
-//o que cada bloco faz, e então, analisei parte por parte relacionando com o o HTML para compreender
-//todo o conceito. Após a entrega inicial do primeiro commit com apenas 20% pude concluir o resto sozinho.
 // link do pull realizado por  Luciano: https://github.com/tryber/sd-07-block5-project-meme-generator/pulls/Berchon
