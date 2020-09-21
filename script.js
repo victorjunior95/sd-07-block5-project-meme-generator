49cf2dfbe306488e0bebec9861359c4a6194ca47
Breenolf <blf@ic.ufal.br>
2020-09-21 12:16:10 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function () {
   memeTextInput();
+  insertImage()
 };
 
 function memeTextInput() {
@@ -8,4 +9,12 @@ function memeTextInput() {
   inputText.addEventListener("keyup", function () {
     memeText.innerText = inputText.value;
   });
+}
+
+function insertImage() {
+  let memeInsert = document.querySelector("#meme-insert");
+  let memeImage = document.querySelector("#meme-image");
+  memeInsert.addEventListener('change', function(event) {
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
+  })
 }
