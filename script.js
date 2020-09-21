c444cc91a25aa41136bfa4499b42144590470f54
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-09-20 21:24:39 -0300

    Adição de imagens de memes pré prontas para ser editado

--- script.js
+++ script.js
@@ -37,3 +37,31 @@ imagemInserida.addEventListener("change", function(event) {
     let quadro = document.getElementById("meme-image-container");
     quadro.style.border = "6px groove green"
  })
+
+ let meme1 = document.getElementById("meme-1");
+
+ meme1.addEventListener("click", function() {
+    let imagemCarregada = document.getElementById("meme-image");
+    imagemCarregada.src = "imgs/meme1.png"
+ })
+
+ let meme2 = document.getElementById("meme-2");
+
+ meme2.addEventListener("click", function() {
+    let imagemCarregada = document.getElementById("meme-image");
+    imagemCarregada.src = "imgs/meme2.png"
+ })
+
+ let meme3 = document.getElementById("meme-3");
+
+ meme3.addEventListener("click", function() {
+    let imagemCarregada = document.getElementById("meme-image");
+    imagemCarregada.src = "imgs/meme3.png"
+ })
+
+ let meme4 = document.getElementById("meme-4");
+
+ meme4.addEventListener("click", function() {
+    let imagemCarregada = document.getElementById("meme-image");
+    imagemCarregada.src = "imgs/meme4.png"
+ })
