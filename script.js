c3bbc4e10912b2cd92a7e3f59cbcd742ca5f9eb2
spawnred <bruno.spawnhell@gmail.com>
2020-09-21 02:44:21 -0300

    clima

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
- 
-window.onload = function(){
-        setupMemeTextInput();
-}
+window.onload = function () {
+  setupMemeTextInput();
+};
 
 function setupMemeTextInput() {
-        let textInput = document.querySelector("#text-input");
-        textInput.addEventListener("input", function(event){
-        let text = event.target.value;
-        let memeTextP = document.querySelector("#meme-text")
-        memeTextP.innerText = text;
-        });
+  let textInput = document.querySelector("#text-input");
+  textInput.addEventListener("input", function (event) {
+    let text = event.target.value;
+    let memeTextP = document.querySelector("#meme-text");
+    memeTextP.innerText = text;
+  });
 }
-const memeInsert = document.getElementById('meme-insert');
- const memeImage = document.getElementById('meme-image');
- memeInsert.addEventListener ('change', function (event) {
-         memeImage.src = URL.createObjectURL(event.target.files[0]);
-         memeImage.onload = function () {
-             URL.revokeObjectURL(memeImage.src);
-     };
- });
+const memeInsert = document.getElementById("meme-insert");
+const memeImage = document.getElementById("meme-image");
+memeInsert.addEventListener("change", function (event) {
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.onload = function () {
+    URL.revokeObjectURL(memeImage.src);
+  };
+});
