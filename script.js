4b0101bc78a577f3d3acaa0e8d69c18c5a8ecd07
Lucas Sá <lucas.nonato1@hotmail.com>
2020-09-22 19:06:49 -0300

    formatação com prettier para aspas simples

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let textInput = document.getElementById("text-input");
+let textInput = document.getElementById('text-input');
 
-textInput.addEventListener("keyup", function () {
-  let memeText = document.getElementById("meme-text");
+textInput.addEventListener('keyup', function () {
+  let memeText = document.getElementById('meme-text');
 
   memeText.innerHTML = textInput.value;
 });
 
-let upload = document.getElementById("meme-insert");
-let image = document.getElementById("meme-image");
+let upload = document.getElementById('meme-insert');
+let image = document.getElementById('meme-image');
 
-upload.addEventListener("change", function (event) {
+upload.addEventListener('change', function (event) {
   image.src = URL.createObjectURL(event.target.files[0]);
   image.onload = function () {
     URL.revokeObjectURL(image.src);
