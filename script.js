a23356c68322e2937d76b0c2691c0450380566c6
lukeCruz <enio.cruz@yahoo.com.br>
2020-09-21 23:20:14 -0300

    add script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    configInput();
+}
+function configInput(){
+    let textInput = document.querySelector("#text-input");
+    textInput.addEventListener("input", function(inputText) {
+        const text = inputText.target.value;
+        const memeText = document.querySelector("#meme-text");
+        memeText.innerText = text;
+    });
+}
+window.addEventListener('load', function(){
+    document.querySelector('input[type="file"]').addEventListener('change', function(){
+        let img = document.getElementById("meme-image");
+        img.src=URL.createObjectURL(this.files[0]);
+    })
+}); 
