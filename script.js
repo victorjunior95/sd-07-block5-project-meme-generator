d4e099327b1a4c8e0f93f3fe2fe5310bd1ada162
Rita Jeveaux <ritajeveaux@gmail.com>
2020-09-22 00:09:31 -0300

    Terminados requisitos obrigatorios + 1 bonus

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ const buttonChoice = document.getElementById("meme-insert");
 buttonChoice.addEventListener("change", function(event) {
   loadFile(event);
 });
+
+const btFire = document.getElementById('fire');
+
+btFire.addEventListener('click', function (){
+   document.getElementById('meme-image-container').className = 'container box-fire';
+})
+
+const btWater= document.getElementById('water');
+
+btWater.addEventListener('click', function(b){
+  document.getElementById('meme-image-container').className = 'container box-water';
+})
+
+const btEarth = document.getElementById('earth');
+
+btEarth.addEventListener('click', function(){
+  document.getElementById('meme-image-container').className = 'container box-earth';
+})
