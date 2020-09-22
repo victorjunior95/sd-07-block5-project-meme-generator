361f8ce647cc4f0d31b66aac68b3433699bc91d4
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-22 01:10:04 -0300

    Commit 27 - Meme Generator

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ function showImage(event) {
     document.querySelector('#meme-image').src = URL.createObjectURL(event.target.files[0]);
 }
 
-function vermelho() {
-    document.getElementById('meme-image-container').style.border = '3px dashed red';
-}
-
 function azul() {
     document.getElementById('meme-image-container').style.border = '5px double blue';
 }
@@ -19,11 +15,30 @@ function vermelho() {
     document.getElementById('meme-image-container').style.border='3px dashed red';
 }
 
-document.getElementById('fire').addEventListener('click', function(){
-    document.getElementById('meme-image-container').style.border='3px dashed red';
+document.getElementById('text-input').addEventListener("keyup", function(){
+    document.getElementById('meme-text').innerText = document.getElementById('text-input').value;
 });
+
+function mostraminiatura(event)
+{
+    document.querySelector('#meme-image').src = event.target.src;
+}
 
 document.getElementById('text-input').addEventListener("keyup", function(){
     document.getElementById('meme-text').innerText = document.getElementById('text-input').value;
 });
 
+
+
+
+
+// let memeAqui = document.querySelector('.meme')
+
+// memeAqui.addEventListener('click', memePronto);
+// function memePronto() {
+//     let selecao = document.querySelector('.selecionado');
+//     if (selecao !== null) {
+//         selecao.classList.remove('selecionado');
+//     }
+//     event.target.classList.add('selecionado');
+// }
