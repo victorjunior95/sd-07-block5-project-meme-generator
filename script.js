4c6a4edc041e9796d55556644561bb8f77af1e59
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-22 00:12:37 -0300

    Commit 22 - Meme Generator

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
 
-caixaTexto.addEventListener('input', aparecerTexto);
-    function aparecerTexto() {
-        document.getElementById("#meme-text").innerText = document.getElementById("#text-input").value;
-    }
-
-
-function aparecerImagem(event) {
-    document.querySelector("#meme-image").src = URL.createObjectURL(event.target.files[0]);
+function showImage(event) {
+    document.querySelector('#meme-image').src = URL.createObjectURL(event.target.files[0]);
 }
 
-let botaoVermelho = document.querySelector("#fire");
-botaoVermelho.addEventListener("click", vermelho);
 function vermelho() {
-    document.getElementById("#meme-image-container").style.border = "3px dashed red";
+    document.getElementById('meme-image-container').style.border = '3px dashed red';
 }
 
-let botaoAzul = document.querySelector("#water");
-botaoAzul.addEventListener("click", azul);
 function azul() {
-    document.getElementById("#meme-image-container").style.border = "5px double blue";
+    document.getElementById('meme-image-container').style.border = '5px double blue';
 }
 
-let botaoVerde = document.querySelector("#earth");
-botaoVerde.addEventListener("click", verde);
 function verde() {
-    document.getElementById("#meme-image-container").style.border = "6px groove green";
+    document.getElementById('meme-image-container').style.border = '6px groove green';
 }
 
-let memeAqui = document.querySelector(".meme")
-
-memeAqui.addEventListener("click", memePronto);
-function memePronto() {
-    let selecao = document.querySelector(".selecionado");
-    if (selecao !== null) {
-        selecao.classList.remove("selecionado");
-    }
-    event.target.classList.add("selecionado");
+function vermelho() {
+    document.getElementById('meme-image-container').style.border='3px dashed red';
 }
+
+document.getElementById('fire').addEventListener('click', function(){
+    document.getElementById('meme-image-container').style.border='3px dashed red';
+});
+
+function addEventos(){
+    document.getElementById('fire').addEventListener("click", vermelho);
+    document.getElementById('water').addEventListener("click", azul);
+    document.document.getElementById('earth').addEventListener("click", verde);
+}
+
+document.addEventListener("load", addEventos);
+
+const inputText = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
+inputText.addEventListener("input", function(){
+    memeText.innerHTML = inputText.nodeValue;
+});
+
+// let memeAqui = document.querySelector('.meme')
+
+// memeAqui.addEventListener('click', memePronto);
+// function memePronto() {
+//     let selecao = document.querySelector('.selecionado');
+//     if (selecao !== null) {
+//         selecao.classList.remove('selecionado');
+//     }
+//     event.target.classList.add('selecionado');
+// }
