90bd0af3c585655d41ae242e0f0409241afc060a
Lucas Sá <lucas.nonato1@hotmail.com>
2020-09-22 19:13:01 -0300

    retirada issues clarity CC

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let textInput = document.getElementById('text-input');
+const textInput = document.getElementById('text-input');
 
 textInput.addEventListener('keyup', function () {
-  let memeText = document.getElementById('meme-text');
+  const memeText = document.getElementById('meme-text');
 
   memeText.innerHTML = textInput.value;
 });
 
-let upload = document.getElementById('meme-insert');
-let image = document.getElementById('meme-image');
+const upload = document.getElementById('meme-insert');
+const image = document.getElementById('meme-image');
 
 upload.addEventListener('change', function (event) {
   image.src = URL.createObjectURL(event.target.files[0]);
