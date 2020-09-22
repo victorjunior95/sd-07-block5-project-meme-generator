e15516ea9a8ca73914797573817ac84096ac7343
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-09-22 10:48:26 -0300

    Alterações para o sétimo requisito

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ let loadFile = function(event) {
 };
 
 let textInputInit = document.querySelector("#text-input");
-textInputInit.addEventListener("keyup", loadText);
-
-function loadText() {
+textInputInit.addEventListener("keyup", function () {
     let textInput = document.querySelector("#text-input").value;
     let textOutput = document.querySelector("#meme-text");
     textOutput.innerText = textInput ;
-};
+});
 
 document.querySelector('#text-input').setAttribute('maxlength',60);
 
@@ -32,4 +30,26 @@ waterButton.addEventListener("click", function () {
 let earthButton = document.getElementById("earth")
 earthButton.addEventListener("click", function () {
     imageContainer.style.border = "6px groove green"
+});
+
+let memeImage = document.getElementById("meme-image");
+
+let meme1 = document.getElementById("meme-1")
+meme1.addEventListener("click", function () {
+    memeImage.src = meme1.src
+});
+
+let meme2 = document.getElementById("meme-2")
+meme2.addEventListener("click", function () {
+    memeImage.src = meme2.src
+});
+
+let meme3 = document.getElementById("meme-3")
+meme3.addEventListener("click", function () {
+    memeImage.src = meme3.src
+});
+
+let meme4 = document.getElementById("meme-4")
+meme4.addEventListener("click", function () {
+    memeImage.src = meme4.src
 });
