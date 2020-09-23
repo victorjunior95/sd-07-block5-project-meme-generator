42f844de8b2f9c4cf0793fb137ccd0afd5c597c3
Victor Junior <victorjr.muchacho@gmail.com>
2020-09-23 00:18:55 -0400

    Configurado o carregamento da imagem e visualizaçao

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 window.onload = function () {
+    setupImageInput();
     setupTextInput();
 }
 
+// apresentar o texto do input no meme-image-container
 function setupTextInput() {
     let textInput = document.querySelector("#text-input");
     //textInput.addEventListener("keydown", function(event) -- form alternativa
-    textInput.addEventListener("input", function(event) {
+    textInput.addEventListener("input", function (event) {
         // recuperar o texto digitado
         let text = event.target.value;
         // recuperar o elemento html
@@ -14,3 +16,29 @@ function setupTextInput() {
         memeTextParagraph.innerText = text;
     })
 }
+
+function setupImageInput() {
+    // https://www.youtube.com/watch?v=VElnT8EoEEM&t=612s
+    let memeInsert = document.getElementById("meme-insert");
+    let memeImageContainer = document.getElementById("meme-image-container");
+    let memeImage = memeImageContainer.querySelector(".meme-image");
+
+    memeInsert.addEventListener("change", function () {
+        let image = this.files[0];
+
+        if (image) {
+            let reader = new FileReader();
+            memeImage.style.display = "block";
+
+            reader.addEventListener("load", function () {
+                memeImage.setAttribute("src", this.result);
+            })
+
+            reader.readAsDataURL(image);
+        } else {
+            memeImage.style.display = null;
+            memeImage.setAttribute("src", "");
+        }
+    })
+}
+
