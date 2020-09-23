d73b71b001db278eedd341cf393324f6dfc3ea69
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2020-09-23 00:21:54 -0300

    adding backgroundColor on buttons

--- script.js
+++ script.js
@@ -17,14 +17,17 @@ window.onload = function () {
 
 const container = document.getElementById('meme-image-container');
 const fireButton = document.getElementById('fire');
+fireButton.style.backgroundColor = 'rgb(255 , 0 , 0)';
 fireButton.addEventListener('click', function () {
   container.style.border = '3px dashed rgb(255 , 0 , 0)';
 });
 const waterButton = document.getElementById('water');
+waterButton.style.backgroundColor = 'rgb(0 , 0 , 255)';
 waterButton.addEventListener('click', function () {
   container.style.border = '5px double rgb(0 , 0 , 255)';
 });
 const earthButton = document.getElementById('earth');
+earthButton.style.backgroundColor = 'rgb(0 , 128 , 0)';
 earthButton.addEventListener('click', function () {
   container.style.border = '6px groove rgb(0 , 128 , 0)';
 });
