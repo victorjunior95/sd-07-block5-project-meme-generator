c881018c0ffe76c6f9b1d43a03e1129653cdbbda
richardfazeredo <richardcompletetour@gmail.com>
2020-09-23 12:14:32 -0300

    Criando botões que adicionam molduras personalizadas para o container da imagem

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let textMeme = document.getElementById('text-input');
 let memeText = document.getElementById('meme-text');
 let memeInsert = document.getElementById('meme-insert');
+let buttons = document.querySelectorAll('button');
 
 textMeme.addEventListener('keyup', function (){
   memeText.innerText = textMeme.value;
 });
 
+
+for(let button of buttons){
+  button.addEventListener('click', function () {
+    let memeImageContainer = document.getElementById('meme-image-container');
+    let border = window.getComputedStyle(button, null)
+    .getPropertyValue("border");
+    memeImageContainer.style.border = border;  
+  });
+}
