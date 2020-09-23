8c5b987412d6fa0d709498c59bdc1e9d76ab1efa
Thays Costa <thayscosta3@gmail.com>
2020-09-22 23:45:38 -0300

    Revertendo alterações

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
-let inputText = document.querySelector('#text-input');
-let inputImage = document.querySelector('#meme-insert');
-let containerBorder = document.querySelector('#meme-image-container');
-let image = document.querySelector('#meme-image');
+const inputText = document.querySelector('#text-input');
+const inputImage = document.querySelector('#meme-insert');
+const containerBorder = document.querySelector('#meme-image-container');
+const image = document.querySelector('#meme-image');
 
-inputText.addEventListener('keyup', function(){
+inputText.addEventListener('keyup', function () {
   document.querySelector('#meme-text').innerText = inputText.value;
 });
 
-document.querySelector('#meme-1').addEventListener('click', function(event){
+document.querySelector('#meme-1').addEventListener('click', function (event) {
   image.src = event.target.src;
 });
 
-document.querySelector('#meme-2').addEventListener('click', function(event){
+document.querySelector('#meme-2').addEventListener('click', function (event) {
   image.src = event.target.src;
 });
 
-document.querySelector('#meme-3').addEventListener('click', function(event){
+document.querySelector('#meme-3').addEventListener('click', function (event) {
   image.src = event.target.src;
 });
 
-document.querySelector('#meme-4').addEventListener('click', function(event){
+document.querySelector('#meme-4').addEventListener('click', function (event) {
   image.src = event.target.src;
 });
 
-inputImage.addEventListener('change', function (event){
+inputImage.addEventListener('change', function (event) {
   image.src = URL.createObjectURL(event.target.files[0]);
   image.onload = function() {
     URL.revokeObjectURL(image.src);
   }
 });
 
-let fire = document.querySelector('#fire')
-fire.addEventListener('click', function(){
+const fire = document.querySelector('#fire');
+fire.addEventListener('click', function () {
   containerBorder.style.border = '3px dashed red';
-})
+});
 
-let water = document.querySelector('#water')
-water.addEventListener('click', function(){
+const water = document.querySelector('#water');
+water.addEventListener('click', function () {
   containerBorder.style.border = '5px double blue';
-})
+});
 
-let earth = document.querySelector('#earth')
-earth.addEventListener('click', function(){
+const earth = document.querySelector('#earth');
+earth.addEventListener('click', function () {
   containerBorder.style.border = 'groove 6px green';
-})
-
-z
+});
