55aab3dfc89963f1c0a495f99a70069d1ed3f397
Lucas Sá <lucas.nonato1@hotmail.com>
2020-09-22 21:43:02 -0300

    inicio requisito bonus 1 com html e css

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const textInput = document.getElementById('text-input');
+const upload = document.getElementById('meme-insert');
+const image = document.getElementById('meme-image');
 
 textInput.addEventListener('keyup', function () {
   const memeText = document.getElementById('meme-text');
 
   memeText.innerHTML = textInput.value;
 });
-
-const upload = document.getElementById('meme-insert');
-const image = document.getElementById('meme-image');
 
 upload.addEventListener('change', function (event) {
   image.src = URL.createObjectURL(event.target.files[0]);
