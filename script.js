6a755ac0996f68d6795ae50692e74627727892c6
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-23 00:05:39 -0300

    commit final?

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 function loadFile(event) {
     let divImage = document.getElementById("meme-image");
     let source = URL.createObjectURL(event.target.files[0]);
+    //O novo objeto URL representa o objeto File passado como argumento
+    //fonte https://developer.mozilla.org/pt-BR/docs/Web/API/URL/createObjectURl
     divImage.innerHTML = `<img  src='${source}'                            
                         </img>`;
+    // Adicionando á div a tag img, carregando com src do objeto passado como argumento.
 }
+//A função em onload="imageOption()" nunca vai ser chamada com src vazio. O onload é disparado justamente quando algo é carregado. Com o src vazio, esse algo é inexistente.
+//fonte https://pt.stackoverflow.com/questions/276808/carregar-imagem-via-javascript
 
-function insertImage(src) {
-    let divImage = document.getElementById("meme-image");
-    divImage.innerHTML = `<img src='/imgs/${src}' >                             
-                        </img>`;
-}
 window.onload = function() {
-    let inputText = document.getElementById("text-input");
-    let memeText = document.getElementById("meme-text");
-    inputText.addEventListener("input", function() {
-        memeText.innerHTML = inputText.value;
-    });
-
     let buttonImage = document.getElementById("meme-insert");
     buttonImage.addEventListener("change", function(event) {
         loadFile(event);
+    });
+
+    let inputText = document.getElementById("text-input");
+    let memeText = document.getElementById("meme-text");
+
+    inputText.addEventListener("input", function() {
+        memeText.innerHTML = inputText.value;
     });
 };
 //consultei o pull requeste de Luciano Brechon - Turma 07 da Trybe para compreender melhor as funções.
