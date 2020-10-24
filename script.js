4617eeb0c0753b2ce853aa59dcf1583462ea448c
wellington <profwellington17@gmail.com>
2020-10-24 19:27:09 -0300

    Fixing meme-text

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const textImput = document.getElementById('text-input');
 const memeText = document.createElement("p");
 const container = document.querySelector(".container");
+
 textImput.addEventListener("keyup", function () {
   memeText.innerText = textImput.value;
-  memeText.className = 'meme-text';
-  memeText.id = 'meme-text';
-  container.appendChild(memeText);
 });
+
+//memeText.className = 'meme-text';
+memeText.id = 'meme-text';
+container.appendChild(memeText);
 
 const memeInsert = document.querySelector("#meme-insert");
 const img = document.createElement("img");
