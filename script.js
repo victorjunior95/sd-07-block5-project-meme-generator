d1e0b17df09c3dafbcf092a0f82bf296f5818a0c
wellington <profwellington17@gmail.com>
2020-10-24 19:13:55 -0300

    inser imagem

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const textImput = document.getElementById('text-input');
 const memeText = document.createElement("p");
 const container = document.querySelector(".container");
-textImput.addEventListener("keyup", function(){
+textImput.addEventListener("keyup", function () {
   memeText.innerText = textImput.value;
   memeText.className = 'meme-text';
   memeText.id = 'meme-text';
   container.appendChild(memeText);
+});
+
+const memeInsert = document.querySelector("#meme-insert");
+const img = document.createElement("img");
+memeInsert.addEventListener("change", function () {
+  img.src = URL.createObjectURL(this.files[0]);
+  img.className = "meme-image";
+  img.id = "meme-image";
+  container.appendChild(img);
 });
